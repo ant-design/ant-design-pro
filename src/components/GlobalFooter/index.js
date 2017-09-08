488867e6ca826d4ee4e48f0588b75282f18194e4
afc163 <afc163@gmail.com>
2017-09-08 14:41:35 +0800

    code style

--- src/components/GlobalFooter/index.js
+++ src/components/GlobalFooter/index.js
@@ -7,10 +7,19 @@ export default ({ className, links, copyright }) => {
   return (
     <div className={clsString}>
       {
-        links &&
-        <div className={styles.links}>
-          {links.map(link => <a key={link.title} target={link.blankTarget ? '_blank' : '_self'} href={link.href}>{link.title}</a>)}
-        </div>
+        links && (
+          <div className={styles.links}>
+            {links.map(link => (
+              <a
+                key={link.title}
+                target={link.blankTarget ? '_blank' : '_self'}
+                href={link.href}
+              >
+                {link.title}
+              </a>
+            ))}
+          </div>
+        )
       }
       {copyright && <div className={styles.copyright}>{copyright}</div>}
     </div>
