21dba8bce623167676da5faf9e9dd6c2a99f90db
afc163 <afc163@gmail.com>
2017-09-14 20:17:17 +0800

    Move theme to a saperate file

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+// https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+module.exports = {
+  'font-size-base': '14px',
+  'badge-font-size': '12px',
+  'btn-font-size-lg': '@font-size-base',
+  'menu-dark-bg': '#00182E',
+  'menu-dark-submenu-bg': '#000B14',
+  'layout-sider-background': '#00182E',
+  'layout-body-background': '#f0f2f5',
+};
