7b8ea8df63a37c5acc47803483728959b3009127
afc163 <afc163@gmail.com>
2017-09-19 15:15:56 +0800

    Fix variable error

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
 // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
 module.exports = {
   'badge-font-size': '12px',
-  'btn-font-size-lg': '@font-size-base',
+  'btn-font-size-lg': '14px',
   'menu-dark-bg': '#00182E',
   'menu-dark-submenu-bg': '#000B14',
   'layout-sider-background': '#00182E',
