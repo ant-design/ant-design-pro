ffc1e5a98e919e461eb29da22b092a030104686d
afc163 <afc163@gmail.com>
2017-10-11 16:19:15 +0800

    Fix NoticeList style

--- src/components/NoticeIcon/NoticeList.js
+++ src/components/NoticeIcon/NoticeList.js
@@ -22,6 +22,7 @@ export default function NoticeList({ data = [], onClick, onClear, title, locale 
           return (
             <List.Item className={itemCls} key={item.key || i} onClick={() => onClick(item)}>
               <List.Item.Meta
+                className={styles.meta}
                 avatar={item.avatar ? <Avatar className={styles.avatar} src={item.avatar} /> : null}
                 title={
                   <div>
