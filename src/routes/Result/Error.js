9fc2c15a24c2dfd05b1c3b2667a7b787dcf98491
ddcat1115 <ddcat1115@gmail.com>
2017-10-18 17:57:36 +0800

    update result pages

--- src/routes/Result/Error.js
+++ src/routes/Result/Error.js
@@ -5,16 +5,16 @@ import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 
 const extra = (
   <div>
-    <div style={{ fontSize: 16, color: 'rgba(0, 0, 0, 0.65)', fontWeight: '600', marginBottom: 16 }}>
+    <div style={{ fontSize: 16, color: 'rgba(0, 0, 0, 0.85)', fontWeight: '500', marginBottom: 16 }}>
       您提交的内容有如下错误：
     </div>
-    <div style={{ marginBottom: 8 }}>
+    <div style={{ marginBottom: 16 }}>
       <Icon style={{ color: '#f04134', marginRight: 8 }} type="close-circle-o" />您的账户已被冻结
-      <a style={{ marginLeft: 24 }}>立即解冻 <Icon type="right" /></a>
+      <a style={{ marginLeft: 16 }}>立即解冻 <Icon type="right" /></a>
     </div>
     <div>
       <Icon style={{ color: '#f04134', marginRight: 8 }} type="close-circle-o" />您的账户还不具备申请资格
-      <a style={{ marginLeft: 24 }}>立即升级 <Icon type="right" /></a>
+      <a style={{ marginLeft: 16 }}>立即升级 <Icon type="right" /></a>
     </div>
   </div>
 );
@@ -30,7 +30,7 @@ export default () => (
         description="请核对并修改以下信息后，再重新提交。"
         extra={extra}
         actions={actions}
-        style={{ marginTop: 30, marginBottom: 30 }}
+        style={{ marginTop: 48, marginBottom: 16 }}
       />
     </Card>
   </PageHeaderLayout>
