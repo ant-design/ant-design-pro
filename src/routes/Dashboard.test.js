12f41ad836075a3ef077e60c3b167550c7c0a54b
afc163 <afc163@gmail.com>
2017-10-23 21:59:31 +0800

    upgrade enzyme

--- src/routes/Dashboard.test.js
+++ src/routes/Dashboard.test.js
@@ -3,8 +3,10 @@ import { shallow } from 'enzyme';
 import Dashboard from './Dashboard';
 
 it('renders Dashboard', () => {
+  const fetchFn = jest.fn();
   const wrapper = shallow(
-    <Dashboard.WrappedComponent user={{ list: [] }} />
+    <Dashboard.WrappedComponent user={{ list: [] }} dispatch={fetchFn} />
   );
   expect(wrapper.find('Table').props().dataSource).toEqual([]);
+  expect(fetchFn).toBeCalled();
 });
