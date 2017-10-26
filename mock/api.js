1e54651cd46a3fdb2e266851c6ba347d4aadf1b4
afc163 <afc163@gmail.com>
2017-10-26 19:09:56 +0800

    update mock data

--- mock/api.js
+++ mock/api.js
@@ -1,53 +1,53 @@
 import { imgMap, getUrlParams } from './utils';
 
+const titles = [
+  'Alipay',
+  'Angular',
+  'Ant Design',
+  'Ant Design Pro',
+  'Bootstrap',
+  'React',
+  'Vue',
+  'Webpack',
+];
+const avatars = [
+  'https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png', // Alipay
+  'https://gw.alipayobjects.com/zos/rmsportal/zOsKZmFRdUtvpqCImOVY.png', // Angular
+  'https://gw.alipayobjects.com/zos/rmsportal/dURIMkkrRFpPgTuzkwnB.png', // Ant Design
+  'https://gw.alipayobjects.com/zos/rmsportal/sfjbOqnsXXJgNCjCzDBL.png', // Ant Design Pro
+  'https://gw.alipayobjects.com/zos/rmsportal/siCrBXXhmvTQGWPNLBow.png', // Bootstrap
+  'https://gw.alipayobjects.com/zos/rmsportal/kZzEzemZyKLKFsojXItE.png', // React
+  'https://gw.alipayobjects.com/zos/rmsportal/ComBAopevLwENQdKWiIn.png', // Vue
+  'https://gw.alipayobjects.com/zos/rmsportal/nxkuOJlFJuAUhzlMTCEe.png', // Webpack
+];
+const covers = [
+  'https://gw.alipayobjects.com/zos/rmsportal/HrxcVbrKnCJOZvtzSqjN.png',
+  'https://gw.alipayobjects.com/zos/rmsportal/alaPpKWajEbIYEUvvVNf.png',
+  'https://gw.alipayobjects.com/zos/rmsportal/RLwlKSYGSXGHuWSojyvp.png',
+  'https://gw.alipayobjects.com/zos/rmsportal/gLaIAoVWTtLbBWZNYEMg.png',
+];
+const desc = [
+  '那是一种内在的东西， 他们到达不了，也无法触及的',
+  '希望是一个好东西，也许是最好的，好东西是不会消亡的',
+  '生命就像一盒巧克力，结果往往出人意料',
+  '城镇中有那么多的酒馆，她却偏偏走进了我的酒馆',
+  '那时候我只会想自己想要什么，从不想自己拥有什么',
+];
+
+const user = [
+  '付小小',
+  '曲丽丽',
+  '林东东',
+  '周星星',
+  '吴加好',
+  '朱偏右',
+  '鱼酱',
+  '乐哥',
+  '谭小仪',
+  '仲尼',
+];
+
 export function fakeList(count) {
-  const titles = [
-    'Alipay',
-    'Angular',
-    'Ant Design Pro',
-    'Ant Design',
-    'Bootstrap',
-    'React',
-    'Vue',
-    'Webpack',
-  ];
-  const avatars = [
-    'https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png', // Alipay
-    'https://gw.alipayobjects.com/zos/rmsportal/zOsKZmFRdUtvpqCImOVY.png', // Angular
-    'https://gw.alipayobjects.com/zos/rmsportal/sfjbOqnsXXJgNCjCzDBL.png', // Ant Design Pro
-    'https://gw.alipayobjects.com/zos/rmsportal/dURIMkkrRFpPgTuzkwnB.png', // Ant Design
-    'https://gw.alipayobjects.com/zos/rmsportal/siCrBXXhmvTQGWPNLBow.png', // Bootstrap
-    'https://gw.alipayobjects.com/zos/rmsportal/kZzEzemZyKLKFsojXItE.png', // React
-    'https://gw.alipayobjects.com/zos/rmsportal/ComBAopevLwENQdKWiIn.png', // Vue
-    'https://gw.alipayobjects.com/zos/rmsportal/nxkuOJlFJuAUhzlMTCEe.png', // Webpack
-  ];
-  const covers = [
-    'https://gw.alipayobjects.com/zos/rmsportal/HrxcVbrKnCJOZvtzSqjN.png',
-    'https://gw.alipayobjects.com/zos/rmsportal/alaPpKWajEbIYEUvvVNf.png',
-    'https://gw.alipayobjects.com/zos/rmsportal/RLwlKSYGSXGHuWSojyvp.png',
-    'https://gw.alipayobjects.com/zos/rmsportal/gLaIAoVWTtLbBWZNYEMg.png',
-  ];
-  const desc = [
-    '那是一种内在的东西， 他们到达不了，也无法触及的',
-    '希望是一个好东西，也许是最好的，好东西是不会消亡的',
-    '生命就像一盒巧克力，结果往往出人意料',
-    '城镇中有那么多的酒馆，她却偏偏走进了我的酒馆',
-    '那时候我只会想自己想要什么，从不想自己拥有什么',
-  ];
-
-  const user = [
-    '付小小',
-    '曲丽丽',
-    '林东东',
-    '周星星',
-    '吴加好',
-    '朱偏右',
-    '鱼酱',
-    '乐哥',
-    '谭小仪',
-    '仲尼',
-  ];
-
   const list = [];
   for (let i = 0; i < count; i += 1) {
     list.push({
@@ -112,8 +112,8 @@ export function getFakeList(req, res, u) {
 export const getNotice = [
   {
     id: 'xxx1',
-    title: 'Vue',
-    logo: imgMap.b,
+    title: titles[0],
+    logo: avatars[0],
     description: '那是一种内在的东西， 他们到达不了，也无法触及的',
     updatedAt: new Date(),
     member: '科学搬砖组',
@@ -122,8 +122,8 @@ export const getNotice = [
   },
   {
     id: 'xxx2',
-    title: 'Angular',
-    logo: imgMap.c,
+    title: titles[1],
+    logo: avatars[1],
     description: '希望是一个好东西，也许是最好的，好东西是不会消亡的',
     updatedAt: new Date('2017-07-24 11:00:00'),
     member: '全组都是吴彦祖',
@@ -132,8 +132,8 @@ export const getNotice = [
   },
   {
     id: 'xxx3',
-    title: 'React',
-    logo: imgMap.a,
+    title: titles[2],
+    logo: avatars[2],
     description: '城镇中有那么多的酒馆，她却偏偏走进了我的酒馆',
     updatedAt: new Date(),
     member: '中二少女团',
@@ -142,8 +142,8 @@ export const getNotice = [
   },
   {
     id: 'xxx4',
-    title: 'Ant Design',
-    logo: imgMap.a,
+    title: titles[3],
+    logo: avatars[3],
     description: '那时候我只会想自己想要什么，从不想自己拥有什么',
     updatedAt: new Date('2017-07-23 06:23:00'),
     member: '程序员日常',
@@ -152,8 +152,8 @@ export const getNotice = [
   },
   {
     id: 'xxx5',
-    title: 'Ant Design Mobile',
-    logo: imgMap.b,
+    title: titles[4],
+    logo: avatars[4],
     description: '凛冬将至',
     updatedAt: new Date('2017-07-23 06:23:00'),
     member: '高逼格设计天团',
@@ -162,8 +162,8 @@ export const getNotice = [
   },
   {
     id: 'xxx6',
-    title: 'Ant Motion',
-    logo: imgMap.c,
+    title: titles[5],
+    logo: avatars[5],
     description: '生命就像一盒巧克力，结果往往出人意料',
     updatedAt: new Date('2017-07-23 06:23:00'),
     member: '骗你来学计算机',
@@ -259,7 +259,7 @@ export const getActivities = [
     updatedAt: new Date(),
     user: {
       name: '乐哥',
-      avatar: imgMap.d,
+      avatar: imgMap.b,
     },
     group: {
       name: '程序员日常',
