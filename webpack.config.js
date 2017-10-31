6f3f75399a906c74a657b7a328f453229289f421
afc163 <afc163@gmail.com>
2017-10-31 11:09:08 +0800

    upgrade roadhog and use ignoreMomentLocale

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-const webpack = require('webpack');
-
-module.exports = (webpackConfig) => {
-  // Avoid import all locale file of moment
-  webpackConfig.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/));
-  return webpackConfig;
-};
