e2eb866442ae2623a35cbcca1efef5f5aa8beb20
afc163 <afc163@gmail.com>
2017-10-31 14:41:24 +0800

    Improve reponsive

--- src/layouts/PageHeaderLayout.js
+++ src/layouts/PageHeaderLayout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'dva/router';
 import PageHeader from '../components/PageHeader';
+import styles from './PageHeaderLayout.less';
 
 export default ({ children, wrapperClassName, top, ...restProps }) => (
   <div style={{ margin: '-24px -24px 0' }} className={wrapperClassName}>
     {top}
     <PageHeader {...restProps} linkElement={Link} />
-    {children ? <div style={{ margin: '24px 24px 0' }}>{children}</div> : null}
+    {children ? <div className={styles.content}>{children}</div> : null}
   </div>
 );
