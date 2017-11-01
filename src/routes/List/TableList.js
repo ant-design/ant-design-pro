97092686cfbcc69b29b1f038c18b17a98a25d8d5
afc163 <afc163@gmail.com>
2017-11-01 21:51:00 +0800

    Responsive search form in Table List page
    close #34

--- src/routes/List/TableList.js
+++ src/routes/List/TableList.js
@@ -156,15 +156,15 @@ export default class TableList extends PureComponent {
     const { getFieldDecorator } = this.props.form;
     return (
       <Form onSubmit={this.handleSearch} layout="inline">
-        <Row gutter={48}>
-          <Col span={8}>
+        <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
+          <Col md={8} sm={24}>
             <FormItem label="规则编号">
               {getFieldDecorator('no')(
                 <Input placeholder="请输入" />
               )}
             </FormItem>
           </Col>
-          <Col span={8}>
+          <Col md={8} sm={24}>
             <FormItem label="使用状态">
               {getFieldDecorator('status')(
                 <Select placeholder="请选择" style={{ width: '100%' }}>
@@ -174,7 +174,7 @@ export default class TableList extends PureComponent {
               )}
             </FormItem>
           </Col>
-          <Col span={8}>
+          <Col md={8} sm={24}>
             <span className={styles.sumbitButtons}>
               <Button type="primary" htmlType="submit">查询</Button>
               <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>重置</Button>
@@ -192,15 +192,15 @@ export default class TableList extends PureComponent {
     const { getFieldDecorator } = this.props.form;
     return (
       <Form onSubmit={this.handleSearch} layout="inline">
-        <Row gutter={48}>
-          <Col span={8}>
+        <Row gutter={{ md: 8, lg: 24, xl: 18 }}>
+          <Col md={8} sm={24}>
             <FormItem label="规则编号">
               {getFieldDecorator('no')(
                 <Input placeholder="请输入" />
               )}
             </FormItem>
           </Col>
-          <Col span={8}>
+          <Col md={8} sm={24}>
             <FormItem label="使用状态">
               {getFieldDecorator('status')(
                 <Select placeholder="请选择" style={{ width: '100%' }}>
@@ -210,7 +210,7 @@ export default class TableList extends PureComponent {
               )}
             </FormItem>
           </Col>
-          <Col span={8}>
+          <Col md={8} sm={24}>
             <FormItem label="调用次数">
               {getFieldDecorator('number')(
                 <InputNumber style={{ width: '100%' }} />
@@ -218,15 +218,15 @@ export default class TableList extends PureComponent {
             </FormItem>
           </Col>
         </Row>
-        <Row gutter={48}>
-          <Col span={8}>
+        <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
+          <Col md={8} sm={24}>
             <FormItem label="更新日期">
               {getFieldDecorator('date')(
                 <DatePicker style={{ width: '100%' }} placeholder="请输入更新日期" />
               )}
             </FormItem>
           </Col>
-          <Col span={8}>
+          <Col md={8} sm={24}>
             <FormItem label="使用状态">
               {getFieldDecorator('status3')(
                 <Select placeholder="请选择" style={{ width: '100%' }}>
@@ -236,7 +236,7 @@ export default class TableList extends PureComponent {
               )}
             </FormItem>
           </Col>
-          <Col span={8}>
+          <Col md={8} sm={24}>
             <FormItem label="使用状态">
               {getFieldDecorator('status4')(
                 <Select placeholder="请选择" style={{ width: '100%' }}>
@@ -248,7 +248,7 @@ export default class TableList extends PureComponent {
           </Col>
         </Row>
         <div style={{ overflow: 'hidden' }}>
-          <span style={{ float: 'right' }}>
+          <span style={{ float: 'right', marginBottom: 24 }}>
             <Button type="primary" htmlType="submit">查询</Button>
             <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>重置</Button>
             <a style={{ marginLeft: 8 }} onClick={this.toggleForm}>
