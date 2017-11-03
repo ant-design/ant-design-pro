10e8c035d18af51a41d01ab2fbfb51df3542c5a2
afc163 <afc163@gmail.com>
2017-11-03 14:06:46 +0800

    Fix API fetch error, close #82

--- src/models/list.js
+++ src/models/list.js
@@ -17,7 +17,7 @@ export default {
       const response = yield call(queryFakeList, payload);
       yield put({
         type: 'save',
-        payload: response,
+        payload: Array.isArray(response) ? response : [],
       });
       yield put({
         type: 'changeLoading',
