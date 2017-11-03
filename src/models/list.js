d8e9f2259bc8040eaa942da4941ebe1640e14033
afc163 <afc163@gmail.com>
2017-11-03 20:21:28 +0800

    Improve search saga and loading logic

--- src/models/list.js
+++ src/models/list.js
@@ -5,38 +5,42 @@ export default {
 
   state: {
     list: [],
-    loading: true,
+    loading: false,
+    cursor: 0,
   },
 
   effects: {
-    *fetch({ payload, callback }, { call, put }) {
+    *fetch({ payload }, { call, put }) {
       yield put({
         type: 'changeLoading',
         payload: true,
       });
       const response = yield call(queryFakeList, payload);
       yield put({
-        type: 'save',
+        type: 'appendList',
         payload: Array.isArray(response) ? response : [],
       });
       yield put({
         type: 'changeLoading',
         payload: false,
       });
-      if (callback) {
-        callback();
-      }
     },
   },
 
   reducers: {
-    save(state, action) {
+    appendList(state, action) {
       return {
         ...state,
-        list: action.payload,
+        list: state.list.concat(action.payload),
       };
     },
     changeLoading(state, action) {
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    },
+    updateCursor(state, action) {
       return {
         ...state,
         loading: action.payload,
