7a95d6bc72354c0da75e48cb4c85f636cf098c5e
afc163 <afc163@gmail.com>
2017-11-14 10:46:56 +0800

    Add spin for dynamic import

--- src/router.js
+++ src/router.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import { LocaleProvider } from 'antd';
+import { LocaleProvider, Spin } from 'antd';
 import zhCN from 'antd/lib/locale-provider/zh_CN';
 import dynamic from 'dva/dynamic';
+import styles from './index.less';
+
+dynamic.setDefaultLoadingComponent(() => {
+  return <Spin size="large" className={styles.globalSpin} />;
+});
 
 function RouterConfig({ history, app }) {
   const BasicLayout = dynamic({
