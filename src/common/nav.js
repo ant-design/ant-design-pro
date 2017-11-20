0688459ba17259dc28626c47ee53a46b2f444dc9
nikogu <644506165@qq.com>
2017-11-20 16:33:52 +0800

    Fixed import BasicLayout login model https://github.com/ant-design/ant-design-pro/issues/243

--- src/common/nav.js
+++ src/common/nav.js
@@ -10,7 +10,7 @@ const dynamicWrapper = (app, models, component) => dynamic({
 // nav data
 export const getNavData = app => [
   {
-    component: dynamicWrapper(app, ['user'], () => import('../layouts/BasicLayout')),
+    component: dynamicWrapper(app, ['user', 'login'], () => import('../layouts/BasicLayout')),
     layout: 'BasicLayout',
     name: '首页', // for breadcrumb
     path: '/',
