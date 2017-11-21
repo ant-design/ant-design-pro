458d601ef2d293bb466ec3378fe95d0fbc474514
nikogu <644506165@qq.com>
2017-11-21 10:34:53 +0800

    add SimpleCard style to fixed: https://github.com/ant-design/ant-design-pro/issues/192

--- src/components/Charts/ChartCard/index.js
+++ src/components/Charts/ChartCard/index.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import { Card, Spin } from 'antd';
+import classNames from 'classnames';
 
 import styles from './index.less';
 
 const ChartCard = ({
-  loading = false, contentHeight, title, action, total, footer, children, ...rest
+  loading = false, contentHeight, title, avatar, action, total, footer, children, ...rest
 }) => {
   const content = (
     <div className={styles.chartCard}>
-      <div className={styles.meta}>
-        <span className={styles.title}>{title}</span>
-        <span className={styles.action}>{action}</span>
-      </div>
-      {
-        // eslint-disable-next-line
-        total && <div className={styles.total} dangerouslySetInnerHTML={{ __html: total }} />
-      }
-      <div className={styles.content} style={{ height: contentHeight || 'auto' }}>
-        <div className={contentHeight && styles.contentFixed}>
-          {children}
+      <div
+        className={classNames(styles.chartTop, { [styles.chartTopMargin]: (!children && !footer) })}
+      >
+        <div className={styles.avatar}>
+          {
+            avatar
+          }
+        </div>
+        <div className={styles.metaWrap}>
+          <div className={styles.meta}>
+            <span className={styles.title}>{title}</span>
+            <span className={styles.action}>{action}</span>
+          </div>
+          {
+            // eslint-disable-next-line
+            total && (<div className={styles.total} dangerouslySetInnerHTML={{ __html: total }} />)
+          }
         </div>
       </div>
       {
+        children && (
+          <div className={styles.content} style={{ height: contentHeight || 'auto' }}>
+            <div className={contentHeight && styles.contentFixed}>
+              {children}
+            </div>
+          </div>
+        )
+      }
+      {
         footer && (
-          <div className={styles.footer}>
+          <div className={classNames(styles.footer, { [styles.footerMargin]: !children })}>
             {footer}
           </div>
         )
