3dec7c8b711b2a85c74116f95ef00fa2154bab04
afc163 <afc163@gmail.com>
2017-11-23 19:23:24 +0800

    use rollbar over rollbar-browser

--- src/rollbar.js
+++ src/rollbar.js
@@ -1,10 +1,11 @@
-import Rollbar from 'rollbar-browser';
+import Rollbar from 'rollbar';
 
 // Track error by https://sentry.io/
 if (location.host === 'preview.pro.ant.design') {
   Rollbar.init({
     accessToken: '033ca6d7c0eb4cc1831cf470c2649971',
     captureUncaught: true,
+    captureUnhandledRejections: true,
     payload: {
       environment: 'production',
     },
