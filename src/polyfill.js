8262d142376c6c574c6849c0480efb98803baa6f
afc163 <afc163@gmail.com>
2017-11-23 11:50:04 +0800

    Fix polyfill, #210

--- src/polyfill.js
+++
@@ -1,8 +0,0 @@
-import 'core-js/es6/map';
-import 'core-js/es6/set';
-import 'babel-polyfill';
-
-global.requestAnimationFrame =
-  global.requestAnimationFrame || function requestAnimationFrame(callback) {
-    setTimeout(callback, 0);
-  };
