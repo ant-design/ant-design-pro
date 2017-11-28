f3609c034cb3e4b8781276ccf6dfe6df030943c9
jim <wasd2144@hotmail.com>
2017-11-28 12:15:22 +0800

    增加CountDown d.ts描述文件

---
+++ src/components/CountDown/index.d.ts
@@ -0,0 +1,9 @@
+import React from "react";
+export interface CountDownProps {
+  format?: (time: number) => void;
+  target: Date | number;
+  onEnd?: () => void;
+  style?: React.CSSProperties;
+}
+
+export default class CountDown extends React.Component<CountDownProps, any> {}
