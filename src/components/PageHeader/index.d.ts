72b519e0c0cb911cf2f7f8e8c0e61c6ed7f4ceb7
jim <wasd2144@hotmail.com>
2017-12-01 13:39:29 +0800

    fix some errors found during use

--- src/components/PageHeader/index.d.ts
+++ src/components/PageHeader/index.d.ts
@@ -6,11 +6,11 @@ export interface PageHeaderProps {
   content?: React.ReactNode;
   extraContent?: React.ReactNode;
   routes?: Array<any>;
-  params: any;
+  params?: any;
   breadcrumbList?: Array<{ title: React.ReactNode; href?: string }>;
   tabList?: Array<{ key: string; tab: React.ReactNode }>;
   onTabChange?: (key: string) => void;
-  linkElement?: string | React.ReactNode;
+  linkElement?: React.ReactNode;
 }
 
 export default class PageHeader extends React.Component<PageHeaderProps, any> {}
