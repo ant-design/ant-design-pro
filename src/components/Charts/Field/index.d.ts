6d14224a3afd7db8a316ee43f7d66f0774c12cfd
jim <wasd2144@hotmail.com>
2017-12-02 14:01:38 +0800

    remove excess property

--- src/components/Charts/Field/index.d.ts
+++ src/components/Charts/Field/index.d.ts
@@ -2,7 +2,6 @@ import * as React from "react";
 export interface FieldProps {
   label: React.ReactNode;
   value: React.ReactNode;
-  contentHeight?: number;
 }
 
 export default class Field extends React.Component<FieldProps, any> {}
