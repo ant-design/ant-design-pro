8146d1f35b3d20cb346e81af620ad0e348bb0f3c
nikogu <644506165@qq.com>
2017-12-25 10:40:48 +0800

    upgrade react@16 & bizcharts@3.1.0

--- src/g2.js
+++ src/g2.js
@@ -2,28 +2,3 @@
 import { track } from 'bizcharts';
 
 track(false);
-
-// const colors = [
-//   '#8543E0',
-//   '#F04864',
-//   '#FACC14',
-//   '#1890FF',
-//   '#13C2C2',
-//   '#2FC25B',
-//   '#fa8c16',
-//   '#a0d911',
-// ];
-
-// const config = {
-//   defaultColor: '#1089ff',
-//   colors,
-//   tooltip: {
-//     background: {
-//       radius: 4,
-//       fill: '#000',
-//       fillOpacity: 0.75,
-//     },
-//   },
-// };
-
-// setTheme(config);
