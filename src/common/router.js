030d8b5a513af05f76606f14ed37288ecec40248
Henry <henry.ding@3vyd.com>
2017-12-27 10:13:21 +0800

    fix ci

--- src/common/router.js
+++ src/common/router.js
@@ -5,6 +5,7 @@ import { getMenuData } from './menu';
 // wrapper of dynamic
 const dynamicWrapper = (app, models, component) => dynamic({
   app,
+  // eslint-disable-next-line no-underscore-dangle
   models: () => models.filter(m => !app._models.some(({ namespace }) => namespace === m)).map(m => import(`../models/${m}.js`)),
   // add routerData prop
   component: () => {
