4120935cedfbcdc9ae12bc95d17bceb85d2a205d
jim <wasd2144@hotmail.com>
2017-12-28 11:23:18 +0800

    fix error

--- src/routes/Dashboard/Monitor.js
+++ src/routes/Dashboard/Monitor.js
@@ -6,7 +6,7 @@ import { Pie, WaterWave, Gauge, TagCloud } from '../../components/Charts';
 import NumberInfo from '../../components/NumberInfo';
 import CountDown from '../../components/CountDown';
 import ActiveChart from '../../components/ActiveChart';
-import { Secured } from '../../utils/Secured';
+import Secured from '../../utils/Secured';
 import styles from './Monitor.less';
 
 const targetTime = new Date().getTime() + 3900000;
