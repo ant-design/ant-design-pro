86a2d26dee3675c937e2890ebe389b1eeba88095
afc163 <afc163@gmail.com>
2017-12-29 13:57:26 +0800

    simplify code

--- src/common/router.js
+++ src/common/router.js
@@ -9,13 +9,10 @@ const dynamicWrapper = (app, models, component) => dynamic({
   models: () => models.filter(m => !app._models.some(({ namespace }) => namespace === m)).map(m => import(`../models/${m}.js`)),
   // add routerData prop
   component: () => {
-    const p = component();
     const routerData = getRouterData(app);
-    return new Promise((resolve, reject) => {
-      p.then((raw) => {
-        const Comp = raw.default || raw;
-        resolve(props => <Comp {...props} routerData={routerData} />);
-      }).catch(err => reject(err));
+    return component().then((raw) => {
+      const Component = raw.default || raw;
+      return props => <Component {...props} routerData={routerData} />;
     });
   },
 });
