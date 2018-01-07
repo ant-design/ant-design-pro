668074c8b03bff5c1dcfa35add5dd4b46390e76e
afc163 <afc163@gmail.com>
2018-01-07 17:12:07 +0800

    Fix test case

--- src/e2e/home.e2e.js
+++
@@ -1,9 +0,0 @@
-import Nightmare from 'nightmare';
-
-describe('Homepage', () => {
-  it('it should have logo text', async () => {
-    const page = Nightmare().goto('http://localhost:8000');
-    const text = await page.wait('h1').evaluate(() => document.body.innerHTML).end();
-    expect(text).toContain('<h1>Ant Design Pro</h1>');
-  });
-});
