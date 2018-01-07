61c60a29e1b03fe37bfd271cea1c81d63526cc54
偏右 <afc163@gmail.com>
2018-01-07 18:09:46 +0800

    Config code split (#562)
    * change to one bundle first
    
    * config code split by babel-plugin-dynamic-import-node-sync
    
    * refactor
    
    * Use roadbog disableDynamicImport
    
    * fix typo
    
    * Fix require default

--- src/common/router.js
+++ src/common/router.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import { createElement } from 'react';
 import dynamic from 'dva/dynamic';
 import { getMenuData } from './menu';
 
+let routerDataCache;
+
+const modelNotExisted = (app, model) => (
+  // eslint-disable-next-line
+  !app._models.some(({ namespace }) => namespace === model)
+);
+
 // wrapper of dynamic
-const dynamicWrapper = (app, models, component) => dynamic({
-  app,
-  // eslint-disable-next-line no-underscore-dangle
-  models: () => models.filter(m => !app._models.some(({ namespace }) => namespace === m)).map(m => import(`../models/${m}.js`)),
-  // add routerData prop
-  component: () => {
-    const routerData = getRouterData(app);
-    return component().then((raw) => {
-      const Component = raw.default || raw;
-      return props => <Component {...props} routerData={routerData} />;
+const dynamicWrapper = (app, models, component) => {
+  // () => require('module')
+  // transformed by babel-plugin-dynamic-import-node-sync
+  if (component.toString().indexOf('.then(') < 0) {
+    models.forEach((model) => {
+      if (modelNotExisted(app, model)) {
+        // eslint-disable-next-line
+        app.model(require(`../models/${model}`).default);
+      }
     });
-  },
-});
+    return (props) => {
+      if (!routerDataCache) {
+        routerDataCache = getRouterData(app);
+      }
+      return createElement(component().default, {
+        ...props,
+        routerData: routerDataCache,
+      });
+    };
+  }
+  // () => import('module')
+  return dynamic({
+    app,
+    models: () => models.filter(
+      model => modelNotExisted(app, model)).map(m => import(`../models/${m}.js`)
+    ),
+    // add routerData prop
+    component: () => {
+      if (!routerDataCache) {
+        routerDataCache = getRouterData(app);
+      }
+      return component().then((raw) => {
+        const Component = raw.default || raw;
+        return props => createElement(Component, {
+          ...props,
+          routerData: routerDataCache,
+        });
+      });
+    },
+  });
+};
 
 function getFlatMenuData(menus) {
   let keys = {};
