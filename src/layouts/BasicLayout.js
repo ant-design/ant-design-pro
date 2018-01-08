cc37cabad450878bef97ff2d3963be2cb82a8dfb
afc163 <afc163@gmail.com>
2018-01-08 14:48:09 +0800

    2017 => 2018

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -201,7 +201,7 @@ class BasicLayout extends React.PureComponent {
               }]}
               copyright={
                 <div>
-                  Copyright <Icon type="copyright" /> 2017 蚂蚁金服体验技术部出品
+                  Copyright <Icon type="copyright" /> 2018 蚂蚁金服体验技术部出品
                 </div>
               }
             />
--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -21,7 +21,7 @@ const links = [{
   href: '',
 }];
 
-const copyright = <div>Copyright <Icon type="copyright" /> 2017 蚂蚁金服体验技术部出品</div>;
+const copyright = <div>Copyright <Icon type="copyright" /> 2018 蚂蚁金服体验技术部出品</div>;
 
 class UserLayout extends React.PureComponent {
   getPageTitle() {
