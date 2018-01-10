b278e0135505f3450a1a13444750e1cc8993015f
jim <wasd2144@hotmail.com>
2018-01-10 12:07:01 +0800

    update doc

--- src/common/router.js
+++ src/common/router.js
@@ -76,7 +76,7 @@ export const getRouterData = (app) => {
     '/dashboard/analysis': {
       component: dynamicWrapper(app, ['chart'], () => import('../routes/Dashboard/Analysis')),
     },
-    '/dashboard/monitor/': {
+    '/dashboard/monitor': {
       component: dynamicWrapper(app, ['monitor'], () => import('../routes/Dashboard/Monitor')),
     },
     '/dashboard/workplace': {
@@ -183,10 +183,8 @@ export const getRouterData = (app) => {
     }
     let router = routerConfig[path];
     // If you need to configure complex parameter routing,
-    // bread crumbs can not be displayed.
+    // https://github.com/ant-design/ant-design-pro-site/blob/master/docs/router-and-nav.md#%E5%B8%A6%E5%8F%82%E6%95%B0%E7%9A%84%E8%B7%AF%E7%94%B1%E8%8F%9C%E5%8D%95
     // eg . /list/:type/user/info/:id
-    // you can configure name in the router,
-    // bread crumbs will normally show
     router = {
       ...router,
       name: router.name || menuItem.name,
