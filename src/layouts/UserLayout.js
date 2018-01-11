5785156fb04ad130fbfe168eda8c347d5bc90327
Andreas Cederström <andriijas@users.noreply.github.com>
2018-01-11 20:03:37 +0100

    promote antd layout and content - tweak mobile styles

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link, Redirect, Switch, Route } from 'dva/router';
 import DocumentTitle from 'react-document-title';
-import { Icon } from 'antd';
+import { Layout, Icon } from 'antd';
 import GlobalFooter from '../components/GlobalFooter';
 import styles from './UserLayout.less';
 import logo from '../assets/logo.svg';
 import { getRoutes } from '../utils/utils';
+
+const { Content } = Layout;
 
 const links = [{
   key: 'help',
@@ -37,31 +39,33 @@ class UserLayout extends React.PureComponent {
     const { routerData, match } = this.props;
     return (
       <DocumentTitle title={this.getPageTitle()}>
-        <div className={styles.container}>
-          <div className={styles.top}>
-            <div className={styles.header}>
-              <Link to="/">
-                <img alt="logo" className={styles.logo} src={logo} />
-                <span className={styles.title}>Ant Design</span>
-              </Link>
+        <Layout className={styles.container}>
+          <Content className={styles.content}>
+            <div className={styles.top}>
+              <div className={styles.header}>
+                <Link to="/">
+                  <img alt="logo" className={styles.logo} src={logo} />
+                  <span className={styles.title}>Ant Design</span>
+                </Link>
+              </div>
+              <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
             </div>
-            <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
-          </div>
-          <Switch>
-            {getRoutes(match.path, routerData).map(item =>
-              (
-                <Route
-                  key={item.key}
-                  path={item.path}
-                  component={item.component}
-                  exact={item.exact}
-                />
-              )
-            )}
-            <Redirect exact from="/user" to="/user/login" />
-          </Switch>
-          <GlobalFooter className={styles.footer} links={links} copyright={copyright} />
-        </div>
+            <Switch>
+              {getRoutes(match.path, routerData).map(item =>
+                (
+                  <Route
+                    key={item.key}
+                    path={item.path}
+                    component={item.component}
+                    exact={item.exact}
+                  />
+                )
+              )}
+              <Redirect exact from="/user" to="/user/login" />
+            </Switch>
+          </Content>
+          <GlobalFooter links={links} copyright={copyright} />
+        </Layout>
       </DocumentTitle>
     );
   }
