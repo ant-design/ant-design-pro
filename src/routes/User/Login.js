e4f75af549d6b24bf2d671fd55965ddab3e10d15
afc163 <afc163@gmail.com>
2018-01-12 19:37:22 +0800

    Add username/password hint

--- src/routes/User/Login.js
+++ src/routes/User/Login.js
@@ -61,7 +61,7 @@ export default class LoginPage extends Component {
               login.status === 'error' &&
               login.type === 'account' &&
               !login.submitting &&
-              this.renderMessage('账户或密码错误')
+              this.renderMessage('账户或密码错误（admin/888888）')
             }
             <UserName name="userName" placeholder="admin/user" />
             <Password name="password" placeholder="888888/123456" />
