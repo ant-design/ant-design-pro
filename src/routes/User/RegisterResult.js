14d093f798dc4baea047d8853648baf87a91edb1
elevensky <346800989@qq.com>
2018-01-16 16:18:54 +0800

    fix account name undefined

--- src/routes/User/RegisterResult.js
+++ src/routes/User/RegisterResult.js
@@ -15,7 +15,7 @@ export default ({ location }) => (
   <Result
     className={styles.registerResult}
     type="success"
-    title={<div className={styles.title}>你的账户：{location.query.account} 注册成功</div>}
+    title={<div className={styles.title}>你的账户：{location.query && location.query.account} 注册成功</div>}
     description="激活邮件已发送到你的邮箱中，邮件有效期为24小时。请及时登录邮箱，点击邮件中的链接激活帐户。"
     actions={actions}
     style={{ marginTop: 56 }}
