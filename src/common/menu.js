4893296e9903cd2b89883b9a36743df31647e979
jim <wasd2144@hotmail.com>
2018-01-16 09:47:08 +0800

    fix #697  when the menu path is url, the parent path is not spliced

--- src/common/menu.js
+++ src/common/menu.js
@@ -1,3 +1,5 @@
+import { isUrl } from '../utils/utils';
+
 const menuData = [{
   name: 'dashboard',
   icon: 'dashboard',
@@ -120,9 +122,13 @@ const menuData = [{
 
 function formatter(data, parentPath = '', parentAuthority) {
   return data.map((item) => {
+    let { path } = item;
+    if (!isUrl(path)) {
+      path = parentPath + item.path;
+    }
     const result = {
       ...item,
-      path: `${parentPath}${item.path}`,
+      path,
       authority: item.authority || parentAuthority,
     };
     if (item.children) {
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -132,3 +132,11 @@ export function getRoutes(path, routerData) {
   });
   return renderRoutes;
 }
+
+
+/* eslint no-useless-escape:0 */
+const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/g;
+
+export function isUrl(path) {
+  return reg.test(path);
+}
