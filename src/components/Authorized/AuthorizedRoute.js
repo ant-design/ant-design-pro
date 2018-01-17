df93c6490fb332aafbf9141c2778afad254dfbc9
jim <wasd2144@hotmail.com>
2018-01-17 08:31:11 +0800

    fix #716 Contact with dva / router coupling

--- src/components/Authorized/AuthorizedRoute.js
+++ src/components/Authorized/AuthorizedRoute.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Redirect } from 'dva/router';
+import { Route, Redirect } from 'react-router-dom';
 import Authorized from './Authorized';
 
 class AuthorizedRoute extends React.Component {
