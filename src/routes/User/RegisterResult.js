7e336e8d240d00675cabea9d817eb134d8ea933b
ddcat1115 <ddcat1115@gmail.com>
2018-01-18 00:59:31 +0800

    add default register email

--- src/routes/User/RegisterResult.js
+++ src/routes/User/RegisterResult.js
@@ -15,7 +15,11 @@ export default ({ location }) => (
   <Result
     className={styles.registerResult}
     type="success"
-    title={<div className={styles.title}>你的账户：{location.state && location.state.account} 注册成功</div>}
+    title={
+      <div className={styles.title}>
+        你的账户：{location.state ? location.state.account : 'AntDesign@example.com'} 注册成功
+      </div>
+    }
     description="激活邮件已发送到你的邮箱中，邮件有效期为24小时。请及时登录邮箱，点击邮件中的链接激活帐户。"
     actions={actions}
     style={{ marginTop: 56 }}
