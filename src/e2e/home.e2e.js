c507b7d325156722ffb87bcd1f13880ee81229c3
jwmann <me@jameswmann.com>
2018-01-24 12:46:13 -0500

    Add longer default timeout and pass done() for jasmine async

--- src/e2e/home.e2e.js
+++ src/e2e/home.e2e.js
@@ -1,9 +1,11 @@
 import Nightmare from 'nightmare';
 
 describe('Homepage', () => {
-  it('it should have logo text', async () => {
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 300000; // 5 minute timeout
+  it('it should have logo text', async (done) => {
     const page = Nightmare().goto('http://localhost:8000');
     const text = await page.wait('h1').evaluate(() => document.body.innerHTML).end();
     expect(text).toContain('<h1>Ant Design Pro</h1>');
+    done();
   });
 });
