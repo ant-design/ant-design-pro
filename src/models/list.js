4d60528a99c938118ddfe3181457e0326b8c89c3
valleykid <valleykiddy@gmail.com>
2018-02-02 11:53:31 +0800

    New page - info modal

--- src/models/list.js
+++ src/models/list.js
@@ -16,10 +16,10 @@ export default {
       });
     },
     *appendFetch({ payload }, { call, put }) {
-      const response = yield call(queryFakeList, payload);
+      const response = yield call(queryFakeList, { count: 5, ...payload }); // post
       yield put({
         type: 'appendList',
-        payload: Array.isArray(response) ? response : [],
+        payload: { list: response, updateItem: payload },
       });
     },
   },
@@ -31,11 +31,26 @@ export default {
         list: action.payload,
       };
     },
-    appendList(state, action) {
-      return {
-        ...state,
-        list: state.list.concat(action.payload),
-      };
+    appendList(state, { payload: { list: data, updateItem } }) {
+      /** ** 模拟入库开始 ** */
+      const list = data;
+      if (updateItem.id) {
+        list.some((item, i) => {
+          if (item.id === updateItem.id) {
+            if (Object.keys(updateItem).length === 1) { // delete
+              list.splice(i, 1);
+            } else {
+              list[i] = { ...item, ...updateItem }; // edit
+            }
+            return true;
+          }
+          return false;
+        });
+      } else {
+        list.unshift({ id: 'fake-list-x', ...updateItem }); // add
+      }
+      /** ** 模拟入库结束 ** */
+      return { ...state, list };
     },
   },
 };
