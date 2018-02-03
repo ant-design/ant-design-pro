ae5639ef593d062559e77ef7683c57358d0a48ac
jim <wasd2144@hotmail.com>
2018-02-03 15:52:54 +0800

    Modify the title style

--- src/routes/Userinfo/MessageView.js
+++ src/routes/Userinfo/MessageView.js
@@ -1,5 +1,8 @@
 import React, { Component, Fragment } from 'react';
+import { Switch } from 'antd';
 import MessageViewItem from './ItemView';
+
+const Action = <Switch defaultChecked />;
 
 export default class MessageView extends Component {
   render() {
@@ -8,17 +11,17 @@ export default class MessageView extends Component {
         <MessageViewItem
           title="消息通知"
           subTitle="其他用户的消息将以站内信的形式通知"
-          action="绑定"
+          action={Action}
         />
         <MessageViewItem
           title="系统消息"
           subTitle="系统消息将以站内信的形式通知"
-          action="绑定"
+          action={Action}
         />
         <MessageViewItem
           title="待办通知"
           subTitle="待办事项将以站内信的形式通知"
-          action="绑定"
+          action={Action}
         />
       </Fragment>
     );
