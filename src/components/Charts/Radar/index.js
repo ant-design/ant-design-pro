7018cdaf44f6cdc278559ac01cc65eed3038bf2c
jim <wasd2144@hotmail.com>
2018-02-06 08:58:01 +0800

    Remove extra divs

--- src/components/Charts/Radar/index.js
+++ src/components/Charts/Radar/index.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { Chart, Tooltip, Geom, Coord, Axis } from 'bizcharts';
 import { Row, Col } from 'antd';
 import autoHeight from '../autoHeight';
@@ -109,71 +109,69 @@ export default class Radar extends Component {
 
     return (
       <div className={styles.radar} style={{ height }}>
-        <Fragment>
-          {title && <h4>{title}</h4>}
-          <Chart
-            scale={scale}
-            height={chartHeight}
-            forceFit={forceFit}
-            data={data}
-            padding={padding}
-            animate={animate}
-            onGetG2Instance={this.getG2Instance}
-          >
-            <Tooltip />
-            <Coord type="polar" />
-            <Axis
-              name="label"
-              line={null}
-              tickLine={null}
-              grid={{
+        {title && <h4>{title}</h4>}
+        <Chart
+          scale={scale}
+          height={chartHeight}
+          forceFit={forceFit}
+          data={data}
+          padding={padding}
+          animate={animate}
+          onGetG2Instance={this.getG2Instance}
+        >
+          <Tooltip />
+          <Coord type="polar" />
+          <Axis
+            name="label"
+            line={null}
+            tickLine={null}
+            grid={{
                 lineStyle: {
                   lineDash: null,
                 },
                 hideFirstLine: false,
               }}
-            />
-            <Axis
-              name="value"
-              grid={{
+          />
+          <Axis
+            name="value"
+            grid={{
                 type: 'polygon',
                 lineStyle: {
                   lineDash: null,
                 },
               }}
-            />
-            <Geom type="line" position="label*value" color={['name', colors]} size={1} />
-            <Geom
-              type="point"
-              position="label*value"
-              color={['name', colors]}
-              shape="circle"
-              size={3}
-            />
-          </Chart>
-          {hasLegend && (
-            <Row className={styles.legend}>
-              {legendData.map((item, i) => (
-                <Col
-                  span={24 / legendData.length}
-                  key={item.name}
-                  onClick={() => this.handleLegendClick(item, i)}
-                >
-                  <div className={styles.legendItem}>
-                    <p>
-                      <span
-                        className={styles.dot}
-                        style={{ backgroundColor: !item.checked ? '#aaa' : item.color }}
-                      />
-                      <span>{item.name}</span>
-                    </p>
-                    <h6>{item.value}</h6>
-                  </div>
-                </Col>
+          />
+          <Geom type="line" position="label*value" color={['name', colors]} size={1} />
+          <Geom
+            type="point"
+            position="label*value"
+            color={['name', colors]}
+            shape="circle"
+            size={3}
+          />
+        </Chart>
+        {hasLegend && (
+        <Row className={styles.legend}>
+          {legendData.map((item, i) => (
+            <Col
+              span={24 / legendData.length}
+              key={item.name}
+              onClick={() => this.handleLegendClick(item, i)}
+            >
+              <div className={styles.legendItem}>
+                <p>
+                  <span
+                    className={styles.dot}
+                    style={{ backgroundColor: !item.checked ? '#aaa' : item.color }}
+                  />
+                  <span>{item.name}</span>
+                </p>
+                <h6>{item.value}</h6>
+              </div>
+            </Col>
               ))}
-            </Row>
+        </Row>
           )}
-        </Fragment>
       </div>
     );
   }
