aea14e5b743614216bf18e4ea965c073136809ae
拷钉 <41830859@qq.com>
2018-03-21 23:37:32 +0800

    PageHeaderLayout 支持栅格模式[Wide]
    PageHeaderLayout 支持栅格模式[Wide]

--- src/layouts/PageHeaderLayout.js
+++ src/layouts/PageHeaderLayout.js
@@ -7,7 +7,7 @@ import styles from './PageHeaderLayout.less';
 export default ({ children, wrapperClassName, top, ...restProps }) => (
   <div style={{ margin: '-24px -24px 0' }} className={wrapperClassName}>
     {top}
-    <PageHeader key="pageheader" {...restProps} linkElement={Link} />
+    <GridContent><PageHeader key="pageheader" {...restProps} linkElement={Link} /></GridContent>
     {children ? (
       <div className={styles.content}>
         <GridContent>{children}</GridContent>
