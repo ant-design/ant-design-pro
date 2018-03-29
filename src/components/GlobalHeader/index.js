f60ee6e755a453cf635a44bb38f01dca3fe86926
jim <wasd2144@hotmail.com>
2018-03-29 14:35:52 +0800

    use less var

--- src/components/GlobalHeader/index.js
+++ src/components/GlobalHeader/index.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { Icon, Divider } from 'antd';
 import { Link } from 'dva/router';
+import Debounce from 'lodash-decorators/debounce';
 import styles from './index.less';
 import RightContent from './RightContent';
 
