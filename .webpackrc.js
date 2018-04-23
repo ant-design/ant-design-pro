598107f041ad3e213afeb608920db1562aabfb7f
chencheng <sorrycc@gmail.com>
2018-04-23 12:29:35 +0800

    fix: babel transform failed with transform-decorators-legacy

--- .webpackrc.js
+++ .webpackrc.js
@@ -3,7 +3,6 @@ const path = require('path');
 export default {
   entry: 'src/index.js',
   extraBabelPlugins: [
-    'transform-decorators-legacy',
     ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
   ],
   env: {
