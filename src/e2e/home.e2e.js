e3b3a2c752db86972f02d854a9621a9c22dcb551
jim <wasd2144@hotmail.com>
2018-05-02 17:24:06 +0800

    fix ci

--- src/e2e/home.e2e.js
+++ src/e2e/home.e2e.js
@@ -4,7 +4,7 @@ describe('Homepage', () => {
   it('it should have logo text', async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://localhost:8000');
+    await page.goto('http://localhost:8000', { waitUntil: 'networkidle2' });
     await page.waitForSelector('h1');
     const text = await page.evaluate(() => document.body.innerHTML);
     expect(text).toContain('<h1>Ant Design Pro</h1>');
