9933e91dfcd0a1c4744d5b3e96a7010843fbbf73
Erwin Zhang <rwinzhang@gmail.com>
2018-05-07 23:00:10 +0700

    add component's name

--- src/routes/User/RegisterResult.js
+++ src/routes/User/RegisterResult.js
@@ -17,7 +17,7 @@ const actions = (
   </div>
 );
 
-export default ({ location }) => (
+const RegisterResult = ({ location }) => (
   <Result
     className={styles.registerResult}
     type="success"
@@ -31,3 +31,5 @@ export default ({ location }) => (
     style={{ marginTop: 56 }}
   />
 );
+
+export default RegisterResult;
