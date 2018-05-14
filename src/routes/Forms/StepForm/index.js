63fcc3d97950d3c0dd14ec4ee2f1f88028b31067
jim <wasd2144@hotmail.com>
2018-05-14 11:09:55 +0800

    Ugly fix  #1409

--- src/routes/Forms/StepForm/index.js
+++ src/routes/Forms/StepForm/index.js
@@ -25,10 +25,11 @@ export default class StepForm extends PureComponent {
     }
   }
   render() {
-    const { match, routerData } = this.props;
+    const { match, routerData, location } = this.props;
     return (
       <PageHeaderLayout
         title="分步表单"
+        tabActiveKey={location.pathname}
         content="将一个冗长或用户不熟悉的表单任务分成多个步骤，指导用户完成。"
       >
         <Card bordered={false}>
