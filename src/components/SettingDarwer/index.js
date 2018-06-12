e9799a3368f87e7f7ed8de705f6ab1e9303ac4d2
F-loat <945852046@qq.com>
2018-06-12 20:31:43 +0800

    fix: less modify method

--- src/components/SettingDarwer/index.js
+++ src/components/SettingDarwer/index.js
@@ -23,7 +23,9 @@ class SettingDarwer extends PureComponent {
   componentDidMount() {
     const { themeColor, colorWeak } = this.props.setting;
     if (themeColor !== '#1890FF') {
-      this.colorChange(themeColor);
+      window.less.refresh().then(() => {
+        this.colorChange(themeColor);
+      });
     }
     if (colorWeak === 'open') {
       document.body.className = 'colorWeak';
