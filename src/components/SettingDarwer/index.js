ff2fc1a81a48293050f9530232d00b59de4405e3
F-loat <945852046@qq.com>
2018-06-19 10:34:50 +0800

    fix: judgment condition of autoHideHeader state

--- src/components/SettingDarwer/index.js
+++ src/components/SettingDarwer/index.js
@@ -101,7 +101,7 @@ class SettingDarwer extends PureComponent {
       }
     }
     if (key === 'fixedHeader') {
-      if (value) {
+      if (!value) {
         nextState.autoHideHeader = false;
       }
     }
