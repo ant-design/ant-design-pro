70448f06b6c7234fc87aae5c750520ead8acaf8b
陈帅 <qixian.cs@outlook.com>
2018-06-22 18:58:20 +0800

    externals bizcharts dataset and rollbar

--- .webpackrc.js
+++ .webpackrc.js
@@ -2,13 +2,16 @@ const path = require('path');
 
 export default {
   entry: 'src/index.js',
-  extraBabelPlugins: [
-    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
-  ],
+  extraBabelPlugins: [['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
   env: {
     development: {
       extraBabelPlugins: ['dva-hmr'],
     },
+  },
+  externals: {
+    '@antv/data-set': 'DataSet',
+    bizcharts: 'BizCharts',
+    rollbar: 'rollbar',
   },
   alias: {
     components: path.resolve(__dirname, 'src/components/'),
