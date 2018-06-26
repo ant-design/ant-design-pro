fdd6035f94faaf7c826741c6c73c004ac306ae56
ubbcou <ubbcou@outlook.com>
2018-06-26 17:48:36 +0800

    Increase the permission judgment: when the user's permission is an array.

--- src/utils/authority.js
+++ src/utils/authority.js
@@ -1,5 +1,6 @@
 // use localStorage to store the authority info, which might be sent from server in actual project.
 export function getAuthority() {
+  // return localStorage.getItem('antd-pro-authority') || ['admin', 'user'];
   return localStorage.getItem('antd-pro-authority') || 'admin';
 }
 
