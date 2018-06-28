5d62c221b1f2956b762e662021fc0b0f544e0b66
陈帅 <qixian.cs@outlook.com>
2018-06-28 11:13:36 +0800

    add timeout

--- src/e2e/login.e2e.js
+++ src/e2e/login.e2e.js
@@ -17,6 +17,9 @@ describe('Login', () => {
   afterEach(() => page.close());
 
   it('should login with failure', async () => {
+    await page.waitForSelector('#userName', {
+      timeout: 2000,
+    });
     await page.type('#userName', 'mockuser');
     await page.type('#password', 'wrong_password');
     await page.click('button[type="submit"]');
@@ -24,6 +27,9 @@ describe('Login', () => {
   });
 
   it('should login successfully', async () => {
+    await page.waitForSelector('#userName', {
+      timeout: 2000,
+    });
     await page.type('#userName', 'admin');
     await page.type('#password', '888888');
     await page.click('button[type="submit"]');
