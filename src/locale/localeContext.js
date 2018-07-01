f07927f813f6b3fe6b0fb23dde99689498efd3dd
陈帅 <qixian.cs@outlook.com>
2018-07-01 20:06:13 +0800

    Increase dynamic switching local

---
+++ src/locale/localeContext.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { addLocaleData, IntlProvider } from 'react-intl';
+import { LocaleProvider } from 'antd';
+import enLocale from './en-US';
+import cnLocale from './zh-CN';
+
+const Context = React.createContext();
+
+function getLang() {
+  if (window.localStorage && localStorage.getItem('locale')) {
+    return localStorage.getItem('locale');
+  }
+  return (navigator.language || navigator.browserLanguage).toLowerCase() === 'en-us'
+    ? 'en-US'
+    : 'zh-CN';
+}
+
+export class LocalComponent extends React.PureComponent {
+  state = {
+    locale: getLang(),
+  };
+
+  changeLocal = () => {
+    this.setState({
+      locale: getLang(),
+    });
+  };
+
+  render() {
+    const { children } = this.props;
+    const { locale } = this.state;
+    return (
+      <Context.Provider
+        value={{
+          appLocale: locale === 'zh-CN' ? cnLocale : enLocale,
+          changeLocal: this.changeLocal,
+        }}
+      >
+        <Context.Consumer>
+          {({ appLocale }) => {
+            addLocaleData(appLocale.data);
+            return (
+              <IntlProvider locale={appLocale.locale} messages={appLocale.messages}>
+                <LocaleProvider locale={appLocale.antd}>{children}</LocaleProvider>
+              </IntlProvider>
+            );
+          }}
+        </Context.Consumer>
+      </Context.Provider>
+    );
+  }
+}
+export default Context;
--- src/router.js
+++ src/router.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { routerRedux, Route, Switch } from 'dva/router';
-import { LocaleProvider, Spin } from 'antd';
+import { Spin } from 'antd';
 import dynamic from 'dva/dynamic';
-import { addLocaleData, IntlProvider } from 'react-intl';
+import { LocalComponent } from './locale/localeContext';
 import { getRouterData } from './common/router';
 import Authorized from './utils/Authorized';
 import { getQueryPath } from './utils/utils';
 import styles from './index.less';
-import enLocale from './locale/en-US';
-import cnLocale from './locale/zh-CN';
 
 const { ConnectedRouter } = routerRedux;
 const { AuthorizedRoute } = Authorized;
@@ -17,37 +15,25 @@ dynamic.setDefaultLoadingComponent(() => {
   return <Spin size="large" className={styles.globalSpin} />;
 });
 
-function getLang() {
-  if (window.localStorage && localStorage.getItem('locale')) {
-    return localStorage.getItem('locale');
-  }
-  return (navigator.language || navigator.browserLanguage).toLowerCase() === 'en-us'
-    ? 'en-US'
-    : 'zh-CN';
-}
-
 function RouterConfig({ history, app }) {
   const routerData = getRouterData(app);
   const UserLayout = routerData['/user'].component;
   const BasicLayout = routerData['/'].component;
-  const appLocale = getLang() === 'zh-CN' ? cnLocale : enLocale;
-  addLocaleData(appLocale.data);
+
   return (
-    <IntlProvider locale={appLocale.locale} messages={appLocale.messages}>
-      <LocaleProvider locale={appLocale.antd}>
-        <ConnectedRouter history={history}>
-          <Switch>
-            <Route path="/user" component={UserLayout} />
-            <AuthorizedRoute
-              path="/"
-              render={props => <BasicLayout {...props} />}
-              authority={['admin', 'user']}
-              redirectPath="/user/login"
-            />
-          </Switch>
-        </ConnectedRouter>
-      </LocaleProvider>
-    </IntlProvider>
+    <LocalComponent>
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route path="/user" component={UserLayout} />
+          <AuthorizedRoute
+            path="/"
+            render={props => <BasicLayout {...props} />}
+            authority={['admin', 'user']}
+            redirectPath="/user/login"
+          />
+        </Switch>
+      </ConnectedRouter>
+    </LocalComponent>
   );
 }
 
