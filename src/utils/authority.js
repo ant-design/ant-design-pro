e4361fa552b7a3cf55a3151c4d87b4769a4a5a7f
rongxingsun <rongxing_sun@qq.com>
2018-07-03 15:20:11 +0800

    JSON
    JSON

--- src/utils/authority.js
+++ src/utils/authority.js
@@ -1,10 +1,9 @@
-import { parse, stringify } from 'qs';
-
 // use localStorage to store the authority info, which might be sent from server in actual project.
 export function getAuthority() {
-  return parse(localStorage.getItem('antd-pro-authority')) || 'admin';
+  const authority = localStorage.getItem('antd-pro-authority');
+  return authority ? JSON.parse(authority) : ['admin', 'user'];
 }
 
 export function setAuthority(authority) {
-  return localStorage.setItem('antd-pro-authority', stringify(authority));
+  return localStorage.setItem('antd-pro-authority', JSON.stringify(authority));
 }
