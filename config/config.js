fc442755f9b6fb28b6f429bab6bc8b1c3c34290c
愚道 <tingzhao.ytz@antfin.com>
2018-07-03 20:45:34 +0800

    rename index.ejs to document.ejs

--- config/config.js
+++ config/config.js
@@ -36,9 +36,9 @@ export default {
   },
   ignoreMomentLocale: true,
   theme: './src/theme.js',
-  html: {
-    template: './src/index.ejs',
-  },
+  // html: { TODO remove
+  //   template: './src/index.ejs',
+  // },
   publicPath: '/',
   // TODO check hash config
   // hash: true,
