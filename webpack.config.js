a44b833bc58942fa326283d36db1ec49c2eac48d
陈帅 <qixian.cs@outlook.com>
2018-07-03 16:00:24 +0800

    chage temp file path

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 export default webpackConfig => {
   // 将所有 less 合并为一个供 themePlugin使用
-  const outFile = path.join(__dirname, './temp/ant-design-pro.less');
+  const outFile = path.join(__dirname, './.temp/ant-design-pro.less');
   const stylesDir = path.join(__dirname, './src/');
 
   const mergeLessPlugin = new MergeLessPlugin({
