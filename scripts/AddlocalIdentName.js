dde8426900a7e1481d93348a37ee66f0f827abc2
陈帅 <qixian.cs@outlook.com>
2018-07-03 15:30:50 +0800

    fix theme change

---
+++ scripts/AddlocalIdentName.js
@@ -0,0 +1,47 @@
+/* eslint-disable */
+const postcss = require('postcss');
+const syntax = require('postcss-less');
+
+const LocalIdentNameplugin = postcss.plugin('LocalIdentNameplugin', ({ localIdentName }) => {
+  return lessAST => {
+    // loop add localIdentName
+    const loop = nodes => {
+      nodes.forEach(item => {
+        // Not converted :global
+        if (item.nodes && item.selector !== ':global') {
+          loop(item.nodes);
+        }
+        // 将global的 节点加到 parents
+        if (item.selector === ':global') {
+          const parentNodes = item.parent.nodes;
+          const childrenNodes = item.nodes;
+          const index = parentNodes.findIndex(node => {
+            return node.selector === ':global';
+          });
+          childrenNodes.unshift(index, 1);
+          Array.prototype.splice.apply(parentNodes, item.nodes);
+          item.parent.nodes = parentNodes;
+          return;
+        }
+        // 删除 :global(className) 保留 className
+        if (item.selector && !item.selector.includes('(')) {
+          if (item.selector.includes(':global(')) {
+            // converted :global(.className）
+            const className = item.selector.match(/:global\((\S*)\)/)[1];
+            item.selector = className;
+            return;
+          }
+          const className = item.selector.replace(/\./g, `.${localIdentName}`);
+          item.selector = className;
+        }
+      });
+    };
+    loop(lessAST.nodes);
+  };
+});
+
+const AddlocalIdentName = (lessPath, lessText, localIdentName) => {
+  return postcss([LocalIdentNameplugin({ localIdentName })]).process(lessText, { syntax });
+};
+
+module.exports = AddlocalIdentName;
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
 import AntDesignThemePlugin from 'antd-theme-webpack-plugin';
+import MergeLessPlugin from './scripts/mergeLessPlugin';
 
 const path = require('path');
 
 export default webpackConfig => {
+  // 将所有 less 合并为一个供 themePlugin使用
+  const outFile = path.join(__dirname, './temp/ant-design-pro.less');
+  const stylesDir = path.join(__dirname, './src/');
+
+  const mergeLessPlugin = new MergeLessPlugin({
+    stylesDir,
+    outFile,
+  });
+
   const options = {
     antDir: path.join(__dirname, './node_modules/antd'),
-    stylesDir: path.join(__dirname, './src/'),
+    stylesDir,
     varFile: path.join(__dirname, './node_modules/antd/lib/style/themes/default.less'),
-    mainLessFile: path.join(__dirname, './src/index.less'),
+    mainLessFile: outFile,
     themeVariables: ['@primary-color'],
     indexFileName: 'index.html',
   };
-
   const themePlugin = new AntDesignThemePlugin(options);
+
   // in config object
+  webpackConfig.plugins.push(mergeLessPlugin);
   webpackConfig.plugins.push(themePlugin);
+
   return webpackConfig;
 };
