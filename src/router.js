043677735668e8b9d95c4060d487d816fec63bb1
Ethan_Wan <wanshiyi@hyperchain.cn>
2018-07-04 10:35:25 +0800

    feat: instead of dva/dynamic using react-loadable

--- src/router.js
+++ src/router.js
@@ -1,39 +1,30 @@
 import React from 'react';
 import { routerRedux, Route, Switch } from 'dva/router';
-import { Spin } from 'antd';
-import dynamic from 'dva/dynamic';
 import { getRouterData } from './common/router';
 import Authorized from './utils/Authorized';
 import { getQueryPath } from './utils/utils';
-import { LocalComponent } from './locale/localeContext';
-import styles from './index.less';
 
 const { ConnectedRouter } = routerRedux;
 const { AuthorizedRoute } = Authorized;
-dynamic.setDefaultLoadingComponent(() => {
-  return <Spin size="large" className={styles.globalSpin} />;
-});
 
 function RouterConfig({ history, app }) {
   const routerData = getRouterData(app);
   const UserLayout = routerData['/user'].component;
   const BasicLayout = routerData['/'].component;
   return (
-    <LocalComponent>
-      <ConnectedRouter history={history}>
-        <Switch>
-          <Route path="/user" component={UserLayout} />
-          <AuthorizedRoute
-            path="/"
-            render={props => <BasicLayout {...props} />}
-            authority={['admin', 'user']}
-            redirectPath={getQueryPath('/user/login', {
-              redirect: window.location.href,
-            })}
-          />
-        </Switch>
-      </ConnectedRouter>
-    </LocalComponent>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/user" component={UserLayout} />
+        <AuthorizedRoute
+          path="/"
+          render={props => <BasicLayout {...props} />}
+          authority={['admin', 'user']}
+          redirectPath={getQueryPath('/user/login', {
+            redirect: window.location.href,
+          })}
+        />
+      </Switch>
+    </ConnectedRouter>
   );
 }
 
