950206ad37f3f97c0b79f558062ea417fb7fb98d
rongxingsun <rongxing_sun@qq.com>
2018-07-06 16:58:07 +0800

    localStorage保存数据问题
    localStorage只能保存字符格式，要用JSON转换一下保存，    后台读取菜单信息

--- src/common/router.js
+++ src/common/router.js
@@ -67,36 +67,6 @@ export function getFlatMenuData(menus) {
   return keys;
 }
 
-export const getRouterData = (routerConfig, menuData) => {
-  // Route configuration data
-  // eg. {name,authority ...routerConfig }
-  const routerData = {};
-  // The route matches the menu
-  Object.keys(routerConfig).forEach(path => {
-    // Regular match item name
-    // eg.  router /user/:id === /user/chen
-    const pathRegexp = pathToRegexp(path);
-    const menuKey = Object.keys(menuData).find(key => pathRegexp.test(`${key}`));
-    let menuItem = {};
-    // If menuKey is not empty
-    if (menuKey) {
-      menuItem = menuData[menuKey];
-    }
-    let router = routerConfig[path];
-    // If you need to configure complex parameter routing,
-    // https://github.com/ant-design/ant-design-pro-site/blob/master/docs/router-and-nav.md#%E5%B8%A6%E5%8F%82%E6%95%B0%E7%9A%84%E8%B7%AF%E7%94%B1%E8%8F%9C%E5%8D%95
-    // eg . /list/:type/user/info/:id
-    router = {
-      ...router,
-      name: router.name || menuItem.name,
-      authority: concat([], router.authority || [], menuItem.authority || []),
-      hideInBreadcrumb: router.hideInBreadcrumb || menuItem.hideInBreadcrumb,
-    };
-    routerData[path] = router;
-  });
-  return routerData;
-};
-
 export const getRouterConfig = app => {
   const routerConfig = {
     '/': {
@@ -250,4 +220,34 @@ export const getRouterConfig = app => {
     payload: routerConfig,
   });
   return routerConfig;
+};
+
+export const getRouterData = (routerConfig, menuData) => {
+  // Route configuration data
+  // eg. {name,authority ...routerConfig }
+  const routerData = {};
+  // The route matches the menu
+  Object.keys(routerConfig).forEach(path => {
+    // Regular match item name
+    // eg.  router /user/:id === /user/chen
+    const pathRegexp = pathToRegexp(path);
+    const menuKey = Object.keys(menuData).find(key => pathRegexp.test(`${key}`));
+    let menuItem = {};
+    // If menuKey is not empty
+    if (menuKey) {
+      menuItem = menuData[menuKey];
+    }
+    let router = routerConfig[path];
+    // If you need to configure complex parameter routing,
+    // https://github.com/ant-design/ant-design-pro-site/blob/master/docs/router-and-nav.md#%E5%B8%A6%E5%8F%82%E6%95%B0%E7%9A%84%E8%B7%AF%E7%94%B1%E8%8F%9C%E5%8D%95
+    // eg . /list/:type/user/info/:id
+    router = {
+      ...router,
+      name: router.name || menuItem.name,
+      authority: concat([], router.authority || [], menuItem.authority || []),
+      hideInBreadcrumb: router.hideInBreadcrumb || menuItem.hideInBreadcrumb,
+    };
+    routerData[path] = router;
+  });
+  return routerData;
 };
