c22b53d2b588ba95bfac824302020b6b7488ffdb
yoyo837 <yoyo837@hotmail.com>
2018-07-10 14:11:48 +0800

    MarginBar for ant-design/ant-design-pro#1799

---
+++ src/components/MarginBar/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default class MarginBar extends React.PureComponent {
+  defVal = '8px';
+
+  render() {
+    const { children, left, top, right, bottom, inline } = this.props;
+    const style = {};
+    if (inline) {
+      style.display = 'inline-block';
+    }
+    if (left != null) {
+      style.marginLeft = left === true ? this.defVal : left;
+    }
+    if (top != null) {
+      style.marginTop = top === true ? this.defVal : top;
+    }
+    if (right != null) {
+      style.marginRight = right === true ? this.defVal : right;
+    }
+    if (bottom != null) {
+      style.marginBottom = bottom === true ? this.defVal : bottom;
+    }
+    return <div style={style}>{children}</div>;
+  }
+}
