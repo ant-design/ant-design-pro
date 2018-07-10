14638df929856273b98f798464a14586a815d851
yoyo837 <yoyo837@hotmail.com>
2018-07-10 14:55:18 +0800

    simplify api

--- src/components/MarginBar/index.js
+++ src/components/MarginBar/index.js
@@ -5,23 +5,7 @@ export default class MarginBar extends React.PureComponent {
   defVal = '8px';
 
   render() {
-    const { children, left, top, right, bottom, inline } = this.props;
-    const style = {};
-    if (inline) {
-      style.display = 'inline-block';
-    }
-    if (left != null) {
-      style.marginLeft = left === true ? this.defVal : left;
-    }
-    if (top != null) {
-      style.marginTop = top === true ? this.defVal : top;
-    }
-    if (right != null) {
-      style.marginRight = right === true ? this.defVal : right;
-    }
-    if (bottom != null) {
-      style.marginBottom = bottom === true ? this.defVal : bottom;
-    }
-    return <div className={style.bar} style={style}>{children}</div>;
+    const { children } = this.props;
+    return <div className={style.bar}>{children}</div>;
   }
 }
