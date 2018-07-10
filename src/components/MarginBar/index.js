9189e64b2444c1f7f5cb156c66afece88bca9915
yoyo837 <yoyo837@hotmail.com>
2018-07-10 14:56:17 +0800

    remove unused

--- src/components/MarginBar/index.js
+++ src/components/MarginBar/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import style from './index.less'
 
 export default class MarginBar extends React.PureComponent {
-  defVal = '8px';
-
   render() {
     const { children } = this.props;
     return <div className={style.bar}>{children}</div>;
