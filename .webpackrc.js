75e4ac6e080cc02a397b8213ad6455f4ec29c0de
陈帅 <qixian.cs@outlook.com>
2018-07-15 20:09:40 +0800

    Optimization: delete externals

---
+++ .webpackrc.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+export default {
+  entry: 'src/index.js',
+  extraBabelPlugins: [['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
+  env: {
+    development: {
+      extraBabelPlugins: ['dva-hmr'],
+    },
+  },
+  externals: {
+    '@antv/data-set': 'DataSet',
+    rollbar: 'rollbar',
+  },
+  alias: {
+    components: path.resolve(__dirname, 'src/components/'),
+  },
+  ignoreMomentLocale: true,
+  theme: './src/theme.js',
+  html: {
+    template: './src/index.ejs',
+  },
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+  },
+  disableDynamicImport: true,
+  publicPath: '/',
+  hash: true,
+};
