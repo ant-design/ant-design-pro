c936a6e7de444d5e60bf5b1aedf68e302943bb55
张秀玲 <zhangxiuling@zhangxiulingdeMacBook-Pro.local>
2018-07-20 15:29:13 +0800

    translate success page

--- src/pages/Result/Success.js
+++ src/pages/Result/Success.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { formatMessage, FormattedMessage } from 'umi/locale';
 import { Button, Row, Col, Icon, Steps, Card } from 'antd';
 import Result from 'components/Result';
 import PageHeaderLayout from '../layouts/PageHeaderLayout';
@@ -15,7 +16,7 @@ const desc1 = (
     }}
   >
     <div style={{ margin: '8px 0 4px' }}>
-      曲丽丽
+      <FormattedMessage id="app.result.success.step1-operator" defaultMessage="Qu Lili" />
       <Icon style={{ marginLeft: 8 }} type="dingding-o" />
     </div>
     <div>2016-12-12 12:32</div>
@@ -25,11 +26,13 @@ const desc1 = (
 const desc2 = (
   <div style={{ fontSize: 12, position: 'relative', left: 42 }}>
     <div style={{ margin: '8px 0 4px' }}>
-      周毛毛
+      <FormattedMessage id="app.result.success.step2-operator" defaultMessage="Zhou Maomao" />
       <Icon type="dingding-o" style={{ color: '#00A0E9', marginLeft: 8 }} />
     </div>
     <div>
-      <a href="">催一下</a>
+      <a href="">
+        <FormattedMessage id="app.result.success.step2-extra" defaultMessage="Urge" />
+      </a>
     </div>
   </div>
 );
@@ -44,36 +47,83 @@ const extra = (
         marginBottom: 20,
       }}
     >
-      项目名称
+      <FormattedMessage id="app.result.success.operate-title" defaultMessage="Project Name" />
     </div>
     <Row style={{ marginBottom: 16 }}>
       <Col xs={24} sm={12} md={12} lg={12} xl={6}>
-        <span style={{ color: 'rgba(0, 0, 0, 0.85)' }}>项目 ID：</span>
+        <span style={{ color: 'rgba(0, 0, 0, 0.85)' }}>
+          <FormattedMessage id="app.result.success.operate-id" defaultMessage="Project ID：" />
+        </span>
         23421
       </Col>
       <Col xs={24} sm={12} md={12} lg={12} xl={6}>
-        <span style={{ color: 'rgba(0, 0, 0, 0.85)' }}>负责人：</span>
-        曲丽丽
+        <span style={{ color: 'rgba(0, 0, 0, 0.85)' }}>
+          <FormattedMessage id="app.result.success.principal" defaultMessage="Principal：" />
+        </span>
+        <FormattedMessage id="app.result.success.step1-operator" defaultMessage="Qu Lili" />
       </Col>
       <Col xs={24} sm={24} md={24} lg={24} xl={12}>
-        <span style={{ color: 'rgba(0, 0, 0, 0.85)' }}>生效时间：</span>
+        <span style={{ color: 'rgba(0, 0, 0, 0.85)' }}>
+          <FormattedMessage
+            id="app.result.success.operate-time"
+            defaultMessage="Effective time："
+          />
+        </span>
         2016-12-12 ~ 2017-12-12
       </Col>
     </Row>
     <Steps style={{ marginLeft: -42, width: 'calc(100% + 84px)' }} progressDot current={1}>
-      <Step title={<span style={{ fontSize: 14 }}>创建项目</span>} description={desc1} />
-      <Step title={<span style={{ fontSize: 14 }}>部门初审</span>} description={desc2} />
-      <Step title={<span style={{ fontSize: 14 }}>财务复核</span>} />
-      <Step title={<span style={{ fontSize: 14 }}>完成</span>} />
+      <Step
+        title={
+          <span style={{ fontSize: 14 }}>
+            <FormattedMessage id="app.result.success.step1-title" defaultMessage="Create project" />
+          </span>
+        }
+        description={desc1}
+      />
+      <Step
+        title={
+          <span style={{ fontSize: 14 }}>
+            <FormattedMessage
+              id="app.result.success.step2-title"
+              defaultMessage="Departmental preliminary review"
+            />
+          </span>
+        }
+        description={desc2}
+      />
+      <Step
+        title={
+          <span style={{ fontSize: 14 }}>
+            <FormattedMessage
+              id="app.result.success.step3-title"
+              defaultMessage="Financial review"
+            />
+          </span>
+        }
+      />
+      <Step
+        title={
+          <span style={{ fontSize: 14 }}>
+            <FormattedMessage id="app.result.success.step4-title" defaultMessage="Finish" />
+          </span>
+        }
+      />
     </Steps>
   </Fragment>
 );
 
 const actions = (
   <Fragment>
-    <Button type="primary">返回列表</Button>
-    <Button>查看项目</Button>
-    <Button>打 印</Button>
+    <Button type="primary">
+      <FormattedMessage id="app.result.success.btn-return" defaultMessage="Back to list" />
+    </Button>
+    <Button>
+      <FormattedMessage id="app.result.success.btn-project" defaultMessage="View project" />
+    </Button>
+    <Button>
+      <FormattedMessage id="app.result.success.btn-print" defaultMessage="Print" />
+    </Button>
   </Fragment>
 );
 
@@ -82,11 +132,8 @@ export default () => (
     <Card bordered={false}>
       <Result
         type="success"
-        title="提交成功"
-        description="提交结果页用于反馈一系列操作任务的处理结果，
-        如果仅是简单操作，使用 Message 全局提示反馈即可。
-        本文字区域可以展示简单的补充说明，如果有类似展示
-        “单据”的需求，下面这个灰色区域可以呈现比较复杂的内容。"
+        title={formatMessage({ id: 'app.result.success.title' }, {})}
+        description={formatMessage({ id: 'app.result.success.description' }, {})}
         extra={extra}
         actions={actions}
         style={{ marginTop: 48, marginBottom: 16 }}
