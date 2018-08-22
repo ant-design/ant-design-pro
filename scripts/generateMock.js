e37787a2241953631c910723b2bb461494c4fe10
陈帅 <qixian.cs@outlook.com>
2018-08-22 13:48:50 +0800

    upgrade merge-umi-mock-data version

--- scripts/generateMock.js
+++ scripts/generateMock.js
@@ -1,4 +1,3 @@
 const generateMock = require('merge-umi-mock-data');
 const path = require('path');
-console.log(path.join(__dirname, '../mock'), path.join(__dirname, '../functions/mock/index.js'));
 generateMock(path.join(__dirname, '../mock'), path.join(__dirname, '../functions/mock/index.js'));
