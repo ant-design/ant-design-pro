ff08655f9a40af39da19524864162a35297ef791
afc163 <afc163@gmail.com>
2018-08-31 11:53:05 +0800

    check every matched route for authority

--- src/pages/Authorized.js
+++ src/pages/Authorized.js
@@ -2,16 +2,23 @@ import React from 'react';
 import RenderAuthorized from '@/components/Authorized';
 import Exception from '@/components/Exception';
 import { matchRoutes } from 'react-router-config';
+import uniq from 'lodash/uniq';
 
 const Authorized = RenderAuthorized(['admin', 'user']);
 const noMatch = <Exception type="403" style={{ minHeight: 500, height: '80%' }} />;
 
 export default ({ children, route, location }) => {
-  const branch =
-    matchRoutes(route.routes, location.pathname).filter(item => item.match.isExact)[0] || {};
-  const { authority } = branch.route || {};
+  const routes = matchRoutes(route.routes, location.pathname);
+  let authorities = [];
+  routes.forEach(item => {
+    if (Array.isArray(item.authority)) {
+      authorities = authorities.concat(item.authority);
+    } else if (typeof item.authority === 'string') {
+      authorities.push(item.authority);
+    }
+  });
   return (
-    <Authorized authority={authority} noMatch={noMatch}>
+    <Authorized authority={uniq(authorities)} noMatch={noMatch}>
       {children}
     </Authorized>
   );
