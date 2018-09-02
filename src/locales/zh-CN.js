d2eaa1035687d96f3ca6ccf0c5225a0426286e91
pkuphy <xiaowangzi61@gmail.com>
2018-09-02 19:02:33 +0200

    fix typo

--- src/locales/zh-CN.js
+++ src/locales/zh-CN.js
@@ -9,7 +9,7 @@ export default {
   'menu.dashboard.workplace': '工作台',
   'menu.form': '表单页',
   'menu.form.basicform': '基础表单',
-  'menu.form.stepform': '分布表单',
+  'menu.form.stepform': '分步表单',
   'menu.form.stepform.info': '分步表单（填写转账信息）',
   'menu.form.stepform.confirm': '分步表单（确认转账信息）',
   'menu.form.stepform.result': '分步表单（完成）',
