10d0a662ecaae8ca44ba28a33c90c17db3483c5e
qj <156257964@qq.com>
2018-09-30 12:46:59 +0800

    修复顶部清空通知/消息/待办后提示国际化不完整

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -43,7 +43,7 @@ class HeaderView extends PureComponent {
   };
 
   handleNoticeClear = type => {
-    message.success(`${formatMessage({ id: 'component.noticeIcon.cleared' })} ${type}`);
+    message.success(`${formatMessage({ id: 'component.noticeIcon.cleared' })} ${formatMessage({ id: `component.globalHeader.${type}` })}`);
     const { dispatch } = this.props;
     dispatch({
       type: 'global/clearNotices',
