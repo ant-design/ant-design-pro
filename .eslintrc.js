ae89994b3b57dccad557999c94d5f4ce9e153190
X2O <57MRx2e4jaQT13wQ@gmail.com>
2018-10-06 03:52:53 +0800

    fix: (#2475, #2445) input box-shadow for login page

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-static-element-interactions': 0,
     'jsx-a11y/anchor-is-valid': 0,
+    'linebreak-style': 0,
   },
   settings: {
     polyfills: ['fetch', 'promises', 'url'],
