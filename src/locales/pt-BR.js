e0882a7c55f70e72bde07d5634a7e952e23353e5
michael <156257964@qq.com>
2018-10-09 19:17:54 +0800

    Internationalization Login error message(#2507)

--- src/locales/pt-BR.js
+++ src/locales/pt-BR.js
@@ -75,6 +75,8 @@ export default {
   'menu.account.settings': 'Configurar Conta',
   'menu.account.trigger': 'Disparar Erro',
   'menu.account.logout': 'Sair',
+  'app.login.message-invalid-credentials': 'Nome de usuário ou senha inválidosd（admin/888888）',
+  'app.login.message-invalid-verification-code': 'Código de verificação inválido',
   'app.login.tab-login-credentials': 'Credenciais',
   'app.login.tab-login-mobile': 'Telefone',
   'app.login.remember-me': 'Lembre-me',
