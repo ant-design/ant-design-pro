99e74c7465e3e9388050ab69c99b03364c082e09
Jing Ma <mjingm87@qq.com>
2018-10-16 19:21:40 +0800

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Link from 'umi/link';
+import { formatMessage } from "umi/locale";
 import Exception from '@/components/Exception';
 
 export default () => (
-  <Exception type="404" style={{ minHeight: 500, height: '100%' }} linkElement={Link} />
+  <Exception
+    type="404"
+    linkElement={Link}
+    desc={formatMessage({id: 'app.exception.description.404'})}
+    backText={formatMessage({id: 'app.exception.back'})}
+  />
 );
