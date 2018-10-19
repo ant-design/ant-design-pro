2d0eb8b8aaf944f33ebaa2baf2f223d1f2406bb5
Rayron Victor <rayronvictor@gmail.com>
2018-10-19 09:30:46 -0300

    SelectLang: languages options don't need to be translated

--- src/locales/en-US.js
+++ src/locales/en-US.js
@@ -1,9 +1,5 @@
 export default {
   'navBar.lang': 'Languages',
-  'lang.simplified-chinese': '🇨🇳 简体中文',
-  'lang.traditional-chinese': '🇭🇰 繁体中文',
-  'lang.english': '🇬🇧 English',
-  'lang.portuguese': '🇵🇹 Portuguese',
   'layout.user.link.help': 'Help',
   'layout.user.link.privacy': 'Privacy',
   'layout.user.link.terms': 'Terms',
