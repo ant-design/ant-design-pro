cf2254fd97afdf4495408e152eee0b9b0e909fab
Rayron Victor <rayronvictor@gmail.com>
2018-10-19 09:30:46 -0300

    SelectLang: languages options don't need to be translated

--- src/components/SelectLang/index.js
+++ src/components/SelectLang/index.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { formatMessage, FormattedMessage, setLocale, getLocale } from 'umi/locale';
+import { formatMessage, setLocale, getLocale } from 'umi/locale';
 import { Menu, Icon, Dropdown } from 'antd';
 import classNames from 'classnames';
 import styles from './index.less';
@@ -15,16 +15,28 @@ export default class SelectLang extends PureComponent {
     const langMenu = (
       <Menu className={styles.menu} selectedKeys={[selectedLang]} onClick={this.changLang}>
         <Menu.Item key="zh-CN">
-          <FormattedMessage id="lang.simplified-chinese" />
+          <span role="img" aria-label="简体中文">
+            🇨🇳
+          </span>{' '}
+          简体中文
         </Menu.Item>
         <Menu.Item key="zh-TW">
-          <FormattedMessage id="lang.traditional-chinese" />
+          <span role="img" aria-label="繁体中文">
+            🇭🇰
+          </span>{' '}
+          繁体中文
         </Menu.Item>
         <Menu.Item key="en-US">
-          <FormattedMessage id="lang.english" />
+          <span role="img" aria-label="English">
+            🇬🇧
+          </span>{' '}
+          English
         </Menu.Item>
         <Menu.Item key="pt-BR">
-          <FormattedMessage id="lang.portuguese" />
+          <span role="img" aria-label="Português">
+            🇵🇹
+          </span>{' '}
+          Português
         </Menu.Item>
       </Menu>
     );
