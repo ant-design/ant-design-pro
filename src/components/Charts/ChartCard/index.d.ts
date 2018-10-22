deb14330ad5e5ba86df404fdb63021866ea93d4a
陈帅 <qixian.cs@outlook.com>
2018-10-22 18:28:36 +0800

    support use typescript

--- src/components/Charts/ChartCard/index.d.ts
+++ src/components/Charts/ChartCard/index.d.ts
@@ -1,5 +1,5 @@
-import * as React from 'react';
 import { CardProps } from 'antd/lib/card';
+import * as React from 'react';
 
 export interface IChartCardProps extends CardProps {
   title: React.ReactNode;
