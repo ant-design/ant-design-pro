6073d7809764b863d32f8f0b1e51513e45138332
卖代码的 <515343908@qq.com>
2018-10-31 11:16:48 +0800

    更新提示密码

--- src/locales/zh-CN.js
+++ src/locales/zh-CN.js
@@ -102,7 +102,7 @@ export default {
   'menu.account.settings': '个人设置',
   'menu.account.trigger': '触发报错',
   'menu.account.logout': '退出登录',
-  'app.login.message-invalid-credentials': '账户或密码错误（admin/888888）',
+  'app.login.message-invalid-credentials': '账户或密码错误（admin/ant.design）',
   'app.login.message-invalid-verification-code': '验证码错误',
   'app.login.tab-login-credentials': '账户密码登录',
   'app.login.tab-login-mobile': '手机号登录',
