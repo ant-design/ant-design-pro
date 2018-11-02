67fccc35293a4b88bd1deab5e8317fb1bf4e4a73
afc163 <afc163@gmail.com>
2018-11-02 11:31:21 +0800

    Add comment

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -178,6 +178,7 @@ export function formatWan(val) {
   return result;
 }
 
+// 给官方演示站点用，用于关闭真实开发环境不需要使用的特性
 export function isAntdPro() {
   return window.location.hostname === 'preview.pro.ant.design';
 }
