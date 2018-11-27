b9aff6b2dfddc0e7435813c1a4f2485bd6ecbc6c
张硕 <zsio@qq.com>
2018-11-27 13:53:33 +0800

    更改api地址

--- config/config.js
+++ config/config.js
@@ -72,13 +72,17 @@ export default {
   externals: {
     '@antv/data-set': 'DataSet',
   },
-  // proxy: {
-  //   '/server/api/': {
-  //     target: 'https://preview.pro.ant.design/',
-  //     changeOrigin: true,
-  //     pathRewrite: { '^/server': '' },
-  //   },
-  // },
+  proxy: {
+    '/backstage/': {
+      target: 'http://farseer.pikaent.com/',
+      changeOrigin: true,
+      pathRewrite: {
+        /*
+        '^/backstage': 'backstage',
+        */
+      },
+    },
+  },
   ignoreMomentLocale: true,
   lessLoaderOptions: {
     javascriptEnabled: true,
