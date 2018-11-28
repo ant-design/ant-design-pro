8371a5ce7c42a82f9974634d451c8ff6f0db8cde
xiaohuoni <448627663@qq.com>
2018-11-28 15:37:04 +0800

    当访问的路由都不在菜单内时，直接过滤掉会有其他问题

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -163,14 +163,6 @@ class BasicLayout extends React.PureComponent {
     return <SettingDrawer />;
   };
 
-  getPageAuthority = (pathname, breadcrumbNameMap) => {
-    const routerConfig = this.matchParamsPath(pathname, breadcrumbNameMap);
-    if (pathname === '/') {
-      return null;
-    }
-    return routerConfig ? routerConfig.authority : ['noAuthority'];
-  };
-
   render() {
     const {
       navTheme,
@@ -183,7 +175,7 @@ class BasicLayout extends React.PureComponent {
     } = this.props;
 
     const isTop = PropsLayout === 'topmenu';
-
+    const routerConfig = this.matchParamsPath(pathname, breadcrumbNameMap);
     const layout = (
       <Layout>
         {isTop && !isMobile ? null : (
@@ -211,7 +203,7 @@ class BasicLayout extends React.PureComponent {
           />
           <Content style={this.getContentStyle()}>
             <Authorized
-              authority={this.getPageAuthority(pathname, breadcrumbNameMap)}
+              authority={routerConfig && routerConfig.authority}
               noMatch={<Exception403 />}
             >
               {children}
