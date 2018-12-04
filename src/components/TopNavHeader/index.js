f5c2919d50c93c6243ff4e10667de98225f7c75f
Rook <407031061@qq.com>
2018-12-04 23:15:34 +0800

    settings update

--- src/components/TopNavHeader/index.js
+++ src/components/TopNavHeader/index.js
@@ -32,7 +32,7 @@ export default class TopNavHeader extends PureComponent {
             <div className={styles.logo} key="logo" id="logo">
               <Link to="/">
                 <img src={logo} alt="logo" />
-                <h1>Ant Design Pro</h1>
+                <h1>易崛企</h1>
               </Link>
             </div>
             <div
