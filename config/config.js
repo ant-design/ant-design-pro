a1d112d2ccb59b7c4390961069049df7d2678c68
愚道 <tingzhao.ytz@antfin.com>
2018-12-07 19:12:10 +0800

    comment hardsource

--- config/config.js
+++ config/config.js
@@ -28,15 +28,15 @@ const plugins = [
           importWorkboxFrom: 'local',
         },
       },
-      ...(!process.env.TEST && os.platform() === 'darwin'
-        ? {
-            dll: {
-              include: ['dva', 'dva/router', 'dva/saga', 'dva/fetch'],
-              exclude: ['@babel/runtime'],
-            },
-            hardSource: true,
-          }
-        : {}),
+      // ...(!process.env.TEST && os.platform() === 'darwin'
+      //   ? {
+      //       dll: {
+      //         include: ['dva', 'dva/router', 'dva/saga', 'dva/fetch'],
+      //         exclude: ['@babel/runtime'],
+      //       },
+      //       hardSource: true,
+      //     }
+      //   : {}),
     },
   ],
 ];
