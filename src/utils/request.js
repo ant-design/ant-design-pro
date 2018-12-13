756936278806b881fd5b2214c2bddf0ec7587ab0
愚道 <tingzhao.ytz@antfin.com>
2018-12-13 13:44:07 +0800

    update request comment

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,6 @@
 /**
  * request 网络请求工具
- * 更详细的api文档: https://bigfish.alipay.com/doc/api#request
+ * 更详细的 api 文档: https://github.com/umijs/umi-request
  */
 import { extend } from 'umi-request';
 import { notification } from 'antd';
