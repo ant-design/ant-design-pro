ef7e5d9237e7de86f8152db73e1762c0e647648b
愚道 <tingzhao.ytz@antfin.com>
2018-12-13 09:49:47 +0800

    remove blocks locale

--- src/locales/zh-TW.js
+++ src/locales/zh-TW.js
@@ -1,11 +1,5 @@
-import analysis from './zh-TW/analysis';
-import exception from './zh-TW/exception';
-import form from './zh-TW/form';
 import globalHeader from './zh-TW/globalHeader';
-import login from './zh-TW/login';
 import menu from './zh-TW/menu';
-import monitor from './zh-TW/monitor';
-import result from './zh-TW/result';
 import settingDrawer from './zh-TW/settingDrawer';
 import settings from './zh-TW/settings';
 import pwa from './zh-TW/pwa';
@@ -19,14 +13,8 @@ export default {
   'app.forms.basic.title': '基礎表單',
   'app.forms.basic.description':
     '表單頁用於向用戶收集或驗證信息，基礎表單常見於數據項較少的表單場景。',
-  ...analysis,
-  ...exception,
-  ...form,
   ...globalHeader,
-  ...login,
   ...menu,
-  ...monitor,
-  ...result,
   ...settingDrawer,
   ...settings,
   ...pwa,
