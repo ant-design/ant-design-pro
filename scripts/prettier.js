c168928b49c943f621a2e732d6adab6002443429
Yanick Landry <yanick.landry@gmail.com>
2018-12-16 15:08:48 +0800

    added less files and config files to the prettier script

--- scripts/prettier.js
+++ scripts/prettier.js
@@ -16,8 +16,12 @@ let didError = false;
 let files = [];
 const jsFiles = glob.sync('src/**/*.js*', { ignore: ['**/node_modules/**', 'build/**'] });
 const tsFiles = glob.sync('src/**/*.ts*', { ignore: ['**/node_modules/**', 'build/**'] });
+const configFiles = glob.sync('config/**/*.js*', { ignore: ['**/node_modules/**', 'build/**'] });
+const lessFiles = glob.sync('src/**/*.less*', { ignore: ['**/node_modules/**', 'build/**'] });
 files = files.concat(jsFiles);
 files = files.concat(tsFiles);
+files = files.concat(configFiles);
+files = files.concat(lessFiles);
 if (!files.length) {
   return;
 }
