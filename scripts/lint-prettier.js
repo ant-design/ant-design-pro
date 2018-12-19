4d3773f30b0ff84df2d3073e9492a0a3be4fcbcf
🔥 TommY ❄️ <lihroff@icloud.com>
2018-12-19 23:45:41 +0800

    alter lint-prettier

--- scripts/lint-prettier.js
+++ scripts/lint-prettier.js
@@ -6,7 +6,6 @@
  *----------*****--------------
  */
 
-const glob = require('glob');
 const prettier = require('prettier');
 const fs = require('fs');
 const prettierConfigPath = require.resolve('../.prettierrc');
@@ -17,30 +16,37 @@ let didError = false;
 let didWarn = false;
 
 files.forEach(file => {
-  const options = prettier.resolveConfig.sync(file, {
-    config: prettierConfigPath,
-  });
-  try {
-    const fileInfo = prettier.getFileInfo.sync(file);
-    if (fileInfo.ignored) {
-      return;
-    }
-    const input = fs.readFileSync(file, 'utf8');
-    const withParserOptions = {
-      ...options,
-      parser: fileInfo.inferredParser,
-    };
-    const isPrettier = prettier.check(input, withParserOptions);
-    if (!isPrettier) {
-      console.log(`\x1b[31m ${file} is no prettier, please use npm run prettier and git add !\x1b[0m`);
-      didWarn = true;
-    }
-  } catch (e) {
-    didError = true;
-  }
+  Promise.all([
+    prettier.resolveConfig(file, {
+      config: prettierConfigPath,
+    }),
+    prettier.getFileInfo(file),
+  ])
+    .then(resolves => {
+      const [options, fileInfo] = resolves;
+      if (fileInfo.ignored) {
+        return;
+      }
+      const input = fs.readFileSync(file, 'utf8');
+      const withParserOptions = {
+        ...options,
+        parser: fileInfo.inferredParser,
+      };
+      const isPrettier = prettier.check(input, withParserOptions);
+      if (!isPrettier) {
+        console.log(
+          `\x1b[31m ${file} is no prettier, please use npm run prettier and git add !\x1b[0m`
+        );
+        didWarn = true;
+      }
+    })
+    .catch(e => {
+      didError = true;
+    })
+    .finally(() => {
+      if (didWarn || didError) {
+        process.exit(1);
+      }
+      console.log('\x1b[32m lint prettier success!\x1b[0m');
+    });
 });
-
-if (didWarn || didError) {
-  process.exit(1);
-}
-console.log('\x1b[32m lint prettier success!\x1b[0m');
