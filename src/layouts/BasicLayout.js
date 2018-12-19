22718f23e381b5e960da4efe9c3311dc270f8156
chencheng <sorrycc@gmail.com>
2018-12-19 17:17:37 +0800

    feat: remove SettingDrawer

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Layout } from 'antd';
 import DocumentTitle from 'react-document-title';
 import isEqual from 'lodash/isEqual';
@@ -14,13 +14,9 @@ import logo from '../assets/logo.svg';
 import Footer from './Footer';
 import Header from './Header';
 import Context from './MenuContext';
-import PageLoading from '@/components/PageLoading';
 import SiderMenu from '@/components/SiderMenu';
 import { title } from '../defaultSettings';
 import styles from './BasicLayout.less';
-
-// lazy load SettingDrawer
-const SettingDrawer = React.lazy(() => import('@/components/SettingDrawer'));
 
 const { Content } = Layout;
 
@@ -145,15 +141,6 @@ class BasicLayout extends React.PureComponent {
     });
   };
 
-  renderSettingDrawer = () => {
-    // Do not render SettingDrawer in production
-    // unless it is deployed in preview.pro.ant.design as demo
-    if (process.env.NODE_ENV === 'production' && APP_TYPE !== 'site') {
-      return null;
-    }
-    return <SettingDrawer />;
-  };
-
   render() {
     const {
       navTheme,
@@ -215,7 +202,6 @@ class BasicLayout extends React.PureComponent {
             )}
           </ContainerQuery>
         </DocumentTitle>
-        <Suspense fallback={<PageLoading />}>{this.renderSettingDrawer()}</Suspense>
       </React.Fragment>
     );
   }
--- src/locales/zh-TW/pwa.js
+++
@@ -1,6 +0,0 @@
-export default {
-  'app.pwa.offline': '當前處於離線狀態',
-  'app.pwa.serviceworker.updated': '有新內容',
-  'app.pwa.serviceworker.updated.hint': '請點擊“刷新”按鈕或者手動刷新頁面',
-  'app.pwa.serviceworker.updated.ok': '刷新',
-};
