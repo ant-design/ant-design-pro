88dcd247da3310c6f3c69c20ab6cbb4e4d39215d
chencheng <sorrycc@gmail.com>
2018-12-19 16:26:00 +0800

    feat: simplify antd-pro's boilerplate

--- scripts/generateMock.js
+++
@@ -1,3 +0,0 @@
-const generateMock = require('merge-umi-mock-data');
-const path = require('path');
-generateMock(path.join(__dirname, '../mock'), path.join(__dirname, '../functions/mock/index.js'));
---
+++ src/locales/pt-BR.js
@@ -0,0 +1,18 @@
+import globalHeader from './pt-BR/globalHeader';
+import menu from './pt-BR/menu';
+import settingDrawer from './pt-BR/settingDrawer';
+import settings from './pt-BR/settings';
+import pwa from './pt-BR/pwa';
+
+export default {
+  'navBar.lang': 'Idiomas',
+  'layout.user.link.help': 'ajuda',
+  'layout.user.link.privacy': 'política de privacidade',
+  'layout.user.link.terms': 'termos de serviços',
+  'app.home.introduce': 'introduzir',
+  ...globalHeader,
+  ...menu,
+  ...settingDrawer,
+  ...settings,
+  ...pwa,
+};
