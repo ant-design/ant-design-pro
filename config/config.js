cc7f8f1dda6f23f3c412361ccfac85704595eef5
chencheng <sorrycc@gmail.com>
2018-12-19 16:26:00 +0800

    feat: simplify antd-pro's boilerplate

--- config/config.js
+++ config/config.js
@@ -1,131 +1,74 @@
-// https://umijs.org/config/
-import os from 'os';
-import webpackPlugin from './plugin.config';
-import defaultSettings from '../src/defaultSettings';
-import slash from 'slash2';
-
-const plugins = [
-  [
-    'umi-plugin-react',
-    {
-      antd: true,
-      dva: {
-        hmr: true,
-      },
-      locale: {
-        enable: true, // default false
-        default: 'zh-CN', // default zh-CN
-        baseNavigator: true, // default true, when it is true, will use `navigator.language` overwrite default
-      },
-      dynamicImport: {
-        loadingComponent: './components/PageLoading/index',
-        webpackChunkName: true,
-      },
-      pwa: {
-        workboxPluginMode: 'InjectManifest',
-        workboxOptions: {
-          importWorkboxFrom: 'local',
-        },
-      },
-      // ...(!process.env.TEST && os.platform() === 'darwin'
-      //   ? {
-      //       dll: {
-      //         include: ['dva', 'dva/router', 'dva/saga', 'dva/fetch'],
-      //         exclude: ['@babel/runtime'],
-      //       },
-      //       hardSource: true,
-      //     }
-      //   : {}),
-    },
-  ],
-];
-
-// 针对 preview.pro.ant.design 的 GA 统计代码
-// 业务上不需要这个
-if (process.env.APP_TYPE === 'site') {
-  plugins.push([
-    'umi-plugin-ga',
-    {
-      code: 'UA-72788897-6',
-    },
-  ]);
-}
+// ref: https://umijs.org/config/
+import { primaryColor } from '../src/defaultSettings';
 
 export default {
-  // add for transfer to umi
-  plugins,
-  define: {
-    APP_TYPE: process.env.APP_TYPE || '',
-  },
-  treeShaking: true,
+  plugins: [
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: {
+          hmr: true,
+        },
+        targets: {
+          ie: 11,
+        },
+        locale: {
+          enable: true, // default false
+          default: 'zh-CN', // default zh-CN
+          baseNavigator: true, // default true, when it is true, will use `navigator.language` overwrite default
+        },
+        dynamicImport: {
+          loadingComponent: './components/PageLoading/index',
+        },
+      },
+    ],
+  ],
   targets: {
     ie: 11,
   },
-  // 路由配置
-  routes: [{
-    path: '/user',
-    components: ['../layouts/UserLayout'],
-    routes: [],
-  }, {
-    path: '/',
-    component: '../layouts/BasicLayout',
-    Routes: ['src/pages/Authorized'],
-    authority: ['admin', 'user'],
-    routes: [
-      // dashboard
-      {
-        path: '/',
-        name: 'dashboard',
-        icon: 'dashboard',
-        component: './BasicDemo',
-      },
-    ],
-  }],
+
+  /**
+   * 路由相关配置
+   */
+  routes: [
+    {
+      path: '/user',
+      components: ['../layouts/UserLayout'],
+      routes: [],
+    },
+    {
+      path: '/',
+      component: '../layouts/BasicLayout',
+      routes: [
+        // dashboard
+        {
+          path: '/',
+          name: 'dashboard',
+          icon: 'dashboard',
+          component: './BasicDemo',
+        },
+      ],
+    },
+  ],
+  disableRedirectHoist: true,
+
+  /**
+   * webpack 相关配置
+   */
+  define: {
+    APP_TYPE: process.env.APP_TYPE || '',
+  },
   // Theme for antd
   // https://ant.design/docs/react/customize-theme-cn
   theme: {
-    'primary-color': defaultSettings.primaryColor,
+    'primary-color': primaryColor,
   },
   externals: {
     '@antv/data-set': 'DataSet',
   },
-  // proxy: {
-  //   '/server/api/': {
-  //     target: 'https://preview.pro.ant.design/',
-  //     changeOrigin: true,
-  //     pathRewrite: { '^/server': '' },
-  //   },
-  // },
   ignoreMomentLocale: true,
   lessLoaderOptions: {
     javascriptEnabled: true,
   },
-  disableRedirectHoist: true,
-  cssLoaderOptions: {
-    modules: true,
-    getLocalIdent: (context, localIdentName, localName) => {
-      if (
-        context.resourcePath.includes('node_modules') ||
-        context.resourcePath.includes('ant.design.pro.less') ||
-        context.resourcePath.includes('global.less')
-      ) {
-        return localName;
-      }
-      const match = context.resourcePath.match(/src(.*)/);
-      if (match && match[1]) {
-        const antdProPath = match[1].replace('.less', '');
-        const arr = slash(antdProPath)
-          .split('/')
-          .map(a => a.replace(/([A-Z])/g, '-$1'))
-          .map(a => a.toLowerCase());
-        return `antd-pro${arr.join('-')}-${localName}`.replace(/--/g, '-');
-      }
-      return localName;
-    },
-  },
-  manifest: {
-    basePath: '/',
-  },
-
-  chainWebpack: webpackPlugin,
 };
