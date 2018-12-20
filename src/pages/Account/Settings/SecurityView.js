14db63f26df38edd5eb71d47023c746712eea7ba
Rook <407031061@qq.com>
2018-12-20 22:52:56 +0800

    安全选项删除

--- src/pages/Account/Settings/SecurityView.js
+++ src/pages/Account/Settings/SecurityView.js
@@ -3,43 +3,10 @@ import { formatMessage, FormattedMessage } from 'umi/locale';
 import { List } from 'antd';
 // import { getTimeDistance } from '@/utils/utils';
 
-const passwordStrength = {
-  strong: (
-    <font className="strong">
-      <FormattedMessage id="app.settings.security.strong" defaultMessage="Strong" />
-    </font>
-  ),
-  medium: (
-    <font className="medium">
-      <FormattedMessage id="app.settings.security.medium" defaultMessage="Medium" />
-    </font>
-  ),
-  weak: (
-    <font className="weak">
-      <FormattedMessage id="app.settings.security.weak" defaultMessage="Weak" />
-      Weak
-    </font>
-  ),
-};
-
 class SecurityView extends Component {
   getData = () => [
     {
-      title: formatMessage({ id: 'app.settings.security.password' }, {}),
-      description: (
-        <Fragment>
-          {formatMessage({ id: 'app.settings.security.password-description' })}：
-          {passwordStrength.strong}
-        </Fragment>
-      ),
-      actions: [
-        <a>
-          <FormattedMessage id="app.settings.security.modify" defaultMessage="Modify" />
-        </a>,
-      ],
-    },
-    {
-      title: formatMessage({ id: 'app.settings.security.phone' }, {}),
+      title: '绑定手机',
       description: `${formatMessage(
         { id: 'app.settings.security.phone-description' },
         {}
@@ -51,32 +18,14 @@ class SecurityView extends Component {
       ],
     },
     {
-      title: formatMessage({ id: 'app.settings.security.question' }, {}),
-      description: formatMessage({ id: 'app.settings.security.question-description' }, {}),
-      actions: [
-        <a>
-          <FormattedMessage id="app.settings.security.set" defaultMessage="Set" />
-        </a>,
-      ],
-    },
-    {
-      title: formatMessage({ id: 'app.settings.security.email' }, {}),
+      title: formatMessage({ id: '绑定邮箱' }, {}),
       description: `${formatMessage(
         { id: 'app.settings.security.email-description' },
         {}
-      )}：ant***sign.com`,
+      )}：123***@qq.com`,
       actions: [
         <a>
           <FormattedMessage id="app.settings.security.modify" defaultMessage="Modify" />
-        </a>,
-      ],
-    },
-    {
-      title: formatMessage({ id: 'app.settings.security.mfa' }, {}),
-      description: formatMessage({ id: 'app.settings.security.mfa-description' }, {}),
-      actions: [
-        <a>
-          <FormattedMessage id="app.settings.security.bind" defaultMessage="Bind" />
         </a>,
       ],
     },
