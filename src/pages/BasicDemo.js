7954726c0c7d8114f0c474670f110f2a0f082ac2
愚道 <tingzhao.ytz@antfin.com>
2018-12-21 14:30:21 +0800

    fix menu bug

--- src/pages/BasicDemo.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export default () => <p>hello pro!</p>;
---
+++ src/pages/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => (
+  <p>
+    想要添加更多页面？请参考{' '}
+    <a href="https://umijs.org/guide/block.html" target="_blank" rel="noopener noreferrer">
+      umi 区块
+    </a>
+    。
+  </p>
+);
