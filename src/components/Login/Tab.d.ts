d5d096eb668820c57ea0f9a3ee3bbcaa570de029
Shuai Chen <wasd2144@hotmail.com>
2018-12-25 21:54:44 +0800

    Tab modified to LoginTab

--- src/components/Login/Tab.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export interface ILoginTabProps {
-  key?: string;
-  tab?: React.ReactNode;
-}
-export default class LoginTab extends React.Component<ILoginTabProps, any> {}
