03af5074fe865df67bc7a4dbfa46e785e1de020b
jarrywen <jarrywen@163.com>
2018-12-26 20:49:17 +0800

    v0.1

---
+++ src/pages/ApiGateway/ApiCreate/Step4.js
@@ -0,0 +1,73 @@
+import React, { Fragment } from 'react';
+import { connect } from 'dva';
+import { Button, Row, Col } from 'antd';
+import router from 'umi/router';
+import Result from '@/components/Result';
+import styles from './style.less';
+
+@connect(({ apiCreate }) => ({
+  data: apiCreate.step,
+}))
+class Step4 extends React.PureComponent {
+  render() {
+    const { data } = this.props;
+    const onFinish = () => {
+      router.push('/apiGateway/apiCreate/info');
+    };
+    const information = (
+      <div className={styles.information}>
+        <Row>
+          <Col xs={24} sm={8} className={styles.label}>
+            api名称：
+          </Col>
+          <Col xs={24} sm={16}>
+            {data.apiName}
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={24} sm={8} className={styles.label}>
+            请求Path：
+          </Col>
+          <Col xs={24} sm={16}>
+            {data.consumerPath}
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={24} sm={8} className={styles.label}>
+            后端地址：
+          </Col>
+          <Col xs={24} sm={16}>
+            {data.producerUrl}
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={24} sm={8} className={styles.label}>
+            后端Path：
+          </Col>
+          <Col xs={24} sm={16}>
+            {data.producerPath}
+          </Col>
+        </Row>
+      </div>
+    );
+    const actions = (
+      <Fragment>
+        <Button type="primary" onClick={onFinish}>
+          再创建一笔
+        </Button>
+      </Fragment>
+    );
+    return (
+      <Result
+        type="success"
+        title="操作成功"
+        description="操作成功，请到Api列表确认该Api是否需要发布！"
+        extra={information}
+        actions={actions}
+        className={styles.result}
+      />
+    );
+  }
+}
+
+export default Step4;
