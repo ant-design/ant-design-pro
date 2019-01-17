cfbfbdcbee9c843d5b52a348beaee9597523bf84
li407031061 <407031061@qq.com>
2019-01-17 20:24:46 +0800

    task board layout

--- src/pages/Task/TaskBoard.js
+++ src/pages/Task/TaskBoard.js
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
+import { Card, Avatar, List } from 'antd';
+import styles from './TaskBoard.less';
 
 @connect(({ team, loading }) => ({
   team,
@@ -9,7 +11,25 @@ class TaskBoard extends PureComponent {
   componentDidMount() {}
 
   render() {
-    return <div />;
+    return (
+      <div className={styles.kanban}>
+        <div className={styles.listContainer}>
+          <div className={styles.listGroup}>
+            <Card bodyStyle={{ padding: 0, height: '100%' }} bordered={false} title="列表">
+              <List size="small">
+                <List.Item className={styles.ListItem}>
+                  <List.Item.Meta
+                    avatar={<Avatar src="" className={styles.avatarPic} />}
+                    title="名称"
+                    description={<span className={styles.membertype}>描述</span>}
+                  />
+                </List.Item>
+              </List>
+            </Card>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
