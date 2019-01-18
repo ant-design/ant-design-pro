a05a26ce0b2e320d99a6f15a18a4ab7a40e64f47
Shuai Chen <wasd2144@hotmail.com>
2019-01-18 21:14:07 +0800

    remove headless config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,6 @@
 // ps https://github.com/GoogleChrome/puppeteer/issues/3120
 module.exports = {
   launch: {
-    headless: true,
     args: [
       '--disable-gpu',
       '--disable-dev-shm-usage',
