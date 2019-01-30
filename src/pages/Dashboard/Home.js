14dd4b2cb74376b346737e4920d4cf1094c50269
li407031061 <407031061@qq.com>
2019-01-30 21:14:11 +0800

    项目列表

--- src/pages/Dashboard/Home.js
+++ src/pages/Dashboard/Home.js
@@ -1,7 +1,155 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { connect } from 'dva';
+import {
+  List,
+  Card,
+  Row,
+  Col,
+  Progress,
+  Button,
+  Icon,
+  Dropdown,
+  Menu,
+  Avatar,
+  Modal,
+  Form,
+} from 'antd';
 
-function Home() {
-  return <div> 正在建设中... </div>;
+import styles from './Home.less';
+
+@connect(({ list, loading }) => ({
+  list,
+  loading: loading.models.list,
+}))
+@Form.create()
+class Home extends PureComponent {
+  state = {};
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'list/fetch',
+      payload: {
+        count: 5,
+      },
+    });
+  }
+
+  showEditModal = item => {
+    console.log(item);
+  };
+
+  handleDone = () => {
+    setTimeout(() => this.addBtn.blur(), 0);
+  };
+
+  handleCancel = () => {
+    setTimeout(() => this.addBtn.blur(), 0);
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { dispatch, form } = this.props;
+    const { current } = this.state;
+    const id = current ? current.id : '';
+
+    setTimeout(() => this.addBtn.blur(), 0);
+    form.validateFields((err, fieldsValue) => {
+      if (err) return;
+      dispatch({
+        type: 'list/submit',
+        payload: { id, ...fieldsValue },
+      });
+    });
+  };
+
+  deleteItem = id => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'list/submit',
+      payload: { id },
+    });
+  };
+
+  render() {
+    const {
+      list: { list },
+      loading,
+    } = this.props;
+
+    const editAndDelete = (key, currentItem) => {
+      if (key === 'edit') this.showEditModal(currentItem);
+      else if (key === 'delete') {
+        Modal.confirm({
+          title: '删除任务',
+          content: '确定删除该任务吗？',
+          okText: '确认',
+          cancelText: '取消',
+          onOk: () => this.deleteItem(currentItem.id),
+        });
+      }
+    };
+
+    const ListContent = ({ data: { percent, status } }) => (
+      <div className={styles.listContent}>
+        <div className={styles.listContentItem}>
+          <Progress percent={percent} status={status} strokeWidth={6} style={{ width: 180 }} />
+        </div>
+      </div>
+    );
+
+    const MoreBtn = props => (
+      <Dropdown
+        overlay={
+          <Menu onClick={({ key }) => editAndDelete(key, props.current)}>
+            <Menu.Item key="edit">编辑</Menu.Item>
+            <Menu.Item key="delete">移动到</Menu.Item>
+          </Menu>
+        }
+      >
+        <a>
+          更多 <Icon type="down" />
+        </a>
+      </Dropdown>
+    );
+
+    return (
+      <Row className={styles.membersList}>
+        <Col span={6} className={styles.listCol} />
+
+        <Col span={18} className={styles.cardCol}>
+          <div className={styles.standardList}>
+            <Card
+              className={styles.listCard}
+              bordered={false}
+              title="项目列表"
+              bodyStyle={{ padding: '0 32px 40px 32px' }}
+            >
+              <Button type="dashed" style={{ width: '100%', marginBottom: 8 }} icon="plus">
+                添加
+              </Button>
+              <List
+                size="large"
+                rowKey="id"
+                loading={loading}
+                dataSource={list}
+                renderItem={item => (
+                  <List.Item actions={[<MoreBtn current={item} />]}>
+                    <List.Item.Meta
+                      avatar={<Avatar src={item.logo} shape="square" size="large" />}
+                      title={<a href={item.href}>{item.title}</a>}
+                      description={item.subDescription}
+                    />
+                    <ListContent data={item} />
+                  </List.Item>
+                )}
+              />
+            </Card>
+          </div>
+        </Col>
+      </Row>
+    );
+  }
 }
 
 export default Home;
