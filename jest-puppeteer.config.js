92bceef42f73b702ca55fa64f58b3026c16eac45
Shuai Chen <wasd2144@hotmail.com>
2019-02-08 23:42:33 +0800

    test ci

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,11 +1,12 @@
 // ps https://github.com/GoogleChrome/puppeteer/issues/3120
 module.exports = {
   launch: {
-    args: [
-      '--disable-gpu',
-      '--disable-dev-shm-usage',
-      '--no-first-run',
-      '--no-zygote',
-    ],
+    args: ['--disable-gpu', '--disable-dev-shm-usage', '--no-first-run', '--no-zygote'],
+  },
+  server: {
+    command: 'yarn start',
+    port: 8000,
+    launchTimeout: 50000,
+    usedPortAction: 'kill',
   },
 };
