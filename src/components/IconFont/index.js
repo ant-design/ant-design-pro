414526030a5f59df7770fd0308701d2a410cf828
LiChuanqing <lichuanqing@100tal.com>
2019-02-11 20:19:18 +0800

    feature: 1.add iconfont component; 2.menu can add iconfont icon.

---
+++ src/components/IconFont/index.js
@@ -0,0 +1,10 @@
+import { Icon } from 'antd';
+
+// Url修改成自己的iconfont图标项目地址
+// 注意：如果需要图标多色，Iconfont图标项目里要进行批量去色处理
+// 使用：
+// import IconFont from '@/components/IconFont';
+// <IconFont type='icon-demo' className='xxx-xxx' />
+export default Icon.createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_1039637_btcrd5co4w.js',
+});
