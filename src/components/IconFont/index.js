96fa9b15170f303a0e8b225c4f25330ec739802e
LiChuanqing <lichuanqing@100tal.com>
2019-02-12 11:55:25 +0800

    ajust: put the IconFont Script Url into defaultSetting.js

--- src/components/IconFont/index.js
+++ src/components/IconFont/index.js
@@ -1,10 +1,11 @@
 import { Icon } from 'antd';
+import { iconFontScriptUrl } from '../../defaultSettings';
 
-// Url修改成自己的iconfont图标项目地址
+// iconFontScriptUrl 修改成自己的iconfont图标项目地址
 // 注意：如果需要图标多色，Iconfont图标项目里要进行批量去色处理
 // 使用：
 // import IconFont from '@/components/IconFont';
 // <IconFont type='icon-demo' className='xxx-xxx' />
 export default Icon.createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_1039637_btcrd5co4w.js',
+  scriptUrl: iconFontScriptUrl,
 });
