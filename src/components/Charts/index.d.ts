d5a71a0695da7215d1cf03081ca65ec497af6e25
vellengs <vellengs@qq.com>
2019-02-13 13:36:51 +0800

    Update index.d.ts

--- src/components/Charts/index.d.ts
+++ src/components/Charts/index.d.ts
@@ -1,16 +1,16 @@
 import * as numeral from 'numeral';
-import { default as ChartCard } from './ChartCard';
 import { default as Bar } from './Bar';
-import { default as Pie } from './Pie';
-import { default as Radar } from './Radar';
+import { default as ChartCard } from './ChartCard';
+import { default as Field } from './Field';
 import { default as Gauge } from './Gauge';
 import { default as MiniArea } from './MiniArea';
 import { default as MiniBar } from './MiniBar';
 import { default as MiniProgress } from './MiniProgress';
-import { default as Field } from './Field';
-import { default as WaterWave } from './WaterWave';
+import { default as Pie } from './Pie';
+import { default as Radar } from './Radar';
 import { default as TagCloud } from './TagCloud';
 import { default as TimelineChart } from './TimelineChart';
+import { default as WaterWave } from './WaterWave';
 
 declare const yuan: (value: number | string) => string;
 
