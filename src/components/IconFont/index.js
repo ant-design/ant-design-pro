faaab599a135a878da737356b283a0ba1f4eebe7
LiChuanqing <lichuanqing@100tal.com>
2019-02-13 17:45:59 +0800

    留空iconfontUrl

--- src/components/IconFont/index.js
+++ src/components/IconFont/index.js
@@ -1,8 +1,6 @@
 import { Icon } from 'antd';
 import { iconfontUrl as scriptUrl } from '../../defaultSettings';
 
-// iconfontUrl 修改成自己的iconfont图标项目地址
-// 注意：如果需要图标多色，Iconfont图标项目里要进行批量去色处理
 // 使用：
 // import IconFont from '@/components/IconFont';
 // <IconFont type='icon-demo' className='xxx-xxx' />
