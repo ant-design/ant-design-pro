1eb11cea5369e57dcd3c2d3116d1640b19a24150
Lucas <lucas@carteiraglobal.com>
2019-02-13 11:29:51 -0200

    i18n pt-BR: analysis & component

--- src/locales/pt-BR/component.js
+++ src/locales/pt-BR/component.js
@@ -1,5 +1,5 @@
 export default {
   'component.tagSelect.expand': 'Expandir',
-  'component.tagSelect.collapse': 'Colapso',
+  'component.tagSelect.collapse': 'Diminuir',
   'component.tagSelect.all': 'Todas',
 };
