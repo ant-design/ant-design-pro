094526ffc910178e99844b9fcb6446e55dd20f72
陈小聪 <xiaohuoni@users.noreply.github.com>
2019-02-28 01:47:23 +0800

    services ts (#3625)

--- src/services/error.ts
+++ src/services/error.ts
@@ -1,5 +1,5 @@
 import request from '@/utils/request';
 
-export default async function queryError(code) {
+export default async function queryError(code: number): Promise<any> {
   return request(`/api/${code}`);
 }
--- src/services/geographic.ts
+++ src/services/geographic.ts
@@ -1,9 +1,9 @@
 import request from '@/utils/request';
 
-export async function queryProvince() {
+export async function queryProvince(): Promise<any> {
   return request('/api/geographic/province');
 }
 
-export async function queryCity(province) {
+export async function queryCity(province: string): Promise<any> {
   return request(`/api/geographic/city/${province}`);
 }
