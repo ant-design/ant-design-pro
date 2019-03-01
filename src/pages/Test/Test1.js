ad907bd9366aba08537b237a54e6e29792943044
jarrywen <jarrywen@163.com>
2019-03-01 19:41:20 +0800

    v0.1

---
+++ src/pages/Test/Test1.js
@@ -0,0 +1,10 @@
+// react页面必须引入的组件
+import React from 'react';
+// 引入面包屑导航组件
+import PageHeaderWrapper from '@/components/PageHeaderWrapper';
+
+export default () => (
+  <PageHeaderWrapper>
+    <div>This is Test Page</div>
+  </PageHeaderWrapper>
+);
