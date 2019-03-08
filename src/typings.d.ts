71646ba767d4c5eb6996c0b9a3f8f91cee5613cc
imhele <work@imhele.com>
2019-03-08 18:57:08 +0800

    fix lost type definition of function wrapped by debounce

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -10,3 +10,17 @@ declare module '*.gif';
 declare module '*.bmp';
 declare module '*.tiff';
 declare var APP_TYPE: string;
+declare module 'lodash-decorators/debounce' {
+  import { Cancelable } from 'lodash';
+  import { DebounceOptions } from 'lodash-decorators/shared';
+
+  function Debounce<T>(
+    wait?: number,
+    options?: DebounceOptions
+  ): (
+    target: Object,
+    propertyKey: string | symbol,
+    descriptor: TypedPropertyDescriptor<T & Cancelable>
+  ) => TypedPropertyDescriptor<T> | void;
+  export default Debounce;
+}
