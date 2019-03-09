2c05063103b17f60b697ddf7c4b3256489cb6f32
xiaohuoni <448627663@qq.com>
2019-03-09 11:54:39 +0800

    src/components/PageLoading

--- src/components/PageLoading/index.tsx
+++ src/components/PageLoading/index.tsx
@@ -3,8 +3,9 @@ import { Spin } from 'antd';
 
 // loading components from code split
 // https://umijs.org/plugin/umi-plugin-react.html#dynamicimport
-export default () => (
+const PageLoding: React.SFC = () => (
   <div style={{ paddingTop: 100, textAlign: 'center' }}>
     <Spin size="large" />
   </div>
 );
+export default PageLoding;
