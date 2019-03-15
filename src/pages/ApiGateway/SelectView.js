8b7abc315223dad308c0abecf68b18c9a90a6610
jarrywen <jarrywen@163.com>
2019-03-15 17:28:23 +0800

    v0.1

---
+++ src/pages/ApiGateway/SelectView.js
@@ -0,0 +1,62 @@
+import React, { PureComponent } from 'react';
+import { Select } from 'antd';
+import { getItems, getItemValue } from '../../utils/masterData';
+
+const { Option } = Select;
+const nullSlectItem = {
+  label: '',
+  key: '',
+};
+class SelectView extends PureComponent {
+  getOption() {
+    const { javaCode, javaKey } = this.props;
+    const items = getItems(javaCode, javaKey);
+    return this.getOptionWhithList(items);
+  }
+
+  getOptionWhithList = list => {
+    if (!list || list.length < 1) {
+      return (
+        <Option key={0} value={0}>
+          没有找到选项
+        </Option>
+      );
+    }
+    return list.map(item => (
+      <Option key={item.itemCode} value={item.itemCode}>
+        {item.itemValue}
+      </Option>
+    ));
+  };
+
+  selectChangeItem = item => {
+    const { onChange } = this.props;
+    onChange(item);
+  };
+
+  conversionObject() {
+    const { javaCode, javaKey, value } = this.props;
+    console.log(javaCode, javaKey, value);
+    if (!value) {
+      return nullSlectItem;
+    }
+    const itemValue = value && value.key ? value.key : value;
+    const valueLabel = {
+      label: getItemValue(javaCode, javaKey, itemValue),
+      key: itemValue,
+    };
+    return valueLabel || nullSlectItem;
+  }
+
+  render() {
+    // const value = this.conversionObject();
+    const { value } = this.props;
+    return (
+      <Select value={value} onSelect={this.selectChangeItem}>
+        {this.getOption()}
+      </Select>
+    );
+  }
+}
+
+export default SelectView;
