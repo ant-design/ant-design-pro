50ae176aa9685aaa33c4039c0358ceedafb77a6e
xiaohuoni <448627663@qq.com>
2019-03-22 14:34:45 +0800

    PageHeaderWrapper

---
+++ src/components/PageHeaderWrapper/GridContent.d.ts
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export interface GridContentProps {
+  contentWidth: string;
+  children: React.ReactNode;
+}
+
+export default class GridContent extends React.Component<GridContentProps, any> {}
