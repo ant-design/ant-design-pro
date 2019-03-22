4b28ced5f5a2e8723ce9aabaa16d7562d56d20ab
xiaohuoni <448627663@qq.com>
2019-03-22 11:36:32 +0800

    TopNavHeader

---
+++ src/components/TopNavHeader/index.d.ts
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+export declare type CollapseType = 'clickTrigger' | 'responsive';
+export declare type SiderTheme = 'light' | 'dark';
+export declare type MenuMode =
+  | 'vertical'
+  | 'vertical-left'
+  | 'vertical-right'
+  | 'horizontal'
+  | 'inline';
+
+export interface ITopNavHeaderProps {
+  theme: SiderTheme;
+  contentWidth?: string;
+  menuData?: any[];
+  logo?: string;
+  mode?: MenuMode;
+  flatMenuKeys?: any[];
+  onCollapse?: (collapsed: boolean, type?: CollapseType) => void;
+  isMobile?: boolean;
+  openKeys?: any;
+  className?: string;
+  collapsed?: boolean;
+  handleOpenChange?: (openKeys: any[]) => void;
+  style?: React.CSSProperties;
+  onOpenChange?: (openKeys: string[]) => void;
+  onNoticeClear?: (type: string) => void;
+  onMenuClick?: ({ key: string }) => void;
+  onNoticeVisibleChange?: (b: boolean) => void;
+}
+
+export default class TopNavHeader extends React.Component<ITopNavHeaderProps, any> {}
