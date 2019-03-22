fe576df8368f9a47d43f91e5b57e75bd8d81029f
何乐 <work@imhele.com>
2019-03-22 17:14:37 +0800

    Update src/components/AvatarList/AvatarItem.d.ts
    Co-Authored-By: xiaohuoni <xiaohuoni@users.noreply.github.com>

--- src/components/AvatarList/AvatarItem.d.ts
+++ src/components/AvatarList/AvatarItem.d.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export declare type SizeType = number | 'small' | 'default' | 'large' | undefined;
+export declare type SizeType = number | 'small' | 'default' | 'large';
 
 export interface AvatarItemProps {
   tips: React.ReactNode;
