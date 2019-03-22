7a92dcd328dcc23577b20a366ed6db660c15e325
chen shuai <qixian.cs@outlook.com>
2019-03-22 14:33:04 +0800

    fix : fix menu error

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,6 @@ module.exports = {
     'linebreak-style': 0,
   },
   settings: {
-    polyfills: ['fetch', 'promises', 'url'],
+    polyfills: ['fetch', 'Promise', 'url', 'object-assign'],
   },
 };
--- config/router.config.js
+++ config/router.config.js
@@ -265,28 +265,28 @@ export default [
               },
             ],
           },
-          //  editor
+        ],
+      },
+      //  editor
+      {
+        name: 'editor',
+        icon: 'highlight',
+        path: '/editor',
+        routes: [
           {
-            name: 'editor',
-            icon: 'highlight',
-            path: '/editor',
-            routes: [
-              {
-                path: '/editor/flow',
-                name: 'flow',
-                component: './Editor/GGEditor/Flow',
-              },
-              {
-                path: '/editor/mind',
-                name: 'mind',
-                component: './Editor/GGEditor/Mind',
-              },
-              {
-                path: '/editor/koni',
-                name: 'koni',
-                component: './Editor/GGEditor/Koni',
-              },
-            ],
+            path: '/editor/flow',
+            name: 'flow',
+            component: './Editor/GGEditor/Flow',
+          },
+          {
+            path: '/editor/mind',
+            name: 'mind',
+            component: './Editor/GGEditor/Mind',
+          },
+          {
+            path: '/editor/koni',
+            name: 'koni',
+            component: './Editor/GGEditor/Koni',
           },
         ],
       },
