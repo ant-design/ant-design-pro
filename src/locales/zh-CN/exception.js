b949d8bf4d2b0f1f49e88fc30c3d5326de259be6
xiaohuoni <448627663@qq.com>
2019-03-23 16:04:37 +0800

    change exception

--- src/locales/zh-CN/exception.js
+++ src/locales/zh-CN/exception.js
@@ -1,6 +1,6 @@
 export default {
   'app.exception.back': '返回首页',
-  'app.exception.description.403': '抱歉，你无权访问该页面',
+  'app.exception.description.403': '抱歉，你无权访问此页面',
   'app.exception.description.404': '抱歉，你访问的页面不存在',
   'app.exception.description.500': '抱歉，服务器出错了',
 };
