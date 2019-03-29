89190ad05482bcacc6bf622d1fdf196608acf72b
imhele <work@imhele.com>
2019-03-29 14:18:54 +0800

    NoticeIcon

--- src/components/HeaderDropdown/index.tsx
+++ src/components/HeaderDropdown/index.tsx
@@ -4,18 +4,14 @@ import React, { Component } from 'react';
 import styles from './index.less';
 import { DropDownProps } from 'antd/lib/dropdown';
 
-declare type OverlayFunc = () => React.ReactNode;
-
 export interface HeaderDropdownProps extends DropDownProps {
   overlayClassName?: string;
-  overlay: React.ReactNode | OverlayFunc;
-  placement?: 'bottomLeft' | 'bottomRight' | 'topLeft' | 'topCenter' | 'topRight' | 'bottomCenter';
 }
 
+// Use class component to support `ref`
 export default class HeaderDropdown extends Component<HeaderDropdownProps> {
   render() {
     const { overlayClassName, ...props } = this.props;
-
     return (
       <Dropdown overlayClassName={classNames(styles.container, overlayClassName)} {...props} />
     );
--- src/components/NoticeIcon/NoticeList.tsx
+++ src/components/NoticeIcon/NoticeList.tsx
@@ -1,39 +1,59 @@
 import { Avatar, List } from 'antd';
 import classNames from 'classnames';
-import React from 'react';
+import React, { PropsWithChildren, ReactElement } from 'react';
 import { NoticeIconData } from './index';
 import styles from './NoticeList.less';
 
-export interface NoticeIconTabProps {
+export interface NoticeIconTabProps<T extends NoticeIconData = NoticeIconData> {
+  className?: string;
   count?: number;
   emptyText?: React.ReactNode;
   emptyImage?: string;
-  list?: NoticeIconData[];
-  name?: string;
+  list?: T[];
   showClear?: boolean;
   showViewMore?: boolean;
   style?: React.CSSProperties;
   title?: string;
-  data?: any[];
-  onClick: (item: any) => void;
-  onClear: (item: any) => void;
-  locale: any;
-  onViewMore: (e: any) => void;
 }
 
-const NoticeList: React.FC<NoticeIconTabProps> = ({
-  data = [],
+export type NoticeIconTabComponent<T extends NoticeIconData = NoticeIconData> = React.FC<
+  NoticeIconTabProps<T>
+>;
+
+interface NoticeListProps<T extends NoticeIconData = NoticeIconData> extends NoticeIconTabProps<T> {
+  onClick: (item: T) => void;
+  onClear: (event: React.MouseEvent) => void;
+  onViewMore: (event: React.MouseEvent) => void;
+  locale: {
+    emptyText: string;
+    clear: string;
+    viewMore: string;
+    [key: string]: string;
+  };
+  /**
+   * `className` and `style` will take effect in TabPanel
+   */
+  className?: never;
+  style?: never;
+}
+
+type NoticeListComponent = <T extends NoticeIconData = NoticeIconData>(
+  props: PropsWithChildren<NoticeListProps<T>>,
+) => ReactElement | null;
+
+const NoticeList: NoticeListComponent = ({
+  list = [],
   onClick,
   onClear,
-  title,
+  title: tabTitle,
   locale,
   emptyText,
   emptyImage,
-  onViewMore = null,
+  onViewMore,
   showClear = true,
   showViewMore = false,
 }) => {
-  if (data.length === 0) {
+  if (list.length === 0) {
     return (
       <div className={styles.notFound}>
         {emptyImage ? <img src={emptyImage} alt="not found" /> : null}
@@ -44,10 +64,8 @@ const NoticeList: React.FC<NoticeIconTabProps> = ({
   return (
     <div>
       <List className={styles.list} renderItem={null}>
-        {data.map((item, i) => {
-          const itemCls = classNames(styles.item, {
-            [styles.read]: item.read,
-          });
+        {list.map((item, i) => {
+          const itemCls = classNames(styles.item, { [styles.read]: item.read });
           // eslint-disable-next-line no-nested-ternary
           const leftIcon = item.avatar ? (
             typeof item.avatar === 'string' ? (
@@ -56,26 +74,30 @@ const NoticeList: React.FC<NoticeIconTabProps> = ({
               <span className={styles.iconElement}>{item.avatar}</span>
             )
           ) : null;
-
+          const ListItemTitle = (
+            <div className={styles.title}>
+              {item.title}
+              <div className={styles.extra}>{item.extra}</div>
+            </div>
+          );
+          const ListItemDesc = (
+            <div>
+              <div
+                className={styles.description}
+                title={typeof item.description === 'string' ? item.description : void 0}
+              >
+                {item.description}
+              </div>
+              <div className={styles.datetime}>{item.datetime}</div>
+            </div>
+          );
           return (
             <List.Item className={itemCls} key={item.key || i} onClick={() => onClick(item)}>
               <List.Item.Meta
                 className={styles.meta}
                 avatar={leftIcon}
-                title={
-                  <div className={styles.title}>
-                    {item.title}
-                    <div className={styles.extra}>{item.extra}</div>
-                  </div>
-                }
-                description={
-                  <div>
-                    <div className={styles.description} title={item.description}>
-                      {item.description}
-                    </div>
-                    <div className={styles.datetime}>{item.datetime}</div>
-                  </div>
-                }
+                title={ListItemTitle}
+                description={ListItemDesc}
               />
             </List.Item>
           );
@@ -84,7 +106,7 @@ const NoticeList: React.FC<NoticeIconTabProps> = ({
       <div className={styles.bottomBar}>
         {showClear ? (
           <div onClick={onClear}>
-            {locale.clear} {locale[title] || title}
+            {locale.clear} {tabTitle && (locale![tabTitle] || tabTitle)}
           </div>
         ) : null}
         {showViewMore ? <div onClick={onViewMore}>{locale.viewMore}</div> : null}
