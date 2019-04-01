3db981921e0aa59907b2c281dc990618fd1a06fa
jarrywen <jarrywen@163.com>
2019-04-01 18:02:56 +0800

    v0.1

--- src/pages/UniComp/Org.js
+++ src/pages/UniComp/Org.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import BindDataStandardTable from '../BindDataStandardTable';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
+import {getItems} from '@/utils/masterData';
 
-const orgTypes = [
-  { itemCode: '1', itemValue: '生产系统' },
-  { itemCode: '2', itemValue: '消费系统' },
-  { itemCode: '0', itemValue: 'Both' },
-];
+// const orgTypes = [
+//   { itemCode: '1', itemValue: '生产系统' },
+//   { itemCode: '2', itemValue: '消费系统' },
+//   { itemCode: '0', itemValue: 'Both' },
+// ];
+const orgTypes = getItems('org', 'orgType');
+const statusList = getItems('org', 'status');
 const columnSchemas = {
   tableName: 'org',
   key: 'id',
   name: 'orgName',
   columnDetails: [
-    { name: 'id', title: 'ID', add: true }, // 第一列需要作为查询条件，新增时不需要采集
-    { name: 'orgCode', title: 'Code', query: true, add: true }, // 第二列需要作为查询条件，新增时需要采集
+    { name: 'id', title: 'ID', add: true, disabledAct:'true' }, // 第一列需要作为查询条件，新增时不需要采集
+    { name: 'orgCode', title: 'Code', query: true}, // 第二列需要作为查询条件，新增时需要采集
     { name: 'orgName', title: 'Name', sorter: true, query: true, add: true }, //  需要排序，需要作为查询条件，新增时需要采集
     { name: 'createTime', title: 'Create Date', query: true, format: 'YYYY-MM-DD HH:mm:ss' }, // 返回是日期类型，需要转换
     { name: 'appKey', title: 'App Key', query: true, add: true },
@@ -26,6 +29,16 @@ const columnSchemas = {
       tag: 'commonSelect',
       tableName: 'org',
       enumData: orgTypes,
+    }, // 需要作为查询条件，新增时需要采集，需要使用绑定的下拉标签
+    {
+      name: 'status',
+      title: 'Status',
+      columnHidden: false,
+      query: false,
+      add: false,
+      tag: 'commonSelect',
+      tableName: 'org',
+      enumData: statusList,
     }, // 需要作为查询条件，新增时需要采集，需要使用绑定的下拉标签
   ],
 };
