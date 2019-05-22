1e14475b68845b471cbad751b5a8e74d4393dd80
高峰 <693614965@qq.com>
2019-05-22 15:46:20 +0800

    user

--- src/models/user.js
+++ src/models/user.js
@@ -9,15 +9,66 @@ export default {
       pagination: {},
     },
     currentUser: {},
+    user: {},
   },
 
   effects: {
-    *fetch(_, { call, put }) {
-      const response = yield call(queryUsers);
+    *fetch({ payload }, { call, put }) {
+      const response = yield call(queryUsers, payload);
       yield put({
         type: 'save',
         payload: response,
       });
+    },
+    *add({ payload }, { call, put }) {
+      const response = yield call(addUser, payload);
+      console.log(response);
+      if (response.code === 0) {
+        message.success('保存成功', 2);
+        router.push('/settings/user/list');
+      } else {
+        message.error(response.msg);
+      }
+    },
+    *get({ payload }, { call, put }) {
+      const response = yield call(queryById, payload);
+      yield put({
+        type: 'getSave',
+        payload: response,
+      });
+    },
+    *update({ payload }, { call, put }) {
+      const response = yield call(updateUser, payload);
+      if (response.code === 0) {
+        message.success('修改成功', 2);
+        router.push('/settings/user/list');
+      } else {
+        message.error(response.msg);
+      }
+    },
+    *delete({ payload }, { call, put }) {
+      const response = yield call(deleteById, payload);
+      if (response.code === 0) {
+        message.success('删除成功', 2);
+        yield put({
+          type: 'fetch',
+          payload: {},
+        });
+      } else {
+        message.error(response.msg);
+      }
+    },
+    *disable({ payload }, { call, put }) {
+      const response = yield call(disableUser, payload);
+      if (response.code === 0) {
+        message.success('操作成功', 2);
+        yield put({
+          type: 'fetch',
+          payload: {},
+        });
+      } else {
+        message.error(response.msg);
+      }
     },
     *fetchCurrent(_, { call, put }) {
       const response = yield call(queryCurrent);
@@ -46,6 +97,12 @@ export default {
       return {
         ...state,
         currentUser: action.payload || {},
+      };
+    },
+    getSave(state, action) {
+      return {
+        ...state,
+        user: action.payload.data || {},
       };
     },
     changeNotifyCount(state, action) {
--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,43 @@
 import request from '@/utils/request';
+import { stringify } from 'qs';
 
-export async function query() {
-  return request('/api/v1/user/page');
+export async function query(params) {
+  return request(`/api/v1/user/page?${stringify(params)}`);
+}
+
+export async function queryById(params) {
+  return request('/api/v1/user/get/' + params);
+}
+
+export async function deleteById(params) {
+  return request('/api/v1/user/delete/' + params, {
+    method: 'DELETE',
+  });
+}
+
+export async function addUser(params) {
+  return request('/api/v1/user/add', {
+    method: 'POST',
+    data: {
+      ...params,
+    },
+  });
+}
+
+export async function updateUser(params) {
+  return request('/api/v1/user/update', {
+    method: 'PUT',
+    data: {
+      ...params,
+    },
+  });
+}
+
+export async function disableUser(params) {
+  return request('/api/v1/user/disable', {
+    method: 'POST',
+    data: params,
+  });
 }
 
 export async function queryCurrent() {
