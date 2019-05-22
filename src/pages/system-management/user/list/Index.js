5063213a1a237338b9a964e7eade6d5bc7c555e9
高峰 <693614965@qq.com>
2019-05-22 18:20:32 +0800

    优化

--- src/pages/system-management/user/list/Index.js
+++ src/pages/system-management/user/list/Index.js
@@ -126,12 +126,7 @@ class UserList extends PureComponent {
         },
       ],
       render(val) {
-        if (val === 0) {
-          return <span style={{ color: 'blue' }}>{status[val]}</span>;
-        }
-        if (val === 1) {
-          return <span style={{ color: 'red' }}>{status[val]}</span>;
-        }
+        return <Badge status={statusMap[val]} text={status[val]} />;
       },
     },
     {
@@ -165,6 +160,56 @@ class UserList extends PureComponent {
       ),
     },
   ];
+
+  getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            this.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => this.handleSearch(selectedKeys, confirm)}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => (
+      <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    },
+    render: text => (
+      <Highlighter
+        highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+        searchWords={[this.state.searchText]}
+        autoEscape
+        textToHighlight={text.toString()}
+      />
+    ),
+  });
 
   handleEdit = record => {
     router.push('/system-management/user/edit/' + record.id);
