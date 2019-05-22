01e2f11035ffe67b0a9d380d550744235537ab82
jarrywen <jarrywen@163.com>
2019-05-22 15:39:06 +0800

    v0.1

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -28,7 +28,7 @@ const FooterView = () => (
       ]}
       copyright={
         <Fragment>
-          Copyright <Icon type="copyright" /> 2018 蚂蚁金服体验技术部出品
+          Copyright <Icon type="copyright" /> 2018 亚信远航
         </Fragment>
       }
     />
