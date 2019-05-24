9e4e132d28acd7e2275c7f9a30f7178bc303d0d2
jarrywen <jarrywen@163.com>
2019-05-24 17:14:52 +0800

    v0.1

--- src/models/user.js
+++ src/models/user.js
@@ -18,7 +18,63 @@ export default {
       });
     },
     *fetchCurrent(_, { call, put }) {
-      const response = yield call(queryCurrent);
+      let response = yield call(queryCurrent);
+      // console.log("------------",response);
+      if(!response.avatar){
+          const simulateUser={
+            name: 'Serati Ma',
+            avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
+            userid: '00000001',
+            id:1,
+            username:'admin',
+            email: 'liuyq6@asiainfo-int.com',
+            signature: '海纳百川，有容乃大',
+            title: '交互专家',
+            group: 'AsiaInfo－国际部－ODC－O2P－UI',
+            tags: [
+            {
+              key: '0',
+              label: '很有想法的',
+            },
+            {
+              key: '1',
+              label: '专注设计',
+            },
+            {
+              key: '2',
+              label: '辣~',
+            },
+            {
+              key: '3',
+              label: '大长腿',
+            },
+            {
+              key: '4',
+              label: '小姐姐',
+            },
+            {
+              key: '5',
+              label: '海纳百川',
+            },
+          ],
+            notifyCount: 12,
+            unreadCount: 11,
+            country: 'China',
+            geographic: {
+            province: {
+              label: '福建省',
+                key: '350000',
+            },
+            city: {
+              label: '福州市',
+                key: '350100',
+            },
+          },
+            address: '福州 77 号',
+            phone: '0591-268888888',
+        };
+          response={...simulateUser,...response};
+      }
       setUser(response);
       yield put({
         type: 'saveCurrentUser',
