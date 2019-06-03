1e4819c39967dab643608ed5366a4208c36a3926
陈帅 <qixian.cs@outlook.com>
2019-06-03 18:41:38 +0800

    edit style

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,14 +5,18 @@ export function isUrl(path: string) {
   return reg.test(path);
 }
 
-// 给官方演示站点用，用于关闭真实开发环境不需要使用的特性
-export function isAntDesignProOrDev() {
-  const { NODE_ENV } = process.env;
+export function isAntDesignPro() {
   if (ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION === 'site') {
     return true;
   }
+  return window.location.hostname === 'preview.pro.ant.design';
+}
+
+// 给官方演示站点用，用于关闭真实开发环境不需要使用的特性
+export function isAntDesignProOrDev() {
+  const { NODE_ENV } = process.env;
   if (NODE_ENV === 'development') {
     return true;
   }
-  return window.location.hostname === 'preview.pro.ant.design';
+  return isAntDesignPro();
 }
