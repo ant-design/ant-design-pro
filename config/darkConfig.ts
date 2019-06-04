157a5bafd8a4510131f52f376e09b016f8ea931b
陈帅 <qixian.cs@outlook.com>
2019-06-04 17:16:03 +0800

    change config  name

---
+++ config/darkConfig.ts
@@ -0,0 +1,39 @@
+import { blue, red, gold } from '@ant-design/colors';
+
+// All antd theme variables: https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+
+export default {
+  '@body-background': '#000C17',
+  '@component-background': '#08121C',
+  '@pro-header-background': '#000C17',
+  '@layout-sider-background': '#001529',
+  '@text-color': '#D9D9D9',
+  '@text-color-secondary': '#D9D9D9',
+  '@heading-color': 'fade(#D9D9D9,85%)',
+  '@layout-body-background': '#000C17',
+  '@pro-header-box-shadow': '0 1px 4px 0 rgba(0,21,41,0.12)',
+  '@btn-default-bg': '#13222E',
+  '@btn-primary-bg': '#052F61',
+  '@border-color-split': '#172C41',
+  '@input-bg': '#08121C',
+  '@border-color-base': 'rgba(255, 255, 255, 0.25)',
+  '@btn-default-border': '#13222E',
+
+  '@table-header-bg': '#0A1A2B',
+  '@table-row-hover-bg': '#3a3a3b',
+  '@table-selected-row-bg': '#3a3a3a',
+
+  '@tag-default-bg': '#13222E',
+  '@alert-info-bg-color': '#102134',
+  '@highlight-color': red[7],
+  '@warning-color': gold[9],
+  '@card-actions-background': '#08121C',
+  '@primary-color': '#549BF9',
+  '@item-hover-bg': `fade(${blue[5]}, 20%)`,
+  '@item-active-bg': `fade(${blue[5]}, 40%)`,
+  '@checkbox-check-color': '#13222E',
+  '@disabled-color': '#404C56',
+  '@input-disabled-bg': '#404C56',
+
+  '@background-color-light': '#001529',
+};
