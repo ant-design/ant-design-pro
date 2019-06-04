d40828926d8f24f72e02db029b4896f2966ca76e
490145346@qq.com <490145346@qq.com>
2019-06-04 14:32:06 +0800

    2019-6-4 whd Api文档修改-数据转化修改

--- src/pages/ApiGateway/ApiDocUpdate.js
+++ src/pages/ApiGateway/ApiDocUpdate.js
@@ -1,13 +1,13 @@
-import React, { PureComponent} from 'react';
-import { Card, Button, Form,  Input,  Tabs, BackTop,message,Row,Col,Icon,Popover, } from 'antd';
+import React, { PureComponent } from 'react';
+import { Card, Button, Form, Input, Tabs, BackTop, message, Row, Col, Icon, Popover } from 'antd';
 import router from 'umi/router';
 import { connect } from 'dva';
 import FooterToolbar from '@/components/FooterToolbar';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 import ApiDocTableForm from './ApiDocTableForm';
 import styles from './style.less';
-import { getPayloadForApiDoc} from './ApiCreate/util';
-import {getPlaceHolder,getQueryArr,isJson,toApiSpecJson, toType} from "../util";
+import { getPayloadForApiDoc } from './ApiCreate/util';
+import { getPlaceHolder, getQueryArr, isJson, toApiSpecJson, toType } from '../util';
 
 // import RoleTransfer from "../UserManager/Privilege";
 // import apiFlowData from '../Editor/GGEditor/mock/apiFlow.json';
@@ -15,13 +15,12 @@ import {getPlaceHolder,getQueryArr,isJson,toApiSpecJson, toType} from "../util";
 const { TabPane } = Tabs;
 const { TextArea } = Input;
 
-const requestHeaderFlag="requestHeader";
-const requestBodyFlag="requestBody";
-const responseBodyFlag="responseBody";
-const responseHeaderFlag="responseHeader";
-const stateCodeFlag="stateCode";
-const busiCodeFlag="busiCode";
-
+const requestHeaderFlag = 'requestHeader';
+const requestBodyFlag = 'requestBody';
+const responseBodyFlag = 'responseBody';
+const responseHeaderFlag = 'responseHeader';
+const stateCodeFlag = 'stateCode';
+const busiCodeFlag = 'busiCode';
 
 const fieldLabels = {
   requestBodySample: 'request Body Sample',
@@ -36,14 +35,14 @@ const fieldLabels = {
 class ApiUpdate extends PureComponent {
   state = {
     width: '100%',
-    apiServiceDoc:{},
-    urlSpec:[],
+    apiServiceDoc: {},
+    urlSpec: [],
     requestHeaderSpec: [],
     requestBodySpec: [],
     responseHeaderSpec: [],
     responseBodySpec: [],
-    stateCodeSpec:[],
-    busiCodeSpec:[],
+    stateCodeSpec: [],
+    busiCodeSpec: [],
   };
 
   componentDidMount() {
@@ -64,8 +63,8 @@ class ApiUpdate extends PureComponent {
     const { dispatch } = this.props;
     if (apiId !== -1) {
       const payload = {};
-      payload.option=4
-      payload.range=1;
+      payload.option = 4;
+      payload.range = 1;
       payload.data = {};
       payload.data.info = {};
       payload.data.info.apiId = apiId;
@@ -81,42 +80,56 @@ class ApiUpdate extends PureComponent {
     }
   };
 
-  convertDocObj = (apiServiceDoc,flag)=>{
+  convertDocObj = (apiServiceDoc, flag) => {
     try {
-      if(apiServiceDoc){
-        const spec=apiServiceDoc[`${flag}Spec`];
-        console.log(flag,spec);
-        if(spec&&spec.trim()!==""){
-          const specArr=(JSON.parse(apiServiceDoc.requestHeaderSpec)||[]).map((item,index)=>({...item,key:`${requestHeaderFlag}-${index}`}));
-          console.log(flag,specArr);
+      if (apiServiceDoc) {
+        const spec = apiServiceDoc[`${flag}Spec`];
+        console.log(flag, spec);
+        if (spec && spec.trim() !== '') {
+          const specArr = (JSON.parse(spec) || []).map((item, index) => ({
+            ...item,
+            key: `${requestHeaderFlag}-${index}`,
+          }));
+          console.log(flag, specArr);
           return specArr;
         }
       }
-    }catch (e) {
+    } catch (e) {
       console.log(e);
     }
     return [];
-  }
+  };
 
   setBaseInfo = resp => {
-    const { data} = resp;
-    const apiServiceDoc=data&&data.apiServiceDoc?data.apiServiceDoc:{};
+    const { data } = resp;
+    const apiServiceDoc = data && data.apiServiceDoc ? data.apiServiceDoc : {};
     // 从db里面获取字符串数据，再转成json对象，在增加key字段，赋值给表格组件
-    const requestHeaderSpec=this.convertDocObj(apiServiceDoc,requestHeaderFlag);
-    const requestBodySpec=this.convertDocObj(apiServiceDoc,requestBodyFlag);
-    const responseHeaderSpec=this.convertDocObj(apiServiceDoc,responseHeaderFlag);
-    const responseBodySpec=this.convertDocObj(apiServiceDoc,responseBodyFlag);
-    const stateCodeSpec=this.convertDocObj(apiServiceDoc,stateCodeFlag);
-    const busiCodeSpec=this.convertDocObj(apiServiceDoc,busiCodeFlag);
+    const requestHeaderSpec = this.convertDocObj(apiServiceDoc, requestHeaderFlag);
+    const requestBodySpec = this.convertDocObj(apiServiceDoc, requestBodyFlag);
+    const responseHeaderSpec = this.convertDocObj(apiServiceDoc, responseHeaderFlag);
+    const responseBodySpec = this.convertDocObj(apiServiceDoc, responseBodyFlag);
+    const stateCodeSpec = this.convertDocObj(apiServiceDoc, stateCodeFlag);
+    const busiCodeSpec = this.convertDocObj(apiServiceDoc, busiCodeFlag);
     // －－－－－初始化url spec数据－－－－－
-    const urlSpec=(JSON.parse(apiServiceDoc.urlSpec)||[]).map((item,index)=>({...item,key:`url-${index}`}));
-    console.log("urlSpec1:",urlSpec);
-    const newUrlSpec=urlSpec.length===0?this.handleUrlGenerate(urlSpec,false):urlSpec;
-    console.log("newUrlSpec:",newUrlSpec);
+    const urlSpec = (JSON.parse(apiServiceDoc.urlSpec) || []).map((item, index) => ({
+      ...item,
+      key: `url-${index}`,
+    }));
+    console.log('urlSpec1:', urlSpec);
+    const newUrlSpec = urlSpec.length === 0 ? this.handleUrlGenerate(urlSpec, false) : urlSpec;
+    console.log('newUrlSpec:', newUrlSpec);
     // －－－－－初始化request header数据－－－－－
-    this.handleBodyGenerate(requestHeaderFlag,requestHeaderSpec);
+    this.handleBodyGenerate(requestHeaderFlag, requestHeaderSpec);
 
-    this.setState({apiServiceDoc,urlSpec:newUrlSpec,requestBodySpec,responseHeaderSpec,responseBodySpec,stateCodeSpec,busiCodeSpec});
+    this.setState({
+      apiServiceDoc,
+      urlSpec: newUrlSpec,
+      requestBodySpec,
+      responseHeaderSpec,
+      responseBodySpec,
+      stateCodeSpec,
+      busiCodeSpec,
+    });
   };
 
   /**
@@ -125,37 +138,40 @@ class ApiUpdate extends PureComponent {
    * @param manualFlag
    * @returns {Array}
    */
-  handleUrlGenerate=(urlSpec,manualFlag)=>{
-
+  handleUrlGenerate = (urlSpec, manualFlag) => {
     const { apiService } = this.props;
     const { requestUrl } = apiService;
-    let url=requestUrl;
-    if(manualFlag){
+    let url = requestUrl;
+    if (manualFlag) {
       const { form } = this.props;
-      const fieldsValue=form.getFieldsValue();
-      const {urlSample} =fieldsValue;
-      url=urlSample;
+      const fieldsValue = form.getFieldsValue();
+      const { urlSample } = fieldsValue;
+      url = urlSample;
     }
-    let retAttr=[];
-    if(url&&url.trim()!==""&&url.indexOf("{")>-1){
-      const flatJsonArray=getPlaceHolder(url);
-      retAttr=flatJsonArray.map((spec)=>{
-        const findObj=urlSpec.find((item)=>(item.name===spec.name&&item.parent===spec.parent));
-        return findObj?{...spec,remark:findObj.remark}:spec;
-      })
+    let retAttr = [];
+    if (url && url.trim() !== '' && url.indexOf('{') > -1) {
+      const flatJsonArray = getPlaceHolder(url);
+      retAttr = flatJsonArray.map(spec => {
+        const findObj = urlSpec.find(
+          item => item.name === spec.name && item.parent === spec.parent
+        );
+        return findObj ? { ...spec, remark: findObj.remark } : spec;
+      });
     }
-    if(url&&url.trim()!==""&&url.indexOf("?")>-1){
-      const flatJsonArray=getQueryArr(url);
-      const mergeArr=flatJsonArray.map((spec)=>{
-        const findObj=retAttr.find((item)=>(item.name===spec.name&&item.parent===spec.parent));
-        return findObj?{...spec,remark:findObj.remark}:spec;
-      })
-      retAttr=retAttr.concat(mergeArr);
+    if (url && url.trim() !== '' && url.indexOf('?') > -1) {
+      const flatJsonArray = getQueryArr(url);
+      const mergeArr = flatJsonArray.map(spec => {
+        const findObj = retAttr.find(
+          item => item.name === spec.name && item.parent === spec.parent
+        );
+        return findObj ? { ...spec, remark: findObj.remark } : spec;
+      });
+      retAttr = retAttr.concat(mergeArr);
     }
 
-    retAttr=retAttr.map((item,index)=>({...item,key:`url-${index}`}));
-    if(manualFlag){
-      this.setState({urlSpec:retAttr});
+    retAttr = retAttr.map((item, index) => ({ ...item, key: `url-${index}` }));
+    if (manualFlag) {
+      this.setState({ urlSpec: retAttr });
     }
     return retAttr;
   };
@@ -165,45 +181,48 @@ class ApiUpdate extends PureComponent {
    * @param typeParam
    * @param oldSpec
    */
-  handleBodyGenerate=(typeParam,oldSpec)=>{
-
+  handleBodyGenerate = (typeParam, oldSpec) => {
     const { form } = this.props;
-    const fieldsValue=form.getFieldsValue();
-    let newValue="";
+    const fieldsValue = form.getFieldsValue();
+    let newValue = '';
     switch (typeParam) {
       case requestHeaderFlag:
-        newValue="[{\"appkey\":\"xxxx\"}]";
+        newValue = '[{"appkey":"xxxx"}]';
         break;
       case requestBodyFlag:
-        newValue=fieldsValue[`${requestBodyFlag}Sample`];
+        newValue = fieldsValue[`${requestBodyFlag}Sample`];
         break;
       case responseBodyFlag:
-        newValue=fieldsValue[`${responseBodyFlag}Sample`];
+        newValue = fieldsValue[`${responseBodyFlag}Sample`];
         break;
       default:
         break;
-    };
-    if(newValue&&newValue.trim()!==""){
-      const isJsonResult=isJson(newValue);
-      if(isJsonResult.result){
-        const newValueJson=JSON.parse(newValue);
-        const parentValue=typeParam===requestHeaderFlag?'-':'root';
-        const flatJsonArray=typeParam===requestHeaderFlag?[]:[{name:'root',type:toType(newValueJson),remark:'root element',parent:"-"}];
-        const requestBodySampleJsonOne=toType(newValueJson)==='array'?newValueJson[0]:newValueJson;
-        toApiSpecJson(requestBodySampleJsonOne,flatJsonArray,parentValue);
-        const mergeArr=flatJsonArray.map((spec)=>{
-          const findObj=oldSpec.find((item)=>(item.name===spec.name&&item.parent===spec.parent));
-          return findObj?{...spec,remark:findObj.remark}:spec;
-        })
-        const specArr=mergeArr.map((item,index)=>({...item,key:`${typeParam}-${index}`}));
-        this.setState({[`${typeParam}Spec`]:specArr});
-      }
-      else{
+    }
+    if (newValue && newValue.trim() !== '') {
+      const isJsonResult = isJson(newValue);
+      if (isJsonResult.result) {
+        const newValueJson = JSON.parse(newValue);
+        const parentValue = typeParam === requestHeaderFlag ? '-' : 'root';
+        const flatJsonArray =
+          typeParam === requestHeaderFlag
+            ? []
+            : [{ name: 'root', type: toType(newValueJson), remark: 'root element', parent: '-' }];
+        const requestBodySampleJsonOne =
+          toType(newValueJson) === 'array' ? newValueJson[0] : newValueJson;
+        toApiSpecJson(requestBodySampleJsonOne, flatJsonArray, parentValue);
+        const mergeArr = flatJsonArray.map(spec => {
+          const findObj = oldSpec.find(
+            item => item.name === spec.name && item.parent === spec.parent
+          );
+          return findObj ? { ...spec, remark: findObj.remark } : spec;
+        });
+        const specArr = mergeArr.map((item, index) => ({ ...item, key: `${typeParam}-${index}` }));
+        this.setState({ [`${typeParam}Spec`]: specArr });
+      } else {
         message.error(isJsonResult.msg);
       }
     }
   };
-
 
   resizeFooterToolbar = () => {
     requestAnimationFrame(() => {
@@ -217,7 +236,6 @@ class ApiUpdate extends PureComponent {
       }
     });
   };
-
 
   getErrorInfo = () => {
     const {
@@ -287,24 +305,32 @@ class ApiUpdate extends PureComponent {
     });
   };
 
-
   render() {
     const {
       form: { getFieldDecorator },
       submitting,
       apiService,
     } = this.props;
-    const { width, apiServiceDoc,urlSpec,requestBodySpec,requestHeaderSpec,responseHeaderSpec,responseBodySpec,stateCodeSpec,busiCodeSpec } = this.state;
-    console.log("urlSpec:",urlSpec);
+    const {
+      width,
+      apiServiceDoc,
+      urlSpec,
+      requestBodySpec,
+      requestHeaderSpec,
+      responseHeaderSpec,
+      responseBodySpec,
+      stateCodeSpec,
+      busiCodeSpec,
+    } = this.state;
+    console.log('urlSpec:', urlSpec);
     // const apiServiceBackendMembers1  = apiService.apiServiceBackends.filter((obj)=>obj.backendType!=="endpoint");
-    const sampleText="sample for post:{\"type\":\"xxx\",\"name\":\"xxx\"}";
+    const sampleText = 'sample for post:{"type":"xxx","name":"xxx"}';
     return (
       <PageHeaderWrapper
         onBack={() => window.history.back()}
         style={{ height: '50px' }}
         title="Compile Api Specification"
       >
-
         <Tabs defaultActiveKey="1">
           <TabPane tab="Url" key="1">
             <Card title="" className={styles.card} bordered={false}>
@@ -316,19 +342,33 @@ class ApiUpdate extends PureComponent {
                   sm={24}
                   style={{ height: 50 }}
                 >
-                  <Form.Item label="Request Method" labelCol={{ span:12  }} wrapperCol={{ span: 12 }}>
+                  <Form.Item
+                    label="Request Method"
+                    labelCol={{ span: 12 }}
+                    wrapperCol={{ span: 12 }}
+                  >
                     {getFieldDecorator('reqMethod', {
                       initialValue: apiService.reqMethod,
                       rules: [],
-                    })(<Input placeholder='request Method' disabled />)}
+                    })(<Input placeholder="request Method" disabled />)}
                   </Form.Item>
                 </Col>
                 <Col lg={18} md={12} sm={24} style={{ height: 50 }}>
                   <Form.Item label="Request Path" labelCol={{ span: 4 }} wrapperCol={{ span: 20 }}>
                     {getFieldDecorator('urlSample', {
-                      initialValue: apiServiceDoc.urlSample&&apiServiceDoc.urlSample.trim()!==""?apiServiceDoc.urlSample:apiService.requestUrl,
-                      rules: [{ required: false, message: 'urlSample must be between 2 and 500 characters！', min:2, max: 500 }],
-                    })(<Input placeholder='request Url' />)}
+                      initialValue:
+                        apiServiceDoc.urlSample && apiServiceDoc.urlSample.trim() !== ''
+                          ? apiServiceDoc.urlSample
+                          : apiService.requestUrl,
+                      rules: [
+                        {
+                          required: false,
+                          message: 'urlSample must be between 2 and 500 characters！',
+                          min: 2,
+                          max: 500,
+                        },
+                      ],
+                    })(<Input placeholder="request Url" />)}
                   </Form.Item>
                 </Col>
               </Row>
@@ -337,11 +377,11 @@ class ApiUpdate extends PureComponent {
                 type="dashed"
                 icon="arrow-down"
                 htmlType="button"
-                onClick={()=>this.handleUrlGenerate(urlSpec,true)}
+                onClick={() => this.handleUrlGenerate(urlSpec, true)}
               >
                 Generate Specification By Url
               </Button>
-              <Form.Item label='Url PlaceHolders Specification'>
+              <Form.Item label="Url PlaceHolders Specification">
                 {getFieldDecorator('urlSpec', {
                   initialValue: urlSpec,
                 })(<ApiDocTableForm />)}
@@ -368,12 +408,16 @@ class ApiUpdate extends PureComponent {
                 type="dashed"
                 icon="arrow-down"
                 htmlType="button"
-                onClick={()=>this.handleBodyGenerate(requestBodyFlag,requestBodySpec)}
+                onClick={() => this.handleBodyGenerate(requestBodyFlag, requestBodySpec)}
               >
                 Generate Specification By Sample
               </Button>
             </Card>
-            <Card title="Request Parameter Body Specification" className={styles.card} bordered={false}>
+            <Card
+              title="Request Parameter Body Specification"
+              className={styles.card}
+              bordered={false}
+            >
               {getFieldDecorator('requestBodySpec', {
                 initialValue: requestBodySpec,
               })(<ApiDocTableForm />)}
@@ -392,19 +436,29 @@ class ApiUpdate extends PureComponent {
                 {getFieldDecorator(`${responseBodyFlag}Sample`, {
                   initialValue: apiServiceDoc.responseBodySample,
                   rules: [{ required: false, message: '不能超过5000字符！', max: 5000 }],
-                })(<TextArea rows={4} placeholder={sampleText} autosize={{ minRows: 4, maxRows: 15 }} />)}
+                })(
+                  <TextArea
+                    rows={4}
+                    placeholder={sampleText}
+                    autosize={{ minRows: 4, maxRows: 15 }}
+                  />
+                )}
               </Form.Item>
               <Button
                 style={{ width: '100%', marginTop: 16, marginBottom: 8 }}
                 type="dashed"
                 icon="arrow-down"
                 htmlType="button"
-                onClick={()=>this.handleBodyGenerate(responseBodyFlag,responseBodySpec)}
+                onClick={() => this.handleBodyGenerate(responseBodyFlag, responseBodySpec)}
               >
                 Generate Specification By Sample
               </Button>
             </Card>
-            <Card title="Response Parameter Body Specification" className={styles.card} bordered={false}>
+            <Card
+              title="Response Parameter Body Specification"
+              className={styles.card}
+              bordered={false}
+            >
               {getFieldDecorator('responseBodySpec', {
                 initialValue: responseBodySpec,
               })(<ApiDocTableForm />)}
