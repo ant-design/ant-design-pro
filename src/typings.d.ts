85aa74202abb9380e34037ff1041f25e277e2cb5
陈帅 <qixian.cs@outlook.com>
2019-06-06 18:47:31 +0800

    add block  a analytics to ga

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -20,6 +20,7 @@ declare module 'react-fittext';
 declare module '@antv/data-set';
 declare module 'nzh/cn';
 
+declare let ga: Function;
 // preview.pro.ant.design only do not use in your production ;
 // preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
 declare let ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: 'site' | undefined;
