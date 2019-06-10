1b9fe7ed391accff39dbb302701470906d0053ab
liangyongrui <leungyongrui@gmail.com>
2019-06-10 22:41:50 +0800

    prettier

--- scripts/fetch-blocks.js
+++ scripts/fetch-blocks.js
@@ -94,9 +94,7 @@ const installBlock = async () => {
       console.log('install ' + chalk.green(item.name) + ' to: ' + chalk.yellow(item.path));
       gitFiles = gitFiles.filter(file => file.path !== gitPath);
       const skipModifyRouter = item.routes ? '--skip-modify-routes' : '';
-      const cmd = `umi block add https://github.com/ant-design/pro-blocks/tree/master/${gitPath}  --path=${
-        item.path
-      } ${skipModifyRouter}`;
+      const cmd = `umi block add https://github.com/ant-design/pro-blocks/tree/master/${gitPath}  --path=${item.path} ${skipModifyRouter}`;
       try {
         await execCmd(cmd);
         console.log(`install ${chalk.hex('#1890ff')(item.name)} success`);
