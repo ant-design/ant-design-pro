a898e7cd59ae0a04b48ed8998746f3fbc9cb3eb4
jarrywen <jarrywen@163.com>
2019-06-11 10:13:28 +0800

    没有选择复选框，自动隐藏选择多少个的提示信息

--- src/locales/en-US/login.js
+++ src/locales/en-US/login.js
@@ -1,7 +1,7 @@
 export default {
   'app.login.userName': 'userName',
   'app.login.password': 'password',
-  'app.login.message-invalid-credentials': 'Invalid username or password（admin/ant.design）',
+  'app.login.message-invalid-credentials': 'Invalid username or password',
   'app.login.message-invalid-verification-code': 'Invalid verification code',
   'app.login.tab-login-credentials': 'Credentials',
   'app.login.tab-login-mobile': 'Mobile number',
--- src/locales/zh-CN/login.js
+++ src/locales/zh-CN/login.js
@@ -1,7 +1,7 @@
 export default {
   'app.login.userName': '用户名',
   'app.login.password': '密码',
-  'app.login.message-invalid-credentials': '账户或密码错误（admin/ant.design）',
+  'app.login.message-invalid-credentials': '账户或密码错误',
   'app.login.message-invalid-verification-code': '验证码错误',
   'app.login.tab-login-credentials': '账户密码登录',
   'app.login.tab-login-mobile': '手机号登录',
