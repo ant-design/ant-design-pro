8cd940dd41976ade50d546581d80281d2b2b60ed
jarrywen <jarrywen@163.com>
2019-06-19 10:52:15 +0800

    org detail

--- src/models/uniComp.js
+++ src/models/uniComp.js
@@ -1,6 +1,6 @@
 import {list, save, statusBatch, token, del} from '../services/uniCompService';
 import {conversion, conversionReq} from "../pages/util";
-// ddd
+
 export default {
   namespace: 'uniComp',
 
@@ -69,17 +69,17 @@ export default {
       // console.log("response in submitRelation model---:",response);
       if (callback) callback(response);
     },
-    // *detail({ payload, callback }, { call }) {
-    //   // console.log('sysdata detail request in Model:', payload);
-    //   const response = yield call(detail, payload);
-    //   // console.log('sysdata detail response in Model:', response,(!response||response.code!=="200"));
-    //   if(!response||response.code!=="200"){
-    //     response.data={};
-    //     const errorMsg=response.msg&&response.msg!==""?response.msg:`Sorry, the server is reporting an error,code=${response.code}`;
-    //     message.error(errorMsg);
-    //   }
-    //   if (callback) callback(response.data);
-    // },
+    *detail({ payload, callback }, { call }) {
+      // console.log('sysdata detail request in Model:', payload);
+      const response = yield call(detail, payload);
+      // console.log('sysdata detail response in Model:', response,(!response||response.code!=="200"));
+      if(!response||response.code!=="200"){
+        response.data={};
+        const errorMsg=response.msg&&response.msg!==""?response.msg:`Sorry, the server is reporting an error,code=${response.code}`;
+        message.error(errorMsg);
+      }
+      if (callback) callback(response.data);
+    },
   },
 
   reducers: {
