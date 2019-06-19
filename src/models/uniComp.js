f8430a2e591bcf50c514965dfd9d81bfc53047e0
jarrywen <jarrywen@163.com>
2019-06-19 15:26:17 +0800

    test

--- src/models/uniComp.js
+++ src/models/uniComp.js
@@ -1,5 +1,5 @@
 import message from 'antd';
-import {list, save, statusBatch, token, del} from '../services/uniCompService';
+import {list, save, statusBatch, detail, token, del} from '../services/uniCompService';
 import {conversion, conversionReq} from "../pages/util";
 
 export default {
