4ccd39f875073181a3fc78bcf7fb1f57d34eb169
陈帅 <qixian.cs@outlook.com>
2019-06-21 16:20:26 +0800

    add magic!!!! code

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -48,8 +48,57 @@ const request = extend({
 });
 
 //
-window.oldFetch = window.fetch;
-window.fetch = (url, option) =>
-  window.oldFetch(`https://5d0c8b8dfee8cf0008c984e4--ant-design-pro.netlify.com${url}`, option);
-
+(() => {
+  var __assign =
+    (this && this.__assign) ||
+    function() {
+      __assign =
+        Object.assign ||
+        function(t) {
+          for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+          }
+          return t;
+        };
+      return __assign.apply(this, arguments);
+    };
+  var prevFetch = window.fetch;
+  var enableAudit = false;
+  function urlHasOwnProtocol(url) {
+    return url.startsWith('http://') || url.startsWith('https://') || url.startsWith('//');
+  }
+  var isStaticAsset = function(url) {
+    return /\.html\??\S*/g.test(url) || /\.js\??\S*/g.test(url) || /\.css\??\S*/g.test(url);
+  };
+  var getEntireUrl = function(url) {
+    if (!urlHasOwnProtocol(url)) {
+      return url.startsWith('/') ? url : '//' + url;
+    }
+    return url;
+  };
+  /*
+       只处理本身 url 不带协议的请求
+        1. 当开启操作审计时，除静态资源请求外所有接口直接走当前域，同时带上相应的头信息
+        2. 当未开启操作审计时，请求转换为跨域请求调用
+       */
+  window.fetch = function(url, options) {
+    if (options === void 0) {
+      options = {};
+    }
+    if (!urlHasOwnProtocol(url)) {
+      if (enableAudit && !isStaticAsset(url)) {
+        // 开启操作审计时带上相应头信息
+        var auditOptions = __assign({}, options, { headers: __assign({}, options.headers) });
+        return prevFetch.call(window, url, auditOptions);
+      }
+      // 未开启操作审计请求以跨域方式发出
+      var corsOptions = __assign({}, options, { mode: 'cors', credentials: 'include' });
+      // 发起跨域请求
+      var entireUrl = getEntireUrl(url);
+      return prevFetch.call(window, entireUrl, corsOptions);
+    }
+    return prevFetch.call(window, url, options);
+  };
+})();
 export default request;
