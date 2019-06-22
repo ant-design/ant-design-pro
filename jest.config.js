e4186a4360c67b9f32ade556770720bb1d5ab2da
陈帅 <qixian.cs@outlook.com>
2019-06-22 18:24:09 +0800

    delete extra comments

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   preset: 'jest-puppeteer',
   extraSetupFiles: ['./tests/setupTests.js'],
   globals: {
-    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: false, // preview.pro.ant.design only do not use in your production ; preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
+    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: false,
   },
 };
