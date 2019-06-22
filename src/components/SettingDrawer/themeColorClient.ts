14d74c52dfd2caeeebe6dd2b571e16c9c8fc42ec
hz <hz932@qq.com>
2019-06-23 00:21:59 +0800

    ts

--- src/components/SettingDrawer/themeColorClient.ts
+++ src/components/SettingDrawer/themeColorClient.ts
@@ -4,17 +4,20 @@ import client from 'webpack-theme-color-replacer/client';
 import generate from '@ant-design/colors/lib/generate';
 
 export default {
-  getAntdSerials(color) {
+  getAntdSerials(color: string) {
     // 淡化（即less的tint）
-    const lightens = new Array(9).fill(0).map((_, i) => client.varyColor.lighten(color, i / 10));
+    let lightens = new Array(9).fill(0);
+    lightens = lightens.map((_, i) => client.varyColor.lighten(color, i / 10));
     const colorPalettes = generate(color);
     return lightens.concat(colorPalettes);
   },
   changeColor(newColor: string) {
     const options = {
-      newColors: this.getAntdSerials(newColor), // new colors array, one-to-one corresponde with `matchColors`
-      changeUrl(cssUrl) {
-        return `/${cssUrl}`; // while router is not `hash` mode, it needs absolute path
+      // new colors array, one-to-one corresponde with `matchColors`
+      newColors: this.getAntdSerials(newColor),
+      changeUrl(cssUrl: string) {
+        // while router is not `hash` mode, it needs absolute path
+        return `/${cssUrl}`;
       },
     };
     return client.changer.changeColor(options, Promise);
