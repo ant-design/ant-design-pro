cf17a02410fa65e1b5548a4b6b5f0bd49a6a9c93
raohong <724736528@qq.com>
2019-06-30 07:01:18 +0800

    v2.2.2 添加示例

--- config/plugin.config.js
+++ config/plugin.config.js
@@ -1,33 +1,42 @@
 // Change theme plugin
 
-import MergeLessPlugin from 'antd-pro-merge-less';
-import AntDesignThemePlugin from 'antd-theme-webpack-plugin';
-import path from 'path';
+// import MergeLessPlugin from 'antd-pro-merge-less';
+// import AntDesignThemePlugin from 'antd-theme-webpack-plugin';
+// import path from 'path';
+import AntdColorReplacer from 'antd-color-replacer';
+
+import defaultSettings from '../src/defaultSettings';
 
 export default config => {
   // pro 和 开发环境再添加这个插件
-  if (process.env.APP_TYPE === 'site' || process.env.NODE_ENV !== 'production') {
-    // 将所有 less 合并为一个供 themePlugin使用
-    const outFile = path.join(__dirname, '../.temp/ant-design-pro.less');
-    const stylesDir = path.join(__dirname, '../src/');
+  //if (process.env.APP_TYPE === 'site' || process.env.NODE_ENV !== 'production') {
+  //  // 将所有 less 合并为一个供 themePlugin使用
+  //  const outFile = path.join(__dirname, '../.temp/ant-design-pro.less');
+  //  const stylesDir = path.join(__dirname, '../src/');
+  //
+  //  config.plugin('merge-less').use(MergeLessPlugin, [
+  //    {
+  //      stylesDir,
+  //      outFile,
+  //    },
+  //  ]);
+  //
+  //  config.plugin('ant-design-theme').use(AntDesignThemePlugin, [
+  //    {
+  //      antDir: path.join(__dirname, '../node_modules/antd'),
+  //      stylesDir,
+  //      varFile: path.join(__dirname, '../node_modules/antd/lib/style/themes/default.less'),
+  //      mainLessFile: outFile, //     themeVariables: ['@primary-color'],
+  //      indexFileName: 'index.html',
+  //      generateOne: true,
+  //      lessUrl: 'https://gw.alipayobjects.com/os/lib/less.js/3.8.1/less.min.js',
+  //    },
+  //  ]);
+  //}
 
-    config.plugin('merge-less').use(MergeLessPlugin, [
-      {
-        stylesDir,
-        outFile,
-      },
-    ]);
-
-    config.plugin('ant-design-theme').use(AntDesignThemePlugin, [
-      {
-        antDir: path.join(__dirname, '../node_modules/antd'),
-        stylesDir,
-        varFile: path.join(__dirname, '../node_modules/antd/lib/style/themes/default.less'),
-        mainLessFile: outFile, //     themeVariables: ['@primary-color'],
-        indexFileName: 'index.html',
-        generateOne: true,
-        lessUrl: 'https://gw.alipayobjects.com/os/lib/less.js/3.8.1/less.min.js',
-      },
-    ]);
-  }
+  config.plugin('antd-color-replacer').use(AntdColorReplacer, [
+    {
+      primaryColor: defaultSettings.primaryColor,
+    },
+  ]);
 };
--- src/models/setting.js
+++ src/models/setting.js
@@ -1,63 +1,73 @@
 import { message } from 'antd';
+import { AntdColorReplacerClient } from 'antd-color-replacer';
+
 import defaultSettings from '../defaultSettings';
 
-let lessNodesAppended;
 const updateTheme = primaryColor => {
-  // Don't compile less in production!
-  if (APP_TYPE !== 'site') {
-    return;
-  }
-  // Determine if the component is remounted
-  if (!primaryColor) {
-    return;
-  }
-  const hideMessage = message.loading('正在编译主题！', 0);
-  function buildIt() {
-    if (!window.less) {
-      return;
-    }
-    setTimeout(() => {
-      window.less
-        .modifyVars({
-          '@primary-color': primaryColor,
-        })
-        .then(() => {
-          hideMessage();
-        })
-        .catch(() => {
-          message.error('Failed to update theme');
-          hideMessage();
-        });
-    }, 200);
-  }
-  if (!lessNodesAppended) {
-    // insert less.js and color.less
-    const lessStyleNode = document.createElement('link');
-    const lessConfigNode = document.createElement('script');
-    const lessScriptNode = document.createElement('script');
-    lessStyleNode.setAttribute('rel', 'stylesheet/less');
-    lessStyleNode.setAttribute('href', '/color.less');
-    lessConfigNode.innerHTML = `
-      window.less = {
-        async: true,
-        env: 'production',
-        javascriptEnabled: true
-      };
-    `;
-    lessScriptNode.src = 'https://gw.alipayobjects.com/os/lib/less.js/3.8.1/less.min.js';
-    lessScriptNode.async = true;
-    lessScriptNode.onload = () => {
-      buildIt();
-      lessScriptNode.onload = null;
-    };
-    document.body.appendChild(lessStyleNode);
-    document.body.appendChild(lessConfigNode);
-    document.body.appendChild(lessScriptNode);
-    lessNodesAppended = true;
-  } else {
-    buildIt();
+  if (primaryColor) {
+    const hideLoading = message.loading('正在编译主题');
+
+    AntdColorReplacerClient.compile(primaryColor).finally(hideLoading);
   }
 };
+
+// let lessNodesAppended;
+// const updateTheme = primaryColor => {
+//   // Don't compile less in production!
+//   if (APP_TYPE !== 'site') {
+//     return;
+//   }
+//   // Determine if the component is remounted
+//   if (!primaryColor) {
+//     return;
+//   }
+//   const hideMessage = message.loading('正在编译主题！', 0);
+//   function buildIt() {
+//     if (!window.less) {
+//       return;
+//     }
+//     setTimeout(() => {
+//       window.less
+//         .modifyVars({
+//           '@primary-color': primaryColor,
+//         })
+//         .then(() => {
+//           hideMessage();
+//         })
+//         .catch(() => {
+//           message.error('Failed to update theme');
+//           hideMessage();
+//         });
+//     }, 200);
+//   }
+//   if (!lessNodesAppended) {
+//     // insert less.js and color.less
+//     const lessStyleNode = document.createElement('link');
+//     const lessConfigNode = document.createElement('script');
+//     const lessScriptNode = document.createElement('script');
+//     lessStyleNode.setAttribute('rel', 'stylesheet/less');
+//     lessStyleNode.setAttribute('href', '/color.less');
+//     lessConfigNode.innerHTML = `
+//       window.less = {
+//         async: true,
+//         env: 'production',
+//         javascriptEnabled: true
+//       };
+//     `;
+//     lessScriptNode.src = 'https://gw.alipayobjects.com/os/lib/less.js/3.8.1/less.min.js';
+//     lessScriptNode.async = true;
+//     lessScriptNode.onload = () => {
+//       buildIt();
+//       lessScriptNode.onload = null;
+//     };
+//     document.body.appendChild(lessStyleNode);
+//     document.body.appendChild(lessConfigNode);
+//     document.body.appendChild(lessScriptNode);
+//     lessNodesAppended = true;
+//   } else {
+//     buildIt();
+//   }
+// };
 
 const updateColorWeak = colorWeak => {
   document.body.className = colorWeak ? 'colorWeak' : '';
