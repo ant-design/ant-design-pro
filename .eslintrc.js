65f5677256f539a5bc386cb597f08cee5eb39248
ycjcl868 <45808948@qq.com>
2019-06-30 10:02:39 +0800

    fix: vscode eslint lang server Node not support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,8 @@
 const { strictEslint } = require('@umijs/fabric');
 
-module.exports = {
-  ...strictEslint,
+module.exports = Object.assign({}, strictEslint, {
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
     page: true,
   },
-};
+});
