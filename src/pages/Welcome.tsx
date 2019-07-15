c6d8c511d52cb2c7a73deb55a2b197cb5c6ea1bf
陈帅 <qixian.cs@outlook.com>
2019-07-15 10:59:54 +0800

    feat: add pageheader demp

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
 
 export default (): React.ReactNode => (
-  <p style={{ textAlign: 'center' }}>
-    Want to add more pages? Please refer to{' '}
-    <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
-      use block
-    </a>
-    。
-  </p>
+  <PageHeaderWrapper>
+    <p style={{ textAlign: 'center' }}>
+      Want to add more pages? Please refer to{' '}
+      <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
+        use block
+      </a>
+      。
+    </p>
+  </PageHeaderWrapper>
 );
