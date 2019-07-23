8ea12955233531f1949b9ed9e7159d43b660d216
Yuns Xie <yuns.xie@qq.com>
2019-07-23 17:00:52 +0800

    fix: code lint (#4886)

--- src/pages/Editor/GGEditor/Koni/index.js
+++ src/pages/Editor/GGEditor/Koni/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Row, Col } from 'antd';
 import GGEditor, { Koni } from 'gg-editor';
+import { FormattedMessage } from 'umi-plugin-react/locale';
 import EditorMinimap from '../components/EditorMinimap';
 import { KoniContextMenu } from '../components/EditorContextMenu';
 import { KoniToolbar } from '../components/EditorToolbar';
@@ -8,7 +9,6 @@ import { KoniItemPanel } from '../components/EditorItemPanel';
 import { KoniDetailPanel } from '../components/EditorDetailPanel';
 import KoniCustomNode from './shape/nodes/KoniCustomNode';
 import styles from '../Flow/index.less';
-import { FormattedMessage } from 'umi-plugin-react/locale';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 
 GGEditor.setTrackable(false);
--- src/pages/Editor/GGEditor/Mind/index.js
+++ src/pages/Editor/GGEditor/Mind/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Row, Col } from 'antd';
 import GGEditor, { Mind } from 'gg-editor';
+import { FormattedMessage } from 'umi-plugin-react/locale';
 import EditorMinimap from '../components/EditorMinimap';
 import { MindContextMenu } from '../components/EditorContextMenu';
 import { MindToolbar } from '../components/EditorToolbar';
 import { MindDetailPanel } from '../components/EditorDetailPanel';
 import data from '../mock/worldCup2018.json';
 import styles from '../Flow/index.less';
-import { FormattedMessage } from 'umi-plugin-react/locale';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 
 GGEditor.setTrackable(false);
