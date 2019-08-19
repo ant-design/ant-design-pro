d70ac0952b7c21b15ec26e504be86bc42c88730e
陈帅 <qixian.cs@outlook.com>
2019-08-19 21:20:54 +0800

    🤔 better sw unregister

--- src/global.tsx
+++ src/global.tsx
@@ -59,13 +59,25 @@ if (pwa) {
     });
   });
 } else if ('serviceWorker' in navigator) {
-  // eslint-disable-next-line compat/compat
-  navigator.serviceWorker.ready
-    .then(registration => {
-      registration.unregister();
-      return true;
-    })
-    .catch(() => {
-      console.log('serviceWorker unregister error');
+  // unregister service worker
+  const { serviceWorker } = navigator;
+  if (serviceWorker.getRegistrations) {
+    serviceWorker.getRegistrations().then(sws => {
+      sws.forEach(sw => {
+        sw.unregister();
+      });
     });
+  }
+  serviceWorker.getRegistration().then(sw => {
+    if (sw) sw.unregister();
+  });
+
+  // remove all caches
+  if (window.caches && window.caches.keys) {
+    caches.keys().then(keys => {
+      keys.forEach(key => {
+        caches.delete(key);
+      });
+    });
+  }
 }
