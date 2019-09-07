a2192f5d57d934aa3e7a562f6d942907ee89a5a2
期贤 <qixian.cs@antfin.com>
2019-09-07 22:17:59 +0800

    doc: add umi ui tip

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Typography } from 'antd';
+import { Card, Typography, Alert } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { FormattedMessage } from 'umi-plugin-react/locale';
 
@@ -20,6 +20,16 @@ const CodePreview: React.FC<{}> = ({ children }) => (
 export default (): React.ReactNode => (
   <PageHeaderWrapper>
     <Card>
+      <Alert
+        message="umi ui 现已发布，欢迎使用 npm run ui 启动体验。"
+        type="success"
+        showIcon
+        banner
+        style={{
+          margin: -12,
+          marginBottom: 24,
+        }}
+      />
       <Typography.Text strong>
         <a target="_blank" rel="noopener noreferrer" href="https://pro.ant.design/docs/block">
           <FormattedMessage
