461d4253708adcef80f55531f7867de2be2c5727
陈帅 <qixian.cs@outlook.com>
2019-09-17 01:03:00 +0800

    new style

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
-import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { PageHeaderWrapper, Typography } from '@ant-design/pro-layout';
+import { FormattedMessage } from 'umi-plugin-react/locale';
+
+import styles from './Welcome.less';
+
+const CodePreview: React.FC<{}> = ({ children }) => (
+  <pre className={styles.pre}>
+    <code>
+      <Typography.Text copyable>{children}</Typography.Text>
+    </code>
+  </pre>
+);
 
 export default (): React.ReactNode => (
   <PageHeaderWrapper>
