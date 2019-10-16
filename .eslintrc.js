8bc6b29ce28c5d8054772d16e5bcdd9ad3e16548
陈帅 <qixian.cs@outlook.com>
2019-10-16 17:22:08 +0800

    eslint use extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
-const { strictEslint } = require('@umijs/fabric');
-
 module.exports = {
-  ...strictEslint,
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
     page: true,
