ba0b08e21bb696e1ecbf8bddd12b0a69fda45c44
yoyo837 <yoyo837@hotmail.com>
2019-11-27 14:18:42 +0800

    revert to extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
-const fabric = require('@umijs/fabric');
-
 module.exports = {
-  ...fabric.eslint,
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
     page: true,
