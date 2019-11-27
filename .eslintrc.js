cbc3a6c45ac0fdcf9240a5744d2e4418a940974d
yoyo837 <yoyo837@hotmail.com>
2019-11-27 14:08:35 +0800

    upgrade @umijs/fabric to beta 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,9 @@
+const fabric = require('@umijs/fabric');
+
 module.exports = {
-  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  ...fabric.eslint,
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
     page: true,
-  },
-  rules: {
-    'react/sort-comp': 'off',
   },
 };
