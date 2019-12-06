1449dca6ced9e83974ba5c09ff8e3e5bbcd4a225
chenshuai2144 <qixian.cs@outlook.com>
2019-12-06 22:05:13 +0800

    chore: test add js dom

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testURL: 'http://localhost:8000',
   preset: 'jest-puppeteer',
+  extraSetupFiles: ['./tests/setupTests.js'],
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: false,
     localStorage: null,
---
+++ tests/setupTests.js
@@ -0,0 +1,22 @@
+import 'jsdom-global/register';
+
+// browserMocks.js
+const localStorageMock = (() => {
+  let store = {};
+
+  return {
+    getItem(key) {
+      return store[key] || null;
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    clear() {
+      store = {};
+    },
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+});
