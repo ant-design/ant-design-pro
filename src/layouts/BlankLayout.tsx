5e8f54cc6df80518cc8a3c57a8b365422853a8cc
chenshuai2144 <qixian.cs@outlook.com>
2019-12-07 19:25:18 +0800

    style: add BlankLayout default style

--- src/layouts/BlankLayout.tsx
+++ src/layouts/BlankLayout.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const Layout: React.FC = ({ children }) => <div>{children}</div>;
+const Layout: React.FC = ({ children }) => (
+  <div
+    style={{
+      width: '100vh',
+      height: '100vh',
+    }}
+  >
+    {children}
+  </div>
+);
 
 export default Layout;
