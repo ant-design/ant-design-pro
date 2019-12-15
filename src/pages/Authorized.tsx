1f56b902f38d1fb45bfbe56944c707ec9be85263
Ding Yi <miaonster17@gmail.com>
2019-12-15 18:19:22 +0800

    fix: authority covered by routes behind (#5723)
    * fix: authority covered by later route
    
    * fix: update code style

--- src/pages/Authorized.tsx
+++ src/pages/Authorized.tsx
@@ -1,34 +1,13 @@
 import React from 'react';
 import Redirect from 'umi/redirect';
 import { connect } from 'dva';
-import pathToRegexp from 'path-to-regexp';
 import Authorized from '@/utils/Authorized';
-import { ConnectProps, ConnectState, Route, UserModelState } from '@/models/connect';
+import { getRouteAuthority } from '@/utils/utils';
+import { ConnectProps, ConnectState, UserModelState } from '@/models/connect';
 
 interface AuthComponentProps extends ConnectProps {
   user: UserModelState;
 }
-
-const getRouteAuthority = (path: string, routeData: Route[]) => {
-  let authorities: string[] | string | undefined;
-  routeData.forEach(route => {
-    if (route.authority) {
-      authorities = route.authority;
-    }
-    // match prefix
-    if (pathToRegexp(`${route.path}(.*)`).test(path)) {
-      // exact match
-      if (route.path === path) {
-        authorities = route.authority || authorities;
-      }
-      // get children authority recursively
-      if (route.routes) {
-        authorities = getRouteAuthority(path, route.routes) || authorities;
-      }
-    }
-  });
-  return authorities;
-};
 
 const AuthComponent: React.FC<AuthComponentProps> = ({
   children,
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,6 @@
 import { parse } from 'querystring';
 import pathRegexp from 'path-to-regexp';
+import { Route } from '@/models/connect';
 
 /* eslint no-useless-escape:0 import/prefer-default-export:0 */
 const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
@@ -36,4 +37,25 @@ export const getAuthorityFromRouter = <T extends { path: string }>(
   const authority = router.find(({ path }) => path && pathRegexp(path).exec(pathname));
   if (authority) return authority;
   return undefined;
+};
+
+export const getRouteAuthority = (path: string, routeData: Route[]) => {
+  let authorities: string[] | string | undefined;
+  routeData.forEach(route => {
+    // match prefix
+    if (pathRegexp(`${route.path}/(.*)`).test(`${path}/`)) {
+      if (route.authority) {
+        authorities = route.authority;
+      }
+      // exact match
+      if (route.path === path) {
+        authorities = route.authority || authorities;
+      }
+      // get children authority recursively
+      if (route.routes) {
+        authorities = getRouteAuthority(path, route.routes) || authorities;
+      }
+    }
+  });
+  return authorities;
 };
