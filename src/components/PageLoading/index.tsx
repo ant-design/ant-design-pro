797a43702c32accb27c97c44c08f7ce98ffd5696
chenshuai2144 <qixian.cs@outlook.com>
2019-12-20 16:52:46 +0800

    code: use pro layout  pageLoading

--- src/components/PageLoading/index.tsx
+++ src/components/PageLoading/index.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
-import { Spin } from 'antd';
+import { PageLoading } from '@ant-design/pro-layout';
 
 // loading components from code split
 // https://umijs.org/plugin/umi-plugin-react.html#dynamicimport
-const PageLoading: React.FC = () => (
-  <div style={{ paddingTop: 100, textAlign: 'center' }}>
-    <Spin size="large" />
-  </div>
-);
 export default PageLoading;
--- src/layouts/SecurityLayout.tsx
+++ src/layouts/SecurityLayout.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { connect } from 'dva';
+import { PageLoading } from '@ant-design/pro-layout';
 import { Redirect } from 'umi';
 import { stringify } from 'querystring';
 import { ConnectState, ConnectProps } from '@/models/connect';
 import { CurrentUser } from '@/models/user';
-import PageLoading from '@/components/PageLoading';
 
 interface SecurityLayoutProps extends ConnectProps {
   loading?: boolean;
