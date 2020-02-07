52151aab110d54989ae221f42fe3a61aff1450b3
yoyo837 <yoyo837@hotmail.com>
2020-02-07 20:54:10 +0800

    add AuthorizedRoute

--- src/components/Authorized/index.tsx
+++ src/components/Authorized/index.tsx
@@ -2,9 +2,11 @@ import Authorized from './Authorized';
 import Secured from './Secured';
 import check from './CheckPermissions';
 import renderAuthorize from './renderAuthorize';
+import AuthorizedRoute from './AuthorizedRoute';
 
 Authorized.Secured = Secured;
 Authorized.check = check;
+Authorized.AuthorizedRoute = AuthorizedRoute;
 
 const RenderAuthorize = renderAuthorize(Authorized);
 
