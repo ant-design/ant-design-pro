27d7b22b84dc5047f007e9fa758bcc2b538a3692
ycjcl868 <45808948@qq.com>
2020-03-01 17:53:22 +0800

    fix: d.ts

--- mock/listTableList.ts
+++ mock/listTableList.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Request, Response } from 'express';
 import { parse } from 'url';
-import { TableListItem, TableListParams } from '../src/pages/ListTableList/data';
+import { TableListItem, TableListParams } from '../src/pages/ListTableList/data.d';
 
 // mock tableListDataSource
 const genList = (current: number, pageSize: number) => {
