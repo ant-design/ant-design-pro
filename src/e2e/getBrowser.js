7cd7a2bd299f4fa113f030ed0edd69355f577595
chenshuai2144 <qixian.cs@outlook.com>
2020-03-25 18:14:04 +0800

    ✂️ dependency:remove puppeteer in dependencies

--- src/e2e/getBrowser.js
+++ src/e2e/getBrowser.js
@@ -1,7 +1,44 @@
-import puppeteer from 'puppeteer';
+/* eslint-disable import/no-extraneous-dependencies */
+const { spawn } = require('child_process');
+const findChrome = require('carlo/lib/find_chrome');
+
+const installPuppeteer = () => {
+  return new Promise((resolve) => {
+    const testCmd = spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['i', 'puppeteer'], {
+      stdio: 'inherit',
+    });
+    testCmd.on('exit', (code) => {
+      resolve(code);
+    });
+  });
+};
 
 const getBrowser = async () => {
+  try {
+    // eslint-disable-next-line global-require
+    const puppeteer = require('puppeteer-core');
+    const findChromePath = await findChrome({});
+    const { executablePath } = findChromePath;
+    console.log(`你的浏览器安装在：${executablePath}`);
+    const browser = await puppeteer.launch({
+      executablePath,
+      args: [
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--no-first-run',
+        '--no-zygote',
+        '--no-sandbox',
+      ],
+    });
+    return browser;
+  } catch (error) {
+    await installPuppeteer();
+  }
+  await installPuppeteer();
+  // eslint-disable-next-line global-require
+  const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({
+    headless: false,
     args: [
       '--disable-gpu',
       '--disable-dev-shm-usage',
