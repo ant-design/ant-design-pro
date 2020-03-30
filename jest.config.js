3a7e54fb94d0e9b599f7c22625dd5e1b90c41586
chenshuai2144 <qixian.cs@outlook.com>
2020-03-30 14:00:26 +0800

    finish set test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testURL: 'http://localhost:8000',
-  extraSetupFiles: ['./tests/setupTests.js'],
+  testEnvironment: './tests/PuppeteerEnvironment',
+  verbose: false,
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: false,
     localStorage: null,
--- tests/getBrowser.js
+++ tests/getBrowser.js
@@ -1,29 +1,13 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-extraneous-dependencies */
-const { execSync } = require('child_process');
-const { join } = require('path');
 const findChrome = require('carlo/lib/find_chrome');
-const detectInstaller = require('detect-installer');
 
-const installPuppeteer = () => {
-  // find can use package manger
-  const packages = detectInstaller(join(__dirname, '../../'));
-  // get installed package manger
-  const package = packages.find(detectInstaller.hasPackageCommand);
-  console.log(`🤖 will use ${package} install puppeteer`);
-  const command = `${package} ${package.includes('yarn') ? 'add' : 'i'} puppeteer`;
-  execSync(command, {
-    stdio: 'inherit',
-  });
-};
-installPuppeteer();
 const getBrowser = async () => {
   try {
     // eslint-disable-next-line import/no-unresolved
     const puppeteer = require('puppeteer-core');
     const findChromePath = await findChrome({});
     const { executablePath } = findChromePath;
-    console.log(`🧲 find you browser in ${executablePath}`);
     const browser = await puppeteer.launch({
       executablePath,
       args: [
@@ -36,13 +20,12 @@ const getBrowser = async () => {
     });
     return browser;
   } catch (error) {
-    // await installPuppeteer();
+    console.log('🧲 no find chrome');
   }
-  await installPuppeteer();
+
   // eslint-disable-next-line import/no-unresolved
   const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({
-    headless: false,
     args: [
       '--disable-gpu',
       '--disable-dev-shm-usage',
