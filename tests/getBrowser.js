075158ca19892039f7796fb2d1a69c299736215d
chenshuai2144 <qixian.cs@outlook.com>
2020-03-30 14:03:14 +0800

    Modify the order and increase the search speed

--- tests/getBrowser.js
+++ tests/getBrowser.js
@@ -5,6 +5,23 @@ const findChrome = require('carlo/lib/find_chrome');
 const getBrowser = async () => {
   try {
     // eslint-disable-next-line import/no-unresolved
+    const puppeteer = require('puppeteer');
+    const browser = await puppeteer.launch({
+      args: [
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--no-first-run',
+        '--no-zygote',
+        '--no-sandbox',
+      ],
+    });
+    return browser;
+  } catch (error) {
+    // console.log(error)
+  }
+
+  try {
+    // eslint-disable-next-line import/no-unresolved
     const puppeteer = require('puppeteer-core');
     const findChromePath = await findChrome({});
     const { executablePath } = findChromePath;
@@ -22,19 +39,7 @@ const getBrowser = async () => {
   } catch (error) {
     console.log('🧲 no find chrome');
   }
-
-  // eslint-disable-next-line import/no-unresolved
-  const puppeteer = require('puppeteer');
-  const browser = await puppeteer.launch({
-    args: [
-      '--disable-gpu',
-      '--disable-dev-shm-usage',
-      '--no-first-run',
-      '--no-zygote',
-      '--no-sandbox',
-    ],
-  });
-  return browser;
+  throw new Error('no find puppeteer');
 };
 
 module.exports = getBrowser;
