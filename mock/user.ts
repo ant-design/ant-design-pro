0d6cd84bc380e9b94a8a94b056466f75d0132d9f
chenshuai2144 <qixian.cs@outlook.com>
2020-04-18 22:50:59 +0800

    add ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION=site env

--- mock/user.ts
+++ mock/user.ts
@@ -4,11 +4,14 @@ function getFakeCaptcha(req: Request, res: Response) {
   return res.json('captcha-xxx');
 }
 
+const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION } = process.env;
+
 /**
  * 当前用户的权限，如果为空代表没登录
  * current user access， if is '', user need login
+ * 如果是 pro 的预览，默认是有权限的
  */
-let access = '';
+let access = ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION === 'site' ? 'admin' : '';
 
 const getAccess = () => {
   return access;
