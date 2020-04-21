010609277633f9a70b3d59a44ef2511a013a9de4
SmallBun <30397655+leshalv@users.noreply.github.com>
2020-04-21 09:29:47 +0800

    Update global.tsx

--- src/global.tsx
+++ src/global.tsx
@@ -1,15 +1,15 @@
-import {Button, message, notification} from 'antd';
+import { Button, message, notification } from 'antd';
 
 import React from 'react';
-import {useIntl} from 'umi';
+import { useIntl } from 'umi';
 import defaultSettings from '../config/defaultSettings';
 
-const {pwa} = defaultSettings;
+const { pwa } = defaultSettings;
 // if pwa is true
 if (pwa) {
   // Notify user if offline now
   window.addEventListener('sw.offline', () => {
-    message.warning(useIntl().formatMessage({id: 'app.pwa.offline'}));
+    message.warning(useIntl().formatMessage({ id: 'app.pwa.offline' }));
   });
 
   // Pop up a prompt on the page asking the user if they want to use the latest version
@@ -32,7 +32,7 @@ if (pwa) {
             resolve(msgEvent.data);
           }
         };
-        worker.postMessage({type: 'skip-waiting'}, [channel.port2]);
+        worker.postMessage({ type: 'skip-waiting' }, [channel.port2]);
       });
       // Refresh current page to use the updated HTML and other assets after SW has skiped waiting
       window.location.reload(true);
@@ -47,21 +47,20 @@ if (pwa) {
           reloadSW();
         }}
       >
-        {useIntl().formatMessage({id: 'app.pwa.serviceworker.updated.ok'})}
+        {useIntl().formatMessage({ id: 'app.pwa.serviceworker.updated.ok' })}
       </Button>
     );
     notification.open({
-      message: useIntl().formatMessage({id: 'app.pwa.serviceworker.updated'}),
-      description: useIntl().formatMessage({id: 'app.pwa.serviceworker.updated.hint'}),
+      message: useIntl().formatMessage({ id: 'app.pwa.serviceworker.updated' }),
+      description: useIntl().formatMessage({ id: 'app.pwa.serviceworker.updated.hint' }),
       btn,
       key,
-      onClose: async () => {
-      },
+      onClose: async () => {},
     });
   });
 } else if ('serviceWorker' in navigator) {
   // unregister service worker
-  const {serviceWorker} = navigator;
+  const { serviceWorker } = navigator;
   if (serviceWorker.getRegistrations) {
     serviceWorker.getRegistrations().then((sws) => {
       sws.forEach((sw) => {
