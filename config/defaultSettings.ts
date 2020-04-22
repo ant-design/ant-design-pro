6b31c151c9491633d89e74da7c3f4c1a7e14dcca
LIUJIAYI <liujiayii@foxmail.com>
2020-04-22 21:24:03 +0800

    import settings form pro-layout

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -1,46 +1,4 @@
-import { MenuTheme } from 'antd/es/menu/MenuContext';
-
-export type ContentWidth = 'Fluid' | 'Fixed';
-
-export interface DefaultSettings {
-  /**
-   * theme for nav menu
-   */
-  navTheme: MenuTheme;
-  /**
-   * primary color of ant design
-   */
-  primaryColor: string;
-  /**
-   * nav menu position: `sidemenu` or `topmenu`
-   */
-  layout: 'sidemenu' | 'topmenu';
-  /**
-   * layout of content: `Fluid` or `Fixed`, only works when layout is topmenu
-   */
-  contentWidth: ContentWidth;
-  /**
-   * sticky header
-   */
-  fixedHeader: boolean;
-  /**
-   * auto hide header
-   */
-  autoHideHeader: boolean;
-  /**
-   * sticky siderbar
-   */
-  fixSiderbar: boolean;
-  menu: { locale: boolean };
-  title: string;
-  pwa: boolean;
-  // Your custom iconfont Symbol script Url
-  // eg：//at.alicdn.com/t/font_1039637_btcrd5co4w.js
-  // 注意：如果需要图标多色，Iconfont 图标项目里要进行批量去色处理
-  // Usage: https://github.com/ant-design/ant-design-pro/pull/3517
-  iconfontUrl: string;
-  colorWeak: boolean;
-}
+import { Settings } from '@ant-design/pro-layout/es/defaultSettings';
 
 export default {
   navTheme: 'dark',
@@ -58,4 +16,4 @@ export default {
   title: 'Ant Design Pro',
   pwa: false,
   iconfontUrl: '',
-} as DefaultSettings;
+} as Settings;
