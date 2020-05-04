9aba57579734c339c2c548a08a8613e8de0d05ce
黄威 <hihuangwei@users.noreply.github.com>
2020-05-04 17:54:54 +0800

    升级为Hook语法
    以适应umi3.0 多语言useIntl

--- src/components/GlobalHeader/AvatarDropdown.tsx
+++ src/components/GlobalHeader/AvatarDropdown.tsx
@@ -13,12 +13,13 @@ export interface GlobalHeaderRightProps extends Partial<ConnectProps> {
   menu?: boolean;
 }
 
-class AvatarDropdown extends React.Component<GlobalHeaderRightProps> {
-  onMenuClick = (event: ClickParam) => {
+const AvatarDropdown: React.FC<GlobalHeaderRightProps> = (props) => {
+  const { currentUser = { avatar: '', name: '' }, menu, dispatch  } = props;
+  
+  const onMenuClick = (event: ClickParam) => {
     const { key } = event;
 
     if (key === 'logout') {
-      const { dispatch } = this.props;
 
       if (dispatch) {
         dispatch({
@@ -32,55 +33,46 @@ class AvatarDropdown extends React.Component<GlobalHeaderRightProps> {
     history.push(`/account/${key}`);
   };
 
-  render(): React.ReactNode {
-    const {
-      currentUser = {
-        avatar: '',
-        name: '',
-      },
-      menu,
-    } = this.props;
-    const menuHeaderDropdown = (
-      <Menu className={styles.menu} selectedKeys={[]} onClick={this.onMenuClick}>
-        {menu && (
-          <Menu.Item key="center">
-            <UserOutlined />
-            个人中心
-          </Menu.Item>
-        )}
-        {menu && (
-          <Menu.Item key="settings">
-            <SettingOutlined />
-            个人设置
-          </Menu.Item>
-        )}
-        {menu && <Menu.Divider />}
-
-        <Menu.Item key="logout">
-          <LogoutOutlined />
-          退出登录
+  const menuHeaderDropdown = (
+    <Menu className={styles.menu} selectedKeys={[]} onClick={onMenuClick}>
+      {menu && (
+        <Menu.Item key="center">
+          <UserOutlined />
+          个人中心
         </Menu.Item>
-      </Menu>
-    );
-    return currentUser && currentUser.name ? (
-      <HeaderDropdown overlay={menuHeaderDropdown}>
-        <span className={`${styles.action} ${styles.account}`}>
-          <Avatar size="small" className={styles.avatar} src={currentUser.avatar} alt="avatar" />
-          <span className={styles.name}>{currentUser.name}</span>
-        </span>
-      </HeaderDropdown>
-    ) : (
+      )}
+      {menu && (
+        <Menu.Item key="settings">
+          <SettingOutlined />
+          个人设置
+        </Menu.Item>
+      )}
+      {menu && <Menu.Divider />}
+
+      <Menu.Item key="logout">
+        <LogoutOutlined />
+        退出登录
+      </Menu.Item>
+    </Menu>
+  );
+  return currentUser && currentUser.name ? (
+    <HeaderDropdown overlay={menuHeaderDropdown}>
       <span className={`${styles.action} ${styles.account}`}>
-        <Spin
-          size="small"
-          style={{
-            marginLeft: 8,
-            marginRight: 8,
-          }}
-        />
+        <Avatar size="small" className={styles.avatar} src={currentUser.avatar} alt="avatar" />
+        <span className={styles.name}>{currentUser.name}</span>
       </span>
-    );
-  }
+    </HeaderDropdown>
+  ) : (
+    <span className={`${styles.action} ${styles.account}`}>
+      <Spin
+        size="small"
+        style={{
+          marginLeft: 8,
+          marginRight: 8,
+        }}
+      />
+    </span>
+  );
 }
 
 export default connect(({ user }: ConnectState) => ({
