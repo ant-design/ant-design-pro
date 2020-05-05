c43f6eba9f6dcb09bf2dc33b9a7b298706bdeb6b
陈帅 <qixian.cs@outlook.com>
2020-05-05 17:27:44 +0800

    🐛 bug: fix submit bug (#6529)
    * 🐛 bug: fix submit bug
    
    * 🔥 clean: use pro-layout typing
    
    * 🔥 clean: use pro-layout typing
    
    * fix ts error

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -1,55 +1,16 @@
-import { MenuTheme } from 'antd/es/menu/MenuContext';
+import { Settings as ProSettings } from '@ant-design/pro-layout';
 
-export type ContentWidth = 'Fluid' | 'Fixed';
-
-export interface DefaultSettings {
-  /**
-   * theme for nav menu
-   */
-  navTheme: MenuTheme;
-  /**
-   * primary color of ant design
-   */
-  primaryColor: string;
-  /**
-   * nav menu position: `sidemenu` or `topmenu`
-   */
-  layout: 'sidemenu' | 'topmenu';
-  /**
-   * layout of content: `Fluid` or `Fixed`, only works when layout is topmenu
-   */
-  contentWidth: ContentWidth;
-  /**
-   * sticky header
-   */
-  fixedHeader: boolean;
-  /**
-   * auto hide header
-   */
-  autoHideHeader: boolean;
-  /**
-   * sticky siderbar
-   */
-  fixSiderbar: boolean;
-  menu: { locale: boolean };
-  title: string;
+type DefaultSettings = ProSettings & {
   pwa: boolean;
-  // Your custom iconfont Symbol script Url
-  // eg：//at.alicdn.com/t/font_1039637_btcrd5co4w.js
-  // 注意：如果需要图标多色，Iconfont 图标项目里要进行批量去色处理
-  // Usage: https://github.com/ant-design/ant-design-pro/pull/3517
-  iconfontUrl: string;
-  colorWeak: boolean;
-}
+};
 
-export default {
+const proSettings: DefaultSettings = {
   navTheme: 'dark',
   // 拂晓蓝
   primaryColor: '#1890ff',
   layout: 'sidemenu',
   contentWidth: 'Fluid',
   fixedHeader: false,
-  autoHideHeader: false,
   fixSiderbar: false,
   colorWeak: false,
   menu: {
@@ -58,4 +19,8 @@ export default {
   title: 'Ant Design Pro',
   pwa: false,
   iconfontUrl: '',
-} as DefaultSettings;
+};
+
+export type { DefaultSettings };
+
+export default proSettings;
