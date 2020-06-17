af37d16355a192695599debdd00f5d7a84d84eee
xiaohuoni <448627663@qq.com>
2020-06-17 10:40:57 +0800

    feat:update layout plugin

--- config/config.ts
+++ config/config.ts
@@ -11,9 +11,6 @@ export default defineConfig({
   dva: {
     hmr: true,
   },
-  accessLayout: {
-    useModel: true,
-  },
   // 这里的逻辑会在 pro-reset 中实现，先放这里防止程序运行出错
   define: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: '',
