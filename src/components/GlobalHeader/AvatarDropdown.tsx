f974b2ec52f8ad2635821f67a59c3e4b87cfedec
chenshuai2144 <qixian.cs@outlook.com>
2020-06-30 18:29:02 +0800

    💄 UI: do not set icon color

--- src/components/GlobalHeader/AvatarDropdown.tsx
+++ src/components/GlobalHeader/AvatarDropdown.tsx
@@ -66,7 +66,7 @@ class AvatarDropdown extends React.Component<GlobalHeaderRightProps> {
       <HeaderDropdown overlay={menuHeaderDropdown}>
         <span className={`${styles.action} ${styles.account}`}>
           <Avatar size="small" className={styles.avatar} src={currentUser.avatar} alt="avatar" />
-          <span className={styles.name}>{currentUser.name}</span>
+          <span className={`${styles.name} anticon`}>{currentUser.name}</span>
         </span>
       </HeaderDropdown>
     ) : (
