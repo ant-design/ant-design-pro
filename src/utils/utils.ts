7e40523d79c6ae28fbabf87e42bd55880977e33b
Lee Fan <535536456@qq.com>
2020-07-13 13:45:41 +0800

    修改hash路由下search参数在hash之前，search最后一个参数错误bug (#6908)

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -21,4 +21,18 @@ export const isAntDesignProOrDev = (): boolean => {
   return isAntDesignPro();
 };
 
-export const getPageQuery = () => parse(window.location.href.split('?')[1]);
+export const getPageQuery = () => {
+  const { href } = window.location;
+  const qsIndex = href.indexOf('?');
+  const sharpIndex = href.indexOf('#');
+
+  if (qsIndex !== -1) {
+    if (qsIndex > sharpIndex) {
+      return parse(href.split('?')[1]);
+    }
+
+    return parse(href.slice(qsIndex + 1, sharpIndex));
+  }
+
+  return {};
+};
