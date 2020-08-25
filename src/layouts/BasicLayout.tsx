d4f8a6f6ef9674610d61bf9f823d4322ccaa9e31
信鑫-King <chaolin.jcl@alibaba-inc.com>
2020-08-25 13:21:40 +0800

    chore: ant group title (#7256)

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -61,7 +61,7 @@ const menuDataRender = (menuList: MenuDataItem[]): MenuDataItem[] =>
 
 const defaultFooterDom = (
   <DefaultFooter
-    copyright={`${new Date().getFullYear()} 蚂蚁金服体验技术部出品`}
+    copyright={`${new Date().getFullYear()} 蚂蚁集团体验技术部出品`}
     links={[
       {
         key: 'Ant Design Pro',
