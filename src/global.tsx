fc57fbb0292210c454885bbce01900ffc85d7435
jjwong <jjwong1991@126.com>
2020-08-31 18:04:54 +0800

    fix issus #7275

--- src/global.tsx
+++ src/global.tsx
@@ -5,6 +5,7 @@ import { useIntl } from 'umi';
 import defaultSettings from '../config/defaultSettings';
 
 const { pwa } = defaultSettings;
+const isHttps = 'https:' == document.location.protocol ? true: false;
 
 // if pwa is true
 if (pwa) {
@@ -59,7 +60,7 @@ if (pwa) {
       onClose: async () => {},
     });
   });
-} else if ('serviceWorker' in navigator) {
+} else if ('serviceWorker' in navigator && isHttps) {
   // unregister service worker
   const { serviceWorker } = navigator;
   if (serviceWorker.getRegistrations) {
