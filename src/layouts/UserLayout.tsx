037507fa0949ed907e324c02f862904d627826ce
kevinadhiguna <kevin26798@gmail.com>
2020-10-30 07:54:15 +0000

    feat: add English translation for Login screen

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -1,6 +1,6 @@
 import { DefaultFooter, MenuDataItem, getMenuData, getPageTitle } from '@ant-design/pro-layout';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
-import { Link, SelectLang, useIntl, ConnectProps, connect } from 'umi';
+import { Link, SelectLang, useIntl, ConnectProps, connect, FormattedMessage } from 'umi';
 import React from 'react';
 import { ConnectState } from '@/models/connect';
 import logo from '../assets/logo.svg';
@@ -52,7 +52,12 @@ const UserLayout: React.FC<UserLayoutProps> = (props) => {
                 <span className={styles.title}>Ant Design</span>
               </Link>
             </div>
-            <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
+            <div className={styles.desc}>
+              <FormattedMessage
+                id="pages.layouts.userLayout.title"
+                defaultMessage="Ant Design 是西湖区最具影响力的 Web 设计规范"
+              />
+            </div>
           </div>
           {children}
         </div>
--- src/locales/en-US.ts
+++ src/locales/en-US.ts
@@ -4,6 +4,7 @@ import menu from './en-US/menu';
 import pwa from './en-US/pwa';
 import settingDrawer from './en-US/settingDrawer';
 import settings from './en-US/settings';
+import pages from './en-US/pages';
 
 export default {
   'navBar.lang': 'Languages',
@@ -19,4 +20,5 @@ export default {
   ...settings,
   ...pwa,
   ...component,
+  ...pages,
 };
