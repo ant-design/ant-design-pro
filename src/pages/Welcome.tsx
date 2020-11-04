214aba7cea4a3da479b65e1df32e1bdbe2806057
kevinadhiguna <kevin26798@gmail.com>
2020-11-04 02:40:45 +0000

    feat: add English translation for Welcome screen, Admin screen, and Form in Search Table screen (partial)

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import { Card, Alert, Typography } from 'antd';
+import { useIntl, FormattedMessage } from 'umi';
 import styles from './Welcome.less';
 
 const CodePreview: React.FC<{}> = ({ children }) => (
@@ -11,46 +12,52 @@ const CodePreview: React.FC<{}> = ({ children }) => (
   </pre>
 );
 
-export default (): React.ReactNode => (
-  <PageContainer>
-    <Card>
-      <Alert
-        message="更快更强的重型组件，已经发布。"
-        type="success"
-        showIcon
-        banner
-        style={{
-          margin: -12,
-          marginBottom: 24,
-        }}
-      />
-      <Typography.Text strong>
-        高级表格{' '}
-        <a
-          href="https://procomponents.ant.design/components/table"
-          rel="noopener noreferrer"
-          target="__blank"
+export default (): React.ReactNode => {
+  const intl = useIntl();
+  return (
+    <PageContainer>
+      <Card>
+        <Alert
+          message={intl.formatMessage({
+            id: 'pages.welcome.alertMessage',
+            defaultMessage: '更快更强的重型组件，已经发布。',
+          })}
+          type="success"
+          showIcon
+          banner
+          style={{
+            margin: -12,
+            marginBottom: 24,
+          }}
+        />
+        <Typography.Text strong>
+          <FormattedMessage id="pages.welcome.advancedComponent" defaultMessage="高级表格" />{' '}
+          <a
+            href="https://procomponents.ant.design/components/table"
+            rel="noopener noreferrer"
+            target="__blank"
+          >
+            <FormattedMessage id="pages.welcome.link" defaultMessage="欢迎使用" />
+          </a>
+        </Typography.Text>
+        <CodePreview>yarn add @ant-design/pro-table</CodePreview>
+        <Typography.Text
+          strong
+          style={{
+            marginBottom: 12,
+          }}
         >
-          欢迎使用
-        </a>
-      </Typography.Text>
-      <CodePreview>yarn add @ant-design/pro-table</CodePreview>
-      <Typography.Text
-        strong
-        style={{
-          marginBottom: 12,
-        }}
-      >
-        高级布局{' '}
-        <a
-          href="https://procomponents.ant.design/components/layout"
-          rel="noopener noreferrer"
-          target="__blank"
-        >
-          欢迎使用
-        </a>
-      </Typography.Text>
-      <CodePreview>yarn add @ant-design/pro-layout</CodePreview>
-    </Card>
-  </PageContainer>
-);
+          <FormattedMessage id="pages.welcome.advancedLayout" defaultMessage="高级布局" />{' '}
+          <a
+            href="https://procomponents.ant.design/components/layout"
+            rel="noopener noreferrer"
+            target="__blank"
+          >
+            <FormattedMessage id="pages.welcome.link" defaultMessage="欢迎使用" />
+          </a>
+        </Typography.Text>
+        <CodePreview>yarn add @ant-design/pro-layout</CodePreview>
+      </Card>
+    </PageContainer>
+  );
+};
