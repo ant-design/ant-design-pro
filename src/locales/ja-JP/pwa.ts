70d0ad73f141d944dfbd0aea2c320a78547c089d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2020-12-04 15:37:34 +0000

    feat: add more Japanese translation

--- src/locales/ja-JP/pwa.ts
+++ src/locales/ja-JP/pwa.ts
@@ -1,6 +1,6 @@
 export default {
-  'app.pwa.offline': '',
-  'app.pwa.serviceworker.updated': '',
-  'app.pwa.serviceworker.updated.hint': '',
+  'app.pwa.offline': 'あなたは今オフラインです',
+  'app.pwa.serviceworker.updated': '新しいコンテンツが利用可能です',
+  'app.pwa.serviceworker.updated.hint': '現在のページをリロードするには、「更新」ボタンを押してください',
   'app.pwa.serviceworker.updated.ok': 'リフレッシュ',
 };
