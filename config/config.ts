818926f4b3e6512197899412f7070b700169cbe3
chenshuai2144 <qixian.cs@outlook.com>
2020-12-08 13:46:42 +0800

    ✨ feat: add react-dev-inspector plugins

--- config/config.ts
+++ config/config.ts
@@ -41,4 +41,12 @@ export default defineConfig({
     basePath: '/',
   },
   esbuild: {},
+  // https://github.com/zthxxx/react-dev-inspector
+  plugins: ['react-dev-inspector/plugins/umi/react-inspector'],
+  inspectorConfig: {
+    // loader options type and docs see below
+    exclude: [],
+    babelPlugins: [],
+    babelOptions: {},
+  },
 });
