9862c9430fb6a319076ecba901cdb15ef7607812
chenshuai2144 <qixian.cs@outlook.com>
2020-12-08 17:57:52 +0800

    🚑 hotfix: support InspectorWrapper

--- config/routes.ts
+++ config/routes.ts
@@ -1,63 +1,69 @@
 ﻿export default [
   {
-    path: '/user',
-    component: '../layouts/UserLayout',
+    path: '/',
+    component: '../layouts/BlankLayout',
     routes: [
       {
-        name: 'login',
-        path: '/user/login',
-        component: './user/login',
+        path: '/user',
+        component: '../layouts/UserLayout',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './user/login',
+          },
+        ],
       },
-    ],
-  },
-  {
-    path: '/',
-    component: '../layouts/SecurityLayout',
-    routes: [
       {
         path: '/',
-        component: '../layouts/BasicLayout',
-        authority: ['admin', 'user'],
+        component: '../layouts/SecurityLayout',
         routes: [
           {
             path: '/',
-            redirect: '/welcome',
-          },
-          {
-            path: '/welcome',
-            name: 'welcome',
-            icon: 'smile',
-            component: './Welcome',
-          },
-          {
-            path: '/admin',
-            name: 'admin',
-            icon: 'crown',
-            component: './Admin',
-            authority: ['admin'],
+            component: '../layouts/BasicLayout',
+            authority: ['admin', 'user'],
             routes: [
               {
-                path: '/admin/sub-page',
-                name: 'sub-page',
+                path: '/',
+                redirect: '/welcome',
+              },
+              {
+                path: '/welcome',
+                name: 'welcome',
                 icon: 'smile',
                 component: './Welcome',
+              },
+              {
+                path: '/admin',
+                name: 'admin',
+                icon: 'crown',
+                component: './Admin',
                 authority: ['admin'],
+                routes: [
+                  {
+                    path: '/admin/sub-page',
+                    name: 'sub-page',
+                    icon: 'smile',
+                    component: './Welcome',
+                    authority: ['admin'],
+                  },
+                ],
+              },
+              {
+                name: 'list.table-list',
+                icon: 'table',
+                path: '/list',
+                component: './ListTableList',
+              },
+              {
+                component: './404',
               },
             ],
-          },
-          {
-            name: 'list.table-list',
-            icon: 'table',
-            path: '/list',
-            component: './ListTableList',
           },
           {
             component: './404',
           },
         ],
-      },
-      {
-        component: './404',
       },
     ],
   },
--- src/layouts/BlankLayout.tsx
+++ src/layouts/BlankLayout.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Inspector } from 'react-dev-inspector';
 
-const Layout: React.FC = ({ children }) => <>{children}</>;
+const InspectorWrapper = process.env.NODE_ENV === 'development' ? Inspector : React.Fragment;
+
+const Layout: React.FC = ({ children }) => {
+  return <InspectorWrapper>{children}</InspectorWrapper>;
+};
 
 export default Layout;
