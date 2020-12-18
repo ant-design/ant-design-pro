f2292ba47fbd493781def65e8a7d7fd953b45015
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2020-12-18 17:24:37 +0000

    feat: add Indonesian translation for Login screen

--- src/locales/id-ID/pages.ts
+++ src/locales/id-ID/pages.ts
@@ -1,1 +1,24 @@
-export default {};
+export default {
+  'pages.layouts.userLayout.title':
+    'Ant Design adalah spesifikasi desain Web yang paling berpengaruh di Kabupaten Xihu',
+  'pages.login.accountLogin.tab': 'Login dengan akun',
+  'pages.login.accountLogin.errorMessage': 'Nama pengguna dan kata sandi salah（admin/ant.design)',
+  'pages.login.username.placeholder': 'nama pengguna: admin atau user',
+  'pages.login.username.required': 'Nama pengguna harus diisi!',
+  'pages.login.password.placeholder': 'kata sandi: ant.design',
+  'pages.login.password.required': 'Kata sandi harus diisi!',
+  'pages.login.phoneLogin.tab': 'Login dengan ponsel',
+  'pages.login.phoneLogin.errorMessage': 'Kesalahan kode verifikasi',
+  'pages.login.phoneNumber.placeholder': 'masukkan nomor telepon',
+  'pages.login.phoneNumber.required': 'Nomor ponsel harus diisi!',
+  'pages.login.phoneNumber.invalid': 'Nomor ponsel tidak valid!',
+  'pages.login.captcha.placeholder': 'kode verifikasi',
+  'pages.login.captcha.required': 'Kode verifikasi diperlukan!',
+  'pages.login.phoneLogin.getVerificationCode': 'Dapatkan kode',
+  'pages.getCaptchaSecondText': 'detik tersisa',
+  'pages.login.rememberMe': 'Ingat saya',
+  'pages.login.forgotPassword': 'Lupa Kata Sandi?',
+  'pages.login.submit': 'Masuk',
+  'pages.login.loginWith': 'Masuk dengan :',
+  'pages.login.registerAccount': 'Daftar Akun',
+};
