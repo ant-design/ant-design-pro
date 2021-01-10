f7578d5017ac199e601aa50c8e04bf5dfaadc05f
mattortiz-ht <75054152+mattortiz-ht@users.noreply.github.com>
2021-01-10 01:14:54 -0700

    Update Secured.tsx
    Translated to english

--- src/components/Authorized/Secured.tsx
+++ src/components/Authorized/Secured.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import CheckPermissions from './CheckPermissions';
 
 /**
- * 默认不能访问任何页面
+ * No pages can be accessed by default
  * default is "NULL"
  */
 const Exception403 = () => 403;
@@ -30,24 +30,24 @@ const checkIsInstantiation = (target: React.ComponentClass | React.ReactNode) =>
 };
 
 /**
- * 用于判断是否拥有权限访问此 view 权限
- * authority 支持传入 string, () => boolean | Promise
- * e.g. 'user' 只有 user 用户能访问
- * e.g. 'user,admin' user 和 admin 都能访问
- * e.g. ()=>boolean 返回true能访问,返回false不能访问
- * e.g. Promise  then 能访问   catch不能访问
+* Used to determine whether you have permission to access this view permission
+ * authority supports incoming string, () => boolean | Promise
+ * e.g.'user' Only user user can access
+ * e.g.'user,admin' user and admin can access
+ * e.g. ()=>boolean return true to access, return false to not access
+ * e.g. Promise then can be accessed, catch can not be accessed
  * e.g. authority support incoming string, () => boolean | Promise
- * e.g. 'user' only user user can access
- * e.g. 'user, admin' user and admin can access
+ * e.g.'user' only user user can access
+ * e.g.'user, admin' user and admin can access
  * e.g. () => boolean true to be able to visit, return false can not be accessed
  * e.g. Promise then can not access the visit to catch
  * @param {string | function | Promise} authority
- * @param {ReactNode} error 非必需参数
+ * @param {ReactNode} error non-required parameter
  */
 const authorize = (authority: string, error?: React.ReactNode) => {
   /**
    * conversion into a class
-   * 防止传入字符串时找不到staticContext造成报错
+   * Prevent the staticContext from being found to cause an error when the string is passed in
    * String parameters can cause staticContext not found error
    */
   let classError: boolean | React.FunctionComponent = false;
