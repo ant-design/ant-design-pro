89fb844c46b3122f4d153840d57ec3591709bb49
chenshuai2144 <qixian.cs@outlook.com>
2021-01-28 18:33:15 +0800

    support schemaPath = url

--- config/config.ts
+++ config/config.ts
@@ -48,6 +48,8 @@ export default defineConfig({
   },
   openAPI: {
     requestLibPath: "import { request } from 'umi'",
+    // 或者使用在线的版本
+    // schemaPath: "https://gw.alipayobjects.com/os/antfincdn/M%24jrzTTYJN/oneapi.json"
     schemaPath: join(__dirname, 'oneapi.json'),
     mock: false,
   },
