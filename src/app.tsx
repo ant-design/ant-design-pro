756cccd731bd83734627434c5114f5d73bf13647
chenshuai2144 <qixian.cs@outlook.com>
2021-01-29 15:11:11 +0800

    add umi-plugins

--- src/app.tsx
+++ src/app.tsx
@@ -8,6 +8,7 @@ import RightContent from '@/components/RightContent';
 import Footer from '@/components/Footer';
 import type { ResponseError } from 'umi-request';
 import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
+import { BookOutlined, LinkOutlined } from '@ant-design/icons';
 
 /** 获取用户信息比较慢的时候会展示一个 loading */
 export const initialStateConfig = {
@@ -55,6 +56,28 @@ export const layout: RunTimeLayoutConfig = ({ initialState }) => {
         history.push('/user/login');
       }
     },
+    links: [
+      <>
+        <LinkOutlined />
+        <span
+          onClick={() => {
+            window.open('/umi/plugin/openapi');
+          }}
+        >
+          openAPI 文档
+        </span>
+      </>,
+      <>
+        <BookOutlined />
+        <span
+          onClick={() => {
+            window.open('/~docs');
+          }}
+        >
+          业务组件文档
+        </span>
+      </>,
+    ],
     menuHeaderRender: undefined,
     // 自定义 403 页面
     // unAccessible: <div>unAccessible</div>,
