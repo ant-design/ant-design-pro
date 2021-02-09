c9dba67c1b496f350c84f7d358cffe2fcfca466b
chenshuai2144 <qixian.cs@outlook.com>
2021-02-09 15:24:34 +0800

    非 dev 环境不展示 openapi

--- src/app.tsx
+++ src/app.tsx
@@ -10,6 +10,8 @@ import type { ResponseError } from 'umi-request';
 import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
 import { BookOutlined, LinkOutlined } from '@ant-design/icons';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 /** 获取用户信息比较慢的时候会展示一个 loading */
 export const initialStateConfig = {
   loading: <PageLoading />,
@@ -60,28 +62,30 @@ export const layout: RunTimeLayoutConfig = ({ initialState }) => {
         history.push('/user/login');
       }
     },
-    links: [
-      <>
-        <LinkOutlined />
-        <span
-          onClick={() => {
-            window.open('/umi/plugin/openapi');
-          }}
-        >
-          openAPI 文档
-        </span>
-      </>,
-      <>
-        <BookOutlined />
-        <span
-          onClick={() => {
-            window.open('/~docs');
-          }}
-        >
-          业务组件文档
-        </span>
-      </>,
-    ],
+    links: isDev
+      ? [
+          <>
+            <LinkOutlined />
+            <span
+              onClick={() => {
+                window.open('/umi/plugin/openapi');
+              }}
+            >
+              openAPI 文档
+            </span>
+          </>,
+          <>
+            <BookOutlined />
+            <span
+              onClick={() => {
+                window.open('/~docs');
+              }}
+            >
+              业务组件文档
+            </span>
+          </>,
+        ]
+      : [],
     menuHeaderRender: undefined,
     // 自定义 403 页面
     // unAccessible: <div>unAccessible</div>,
