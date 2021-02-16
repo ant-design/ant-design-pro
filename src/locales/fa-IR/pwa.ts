1740da2dd8d6a58ec8337b7ef122e434b8d72b46
abolfazl <Abolfazl.rajabpour@gmail.com>
2021-02-16 16:33:44 +0330

    add persian local and fix menu direction for rtl display (#8079)

---
+++ src/locales/fa-IR/pwa.ts
@@ -0,0 +1,7 @@
+export default {
+  'app.pwa.offline': 'شما اکنون آفلاین هستید',
+  'app.pwa.serviceworker.updated': 'مطالب جدید در دسترس است',
+  'app.pwa.serviceworker.updated.hint':
+    'لطفاً برای بارگیری مجدد صفحه فعلی ، دکمه "تازه سازی" را فشار دهید',
+  'app.pwa.serviceworker.updated.ok': 'تازه سازی',
+};
