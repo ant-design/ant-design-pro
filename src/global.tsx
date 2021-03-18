3254d44508291a33e15b29432a8c9473597c4195
kaoding <41830859@qq.com>
2021-03-18 16:45:02 +0800

    Update global.tsx

--- src/global.tsx
+++ src/global.tsx
@@ -1,6 +1,4 @@
 import { Button, message, notification } from 'antd';
-
-import React from 'react';
 import { useIntl } from 'umi';
 import defaultSettings from '../config/defaultSettings';
 
@@ -75,7 +73,7 @@ if (pwa) {
   });
 
   // remove all caches
-  if (window.caches && window.caches.keys) {
+  if (window.caches && window.caches.keys()) {
     caches.keys().then((keys) => {
       keys.forEach((key) => {
         caches.delete(key);
