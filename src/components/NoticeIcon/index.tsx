7e20ae6fb67888d6810591dd12b4ce410173d2e9
kaoding <41830859@qq.com>
2021-03-18 16:45:47 +0800

    Update index.tsx

--- src/components/NoticeIcon/index.tsx
+++ src/components/NoticeIcon/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Tag, message } from 'antd';
 import { groupBy } from 'lodash';
 import moment from 'moment';
