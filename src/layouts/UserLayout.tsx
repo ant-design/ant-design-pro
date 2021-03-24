d2a75197eb2c96410801a11dacf986fd4ded55a1
xrkffgg <xrkffgg@gmail.com>
2021-03-24 21:13:52 +0800

    test: test

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -49,7 +49,7 @@ const UserLayout: React.FC<UserLayoutProps> = (props) => {
             <div className={styles.header}>
               <Link to="/">
                 <img alt="logo" className={styles.logo} src={logo} />
-                <span className={styles.title}>Ant Design</span>
+                <span className={styles.title}>Ant Design Pro</span>
               </Link>
             </div>
             <div className={styles.desc}>
