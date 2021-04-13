637017a73b410271c4ecaf9e58b36240ea673a2e
小鹅鹅鹅 <littleee.lau@gmail.com>
2021-04-13 16:00:27 +0800

    chore: fix locales translate (#8343)

--- src/locales/zh-TW/settingDrawer.ts
+++ src/locales/zh-TW/settingDrawer.ts
@@ -25,7 +25,7 @@ export default {
   'app.setting.othersettings': '其他設置',
   'app.setting.weakmode': '色弱模式',
   'app.setting.copy': '拷貝設置',
-  'app.setting.copyinfo': '拷貝成功，請到 src/defaultSettings.js 中替換默認配置',
+  'app.setting.copyinfo': '拷貝成功，請到 config/defaultSettings.js 中替換默認配置',
   'app.setting.production.hint':
     '配置欄只在開發環境用於預覽，生產環境不會展現，請拷貝後手動修改配置文件',
 };
