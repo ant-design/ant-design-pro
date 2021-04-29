a2057468e5851a150a47258bc30da2fd8b423ee6
chenshuai2144 <qixian.cs@outlook.com>
2021-04-29 10:15:30 +0800

    chore: support verify commit

--- src/app.tsx
+++ src/app.tsx
@@ -49,7 +49,55 @@ export async function getInitialState(): Promise<{
   };
 }
 
-// https://umijs.org/zh-CN/plugins/plugin-layout
+/**
+ * 异常处理程序
+ const codeMessage = {
+    200: '服务器成功返回请求的数据。',
+    201: '新建或修改数据成功。',
+    202: '一个请求已经进入后台排队（异步任务）。',
+    204: '删除数据成功。',
+    400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
+    401: '用户没有权限（令牌、用户名、密码错误）。',
+    403: '用户得到授权，但是访问是被禁止的。',
+    404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
+    405: '请求方法不被允许。',
+    406: '请求的格式不可得。',
+    410: '请求的资源被永久删除，且不会再得到的。',
+    422: '当创建一个对象时，发生一个验证错误。',
+    500: '服务器发生错误，请检查服务器。',
+    502: '网关错误。',
+    503: '服务不可用，服务器暂时过载或维护。',
+    504: '网关超时。',
+ };
+ * @see https://beta-pro.ant.design/docs/request-cn
+ */
+export const request: RequestConfig = {
+  errorHandler: (error: ResponseError) => {
+    const { messages } = getIntl(getLocale());
+    const { response } = error;
+
+    if (response && response.status) {
+      const { status, statusText, url } = response;
+      const requestErrorMessage = messages['app.request.error'];
+      const errorMessage = `${requestErrorMessage} ${status}: ${url}`;
+      const errorDescription = messages[`app.request.${status}`] || statusText;
+      notification.error({
+        message: errorMessage,
+        description: errorDescription,
+      });
+    }
+
+    if (!response) {
+      notification.error({
+        description: '您的网络发生异常，无法连接服务器',
+        message: '网络异常',
+      });
+    }
+    throw error;
+  },
+};
+
+// ProLayout 支持的api https://procomponents.ant.design/components/layout
 export const layout: RunTimeLayoutConfig = ({ initialState }) => {
   return {
     rightContentRender: () => <RightContent />,
@@ -82,58 +130,4 @@ export const layout: RunTimeLayoutConfig = ({ initialState }) => {
     // unAccessible: <div>unAccessible</div>,
     ...initialState?.settings,
   };
-};
-
-/**
- * 异常处理程序
-const codeMessage = {
-  200: '服务器成功返回请求的数据。',
-  201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  405: '请求方法不被允许。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。',
-};
-
-/** 异常处理程序
- * @see https://beta-pro.ant.design/docs/request-cn
- */
-const errorHandler = (error: ResponseError) => {
-  const { messages } = getIntl(getLocale());
-  const { response } = error;
-
-  if (response && response.status) {
-    const { status, statusText, url } = response;
-    const requestErrorMessage = messages['app.request.error'];
-    const errorMessage = `${requestErrorMessage} ${status}: ${url}`;
-    const errorDescription = messages[`app.request.${status}`] || statusText;
-
-    notification.error({
-      message: errorMessage,
-      description: errorDescription,
-    });
-  }
-
-  if (!response) {
-    notification.error({
-      description: '您的网络发生异常，无法连接服务器',
-      message: '网络异常',
-    });
-  }
-  throw error;
-};
-
-// https://umijs.org/zh-CN/plugins/plugin-request
-export const request: RequestConfig = {
-  errorHandler,
 };
