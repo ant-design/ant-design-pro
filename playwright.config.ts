6148cbcc64f60f0767ac25f029cf37712a3a2db7
chenshuai2144 <qixian.cs@outlook.com>
2021-11-25 13:04:16 +0800

    e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,10 +17,6 @@ const config: PlaywrightTestConfig = {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
   ],
 };
 export default config;
