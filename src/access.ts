b2710426ef0b9a8c3735d58b7c786116ec745c15
zqran <uuxnet@gmail.com>
2022-02-14 18:18:45 +0800

    refactor: modify the parameter type of `access` function

--- src/access.ts
+++ src/access.ts
@@ -1,8 +1,8 @@
 /**
  * @see https://umijs.org/zh-CN/plugins/plugin-access
  * */
-export default function access(initialState: { currentUser?: API.CurrentUser | undefined }) {
-  const { currentUser } = initialState || {};
+export default function access(initialState: { currentUser?: API.CurrentUser } | undefined) {
+  const { currentUser } = initialState ?? {};
   return {
     canAdmin: currentUser && currentUser.access === 'admin',
   };
