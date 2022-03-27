9d8b711605a280d4339f29ab2dfb83851061ef98
陈帅 <qixian.cs@outlook.com>
2022-03-27 20:20:26 +0800

    chore: update package.json version

--- config/config.ts
+++ config/config.ts
@@ -38,6 +38,9 @@ export default defineConfig({
   routes,
   // Theme for antd: https://ant.design/docs/react/customize-theme-cn
   theme: {
+    // 如果不想要 configProvide 动态设置主题需要把这个设置为 default
+    // 只有设置为 variable， 才能使用 configProvide 动态设置主色调
+    // https://ant.design/docs/react/customize-theme-variable-cn
     'root-entry-name': 'variable',
   },
   // esbuild is father build tools
