d160dc04a543b484edf031f8aaa18833fdbfcc29
期贤 <qixian.cs@antgroup.com>
2022-04-25 22:21:18 +0800

    fix: support access new version

--- config/config.ts
+++ config/config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
   },
   // umi routes: https://umijs.org/docs/routing
   routes,
+  access: {},
   // Theme for antd: https://ant.design/docs/react/customize-theme-cn
   theme: {
     // 如果不想要 configProvide 动态设置主题需要把这个设置为 default
