5f833745cf2bad25bfa4be76814787a6093c7f3c
chenshuai2144 <qixian.cs@outlook.com>
2022-07-03 21:32:46 +0800

    doc: add more docs

--- config/proxy.ts
+++ config/proxy.ts
@@ -1,10 +1,13 @@
 /**
- * 在生产环境 代理是无法生效的，所以这里没有生产环境的配置
+ * @name 代理的配置
+ * @see 在生产环境 代理是无法生效的，所以这里没有生产环境的配置
  * -------------------------------
  * The agent cannot take effect in the production environment
  * so there is no configuration of the production environment
  * For details, please see
  * https://pro.ant.design/docs/deploy
+ *
+ * @doc https://umijs.org/docs/guides/proxy
  */
 export default {
   dev: {
