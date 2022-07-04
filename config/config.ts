3345c2dcb257c815de4958d613fb2c7bed20319c
HongJian <tanhongjian@outlook.com>
2022-07-04 09:43:23 +0800

    typo

--- config/config.ts
+++ config/config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
 
   /**
    * @name 兼容性设置
-   * @description 设置 ie11 不一定完美兼容，需要检查自己使用的所有以来
+   * @description 设置 ie11 不一定完美兼容，需要检查自己使用的所有依赖
    * @doc https://umijs.org/docs/api/config#targets
    */
   targets: {
