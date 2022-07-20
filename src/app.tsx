435efa678c071c028379f079547873fae4434b8f
DaoYuan <txp1035@gmail.com>
2022-07-20 12:23:49 +0800

    fix: location

--- src/app.tsx
+++ src/app.tsx
@@ -33,7 +33,7 @@ export async function getInitialState(): Promise<{
     return undefined;
   };
   // 如果不是登录页面，执行
-  if (history.location.pathname !== loginPath) {
+  if (window.location.pathname !== loginPath) {
     const currentUser = await fetchUserInfo();
     return {
       fetchUserInfo,
