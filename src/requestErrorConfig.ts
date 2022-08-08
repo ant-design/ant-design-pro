0fd1403f8658c715361e0f5c81eb9348ee176198
期贤 <qixian.cs@antgroup.com>
2022-08-08 14:04:11 +0800

    fix tsc error

--- src/requestErrorConfig.ts
+++ src/requestErrorConfig.ts
@@ -1,4 +1,5 @@
-﻿import type { RequestConfig } from '@umijs/max';
+﻿import type { RequestOptions } from '@@/plugin-request/request';
+import type { RequestConfig } from '@umijs/max';
 import { message, notification } from 'antd';
 
 // 错误处理方案： 错误类型
@@ -86,9 +87,9 @@ export const errorConfig: RequestConfig = {
 
   // 请求拦截器
   requestInterceptors: [
-    (config) => {
+    (config: RequestOptions) => {
       // 拦截请求配置，进行个性化处理。
-      const url = config.url.concat('?token = 123');
+      const url = config?.url?.concat('?token = 123');
       return { ...config, url };
     },
   ],
