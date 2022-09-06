7d51aeb221f5c206e7342cb53d7e2ceb08a6b156
CN-traveler <55753029+CN-traveler@users.noreply.github.com>
2022-09-06 15:50:03 +0800

    feat: 替换管理页组件 & 更正路由 (#10195)
    * 替换组件
    
    * 更新管理页面路由

--- config/routes.ts
+++ config/routes.ts
@@ -33,10 +33,13 @@ export default [
     access: 'canAdmin',
     routes: [
       {
+        path: '/admin',
+        redirect: '/admin/sub-page',
+      },
+      {
         path: '/admin/sub-page',
         name: 'sub-page',
-        icon: 'smile',
-        component: './Welcome',
+        component: './Admin',
       },
     ],
   },
