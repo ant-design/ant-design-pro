b282681e72037518cddd619278cc168f0a886da0
Yanghc <hqwlkj@sina.com>
2022-09-08 10:26:27 +0800

    fix: RightContent 中的 navTheme 条件值
    build (16.x, ubuntu-latest): src/components/RightContent/index.tsx#L1
    This condition will always return 'false' since the types '"light" | "realDark" | undefined' and '"dark"' have no overlap.
    
    https://github.com/ant-design/ant-design-pro/pull/10209/files#annotation_4543727834

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -18,7 +18,7 @@ const GlobalHeaderRight: React.FC = () => {
   const { navTheme, layout } = initialState.settings;
   let className = styles.right;
 
-  if ((navTheme === 'dark' && layout === 'top') || layout === 'mix') {
+  if ((navTheme === 'realDark' && layout === 'top') || layout === 'mix') {
     className = `${styles.right}  ${styles.dark}`;
   }
   return (
