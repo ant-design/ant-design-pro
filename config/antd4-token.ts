c35ce8fa5846e43dc71fb9152c4603954eba1488
vagusx <vagusxl@gmail.com>
2022-12-21 15:08:41 +0800

    fix: disable import less

--- config/antd4-token.ts
+++ config/antd4-token.ts
@@ -5,3 +5,4 @@ const { defaultAlgorithm, defaultSeed } = theme;
 
 const mapToken = defaultAlgorithm(defaultSeed);
 export const v4Token = convertLegacyToken(mapToken);
+console.log(JSON.stringify(v4Token, null, 4));
