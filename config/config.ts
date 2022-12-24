59ef282d0783e6dbd750573f06acf3f2733d9f36
黄威 <hihuangwei@users.noreply.github.com>
2022-12-24 10:45:08 +0800

    修复刷新时 title 不显示的问题

--- config/config.ts
+++ config/config.ts
@@ -76,6 +76,7 @@ export default defineConfig({
    * @name layout 插件
    * @doc https://umijs.org/docs/max/layout-menu
    */
+  title: 'Ant Design Pro',
   layout: {
     locale: true,
     ...defaultSettings,
