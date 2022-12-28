0b4b0b14ba70d31fd796f6464bfb06db326942c9
Tony Han <huarui2219@qq.com>
2022-12-28 10:47:52 +0800

    修复api 登录问题 (#10453)

--- config/proxy.ts
+++ config/proxy.ts
@@ -10,7 +10,18 @@
  * @doc https://umijs.org/docs/guides/proxy
  */
 export default {
-  dev: {},
+  // 如果需要自定义本地开发服务器  请取消注释按需调整
+  // dev: {
+  //   // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
+  //   '/api/': {
+  //     // 要代理的地址
+  //     target: 'https://preview.pro.ant.design',
+  //     // 配置了这个可以从 http 代理到 https
+  //     // 依赖 origin 的功能可能需要这个，比如 cookie
+  //     changeOrigin: true,
+  //   },
+  // },
+  
   /**
    * @name 详细的代理配置
    * @doc https://github.com/chimurai/http-proxy-middleware
