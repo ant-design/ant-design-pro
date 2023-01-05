e65227ce7e9367c91d50309ffbd68acac80dd192
Tony Han <huarui2219@qq.com>
2023-01-05 17:27:31 +0800

    更新TS 声明,

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -1,9 +1,9 @@
-import { Settings as LayoutSettings } from '@ant-design/pro-components';
+import { ProLayoutProps } from '@ant-design/pro-components';
 
 /**
  * @name
  */
-const Settings: LayoutSettings & {
+const Settings: ProLayoutProps & {
   pwa?: boolean;
   logo?: string;
 } = {
@@ -16,9 +16,13 @@ const Settings: LayoutSettings & {
   fixSiderbar: true,
   colorWeak: false,
   title: 'Ant Design Pro',
-  pwa: false,
+  pwa: true,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
+  token: {
+    // 参见ts声明，demo 见文档，通过token 修改样式 
+    //https://procomponents.ant.design/components/layout#%E9%80%9A%E8%BF%87-token-%E4%BF%AE%E6%94%B9%E6%A0%B7%E5%BC%8F
+  }
 };
 
-export default Settings;
+export default Settings
