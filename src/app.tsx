c8e7a4abb96fe047e978194ed0dfb8aa237d197a
Marushiru <gsc1997@foxmail.com>
2023-03-10 14:15:01 +0800

    fix: hide SettingDrawer in production environment (#10523)

--- src/app.tsx
+++ src/app.tsx
@@ -108,17 +108,19 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
       return (
         <>
           {children}
-          <SettingDrawer
-            disableUrlParams
-            enableDarkTheme
-            settings={initialState?.settings}
-            onSettingChange={(settings) => {
-              setInitialState((preInitialState) => ({
-                ...preInitialState,
-                settings,
-              }));
-            }}
-          />
+          {isDev &&
+            <SettingDrawer
+              disableUrlParams
+              enableDarkTheme
+              settings={initialState?.settings}
+              onSettingChange={(settings) => {
+                setInitialState((preInitialState) => ({
+                  ...preInitialState,
+                  settings,
+                }));
+              }}
+            />
+          }
         </>
       );
     },
