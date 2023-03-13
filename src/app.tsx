72d06a2ce4a751286b547017d140deef9cf12fc3
DaoYuan <txp1035@gmail.com>
2023-03-13 10:36:07 +0800

    chore: symbol

--- src/app.tsx
+++ src/app.tsx
@@ -9,7 +9,7 @@ import defaultSettings from '../config/defaultSettings';
 import { errorConfig } from './requestErrorConfig';
 import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
 import React from 'react';
-import { AvatarDropdown, AvatarName } from './components/RightContent/AvatarDropdown';
+import { AvatarDropdown, AvatarName } from '@/components/RightContent/AvatarDropdown';
 const isDev = process.env.NODE_ENV === 'development';
 const loginPath = '/user/login';
 
@@ -108,7 +108,7 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
       return (
         <>
           {children}
-          {isDev &&
+          {isDev && (
             <SettingDrawer
               disableUrlParams
               enableDarkTheme
@@ -120,7 +120,7 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
                 }));
               }}
             />
-          }
+          )}
         </>
       );
     },
