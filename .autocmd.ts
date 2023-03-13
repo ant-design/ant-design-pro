749abd5bd779925dd0a0b404d12278e1ae0d2ddc
DaoYuan <txp1035@gmail.com>
2023-03-13 10:33:16 +0800

    chore: better component

---
+++ .autocmd.ts
@@ -0,0 +1,22 @@
+
+
+export default {
+  translate: {
+    prettierPath: './.prettierrc.js',
+    outDir: './src/locales',
+    language: {
+      from: 'zh-CN', //中文（简体）
+      to: [
+        'bn-BD', //英语
+        'en-US', //英语
+        'fa-IR', //英语
+        'id-ID', //英语
+        'ja-JP', //英语
+        'pt-BR', //英语
+        'zh-TW', //中文（繁体）
+        'zh-CN', //日语
+        // 14种
+      ],
+    },
+  },
+};
