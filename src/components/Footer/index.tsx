a8f3cff70681940c3d2c95be87d24cb7def7b10f
期贤 <qixian.cs@antgroup.com>
2023-12-07 22:26:25 +0800

    chore: remove footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,23 +1,13 @@
 import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
-import { useIntl } from '@umijs/max';
 import React from 'react';
 
 const Footer: React.FC = () => {
-  const intl = useIntl();
-  const defaultMessage = intl.formatMessage({
-    id: 'app.copyright.produced',
-    defaultMessage: '蚂蚁集团体验技术部出品',
-  });
-
-  const currentYear = new Date().getFullYear();
-
   return (
     <DefaultFooter
       style={{
         background: 'none',
       }}
-      copyright={`${currentYear} ${defaultMessage}`}
       links={[
         {
           key: 'Ant Design Pro',
--- src/locales/en-US.ts
+++ src/locales/en-US.ts
@@ -11,7 +11,6 @@ export default {
   'layout.user.link.help': 'Help',
   'layout.user.link.privacy': 'Privacy',
   'layout.user.link.terms': 'Terms',
-  'app.copyright.produced': 'Produced by Ant Financial Experience Department',
   'app.preview.down.block': 'Download this page to your local project',
   'app.welcome.link.fetch-blocks': 'Get all block',
   'app.welcome.link.block-list': 'Quickly build standard, pages based on `block` development',
