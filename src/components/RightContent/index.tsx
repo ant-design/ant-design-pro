da86616b920794df4a4a79b2e068a06027cdcb1d
afc163 <afc163@gmail.com>
2025-07-04 12:39:20 +0800

    Apply suggestion from @afc163

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -1,6 +1,8 @@
 import { QuestionCircleOutlined } from '@ant-design/icons';
 import { SelectLang as UmiSelectLang } from '@umijs/max';
 
+import React from 'react';
+
 export type SiderTheme = 'light' | 'dark';
 
 export const SelectLang = () => {
