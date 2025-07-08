69773466154b2a33ac611c697caca40c524be1fa
afc163 <afc163@gmail.com>
2025-07-08 19:17:30 +0800

    refactor: simplify routes and remove unused account pages

--- src/pages/form/step-form/data.d.ts
+++
@@ -1,9 +0,0 @@
-export interface StepDataType {
-  payAccount: string;
-  receiverAccount: string;
-  receiverName: string;
-  amount: string;
-  receiverMode: string;
-}
-
-export type CurrentTypes = 'base' | 'confirm' | 'result';
