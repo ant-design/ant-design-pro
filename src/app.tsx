f5c75cfe0851dd4f96f3ec22e4d553e019da7d9e
afc163 <afc163@gmail.com>
2025-07-08 22:01:31 +0800

    Apply suggestion from @afc163

--- src/app.tsx
+++ src/app.tsx
@@ -17,7 +17,7 @@ import { errorConfig } from './requestErrorConfig';
 import '@ant-design/v5-patch-for-react-19';
 
 const isDev =
-  process.env.NODE_ENV === 'development' || process.env.GITHUB_ACTIONS;
+  process.env.NODE_ENV === 'development' || process.env.CI;
 const loginPath = '/user/login';
 
 /**
