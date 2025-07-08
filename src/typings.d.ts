ebe8e98b528c6f2b09d6d2549642efedfab79dfa
afc163 <afc163@gmail.com>
2025-07-08 17:45:04 +0800

    chore: 清理未用类型声明，保持 typings.d.ts 简洁

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -10,11 +10,5 @@ declare module '*.jpeg';
 declare module '*.gif';
 declare module '*.bmp';
 declare module '*.tiff';
-declare module 'omit.js';
-declare module 'numeral';
-declare module '@antv/data-set';
-declare module 'mockjs';
-declare module 'react-fittext';
-declare module 'bizcharts-plugin-slider';
 
 declare const REACT_APP_ENV: 'test' | 'dev' | 'pre' | false;
