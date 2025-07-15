79bf06760c8c24e017d3cb112ac97b8ea45c3080
afc163 <afc163@gmail.com>
2025-07-15 09:55:18 +0800

    chore: update dependencies and config

--- config/config.ts
+++ config/config.ts
@@ -7,7 +7,7 @@ import proxy from './proxy';
 
 import routes from './routes';
 
-const { REACT_APP_ENV = 'dev' } = process.env;
+const { UMI_ENV = 'dev' } = process.env;
 
 /**
  * @name 使用公共路径
@@ -61,7 +61,7 @@ export default defineConfig({
    * @doc 代理介绍 https://umijs.org/docs/guides/proxy
    * @doc 代理配置 https://umijs.org/docs/api/config#proxy
    */
-  proxy: proxy[REACT_APP_ENV as keyof typeof proxy],
+  proxy: proxy[UMI_ENV as keyof typeof proxy],
   /**
    * @name 快速热更新配置
    * @description 一个不错的热更新组件，更新时可以保留 state
