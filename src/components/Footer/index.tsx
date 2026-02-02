607e63f4fdb49d78306a618f2b2c29291ce85500
LiShuai (阿木) <lishuai4415@gmail.com>
2026-02-02 16:01:26 +0800

    fix: correct typo 'Desgin' to 'Design' in Footer (#11618)

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => {
       style={{
         background: 'none',
       }}
-      copyright="Powered by Ant Desgin"
+      copyright="Powered by Ant Design"
       links={[
         {
           key: 'Ant Design Pro',
